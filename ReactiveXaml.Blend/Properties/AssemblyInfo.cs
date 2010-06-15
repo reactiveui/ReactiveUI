cd4d4a4bc593764cc5321734bf3019e59e12a185
unknown <paul@.(none)>
2010-06-14 22:34:45 -0700

    Add Properties folder

---
+++ ReactiveXaml.Blend/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("An MVVM framework that integrates the Reactive Extensions")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Reactive XAML Expression Blend")]
+[assembly: AssemblyVersion("1.1.1.0")]
+
