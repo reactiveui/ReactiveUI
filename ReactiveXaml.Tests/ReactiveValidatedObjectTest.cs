3478a6cb0c86757fb733cb58155d415f25c8f041
Paul Betts <paul@paulbetts.org>
2010-08-09 19:25:02 -0700

    Add tests for ReactiveObject and ReactiveValidatedObject

---
+++ ReactiveXaml.Tests/ReactiveValidatedObjectTest.cs
@@ -0,0 +1,57 @@
+﻿using ReactiveXaml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+
+namespace ReactiveXaml.Tests
+{
+    public class ValidatedTestFixture : ReactiveValidatedObject
+    {
+        string _IsNotNullString;
+        [Required]
+        public string IsNotNullString {
+            get { return _IsNotNullString; }
+            set { RaiseAndSetIfChanged(_IsNotNullString, value, x => _IsNotNullString = x, "IsNotNullString"); }
+        }
+        
+        string _IsOnlyOneWord;
+        [RegularExpression(@"^[a-zA-Z]+$")]
+        public string IsOnlyOneWord {
+            get { return _IsOnlyOneWord; }
+            set { RaiseAndSetIfChanged(_IsOnlyOneWord, value, x => _IsOnlyOneWord = x, "IsOnlyOneWord"); }
+        }
+    }
+
+    [TestClass()]
+    public class ReactiveValidatedObjectTest
+    {
+        [TestMethod()]
+        public void IsValidTest()
+        {
+            var output = new List<bool>();
+            var fixture = new ValidatedTestFixture();
+            //fixture.IsValidObservable.Subscribe(output.Add);
+
+            Assert.IsFalse(fixture.IsValid());
+
+            fixture.IsNotNullString = "foo";
+            Assert.IsFalse(fixture.IsValid());
+
+            fixture.IsOnlyOneWord = "Foo Bar";
+            Assert.IsFalse(fixture.IsValid());
+
+            fixture.IsOnlyOneWord = "Foo";
+            Assert.IsTrue(fixture.IsValid());
+
+            fixture.IsOnlyOneWord = "";
+            Assert.IsFalse(fixture.IsValid());
+
+            new[] { false, false, false, true, false }.Zip(output, (expected, actual) => new { expected, actual })
+                .Do(Console.WriteLine)
+                .Run(x => Assert.AreEqual(x.expected, x.actual));
+        }
+    }
+}
