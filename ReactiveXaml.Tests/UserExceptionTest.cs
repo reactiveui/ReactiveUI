7649a2589d2ead1e8c68ee0bffb5bdf44358cad1
Paul Betts <paul@paulbetts.org>
2010-08-09 20:15:35 -0700

    Start to work on user-friendly error handling

---
+++ ReactiveXaml.Tests/UserExceptionTest.cs
@@ -0,0 +1,17 @@
+﻿using ReactiveXaml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ReactiveXaml.Tests
+{
+    [TestClass()]
+    public class UserExceptionTest
+    {
+        [TestMethod()]
+        public void UserExceptionConstructorTest()
+        {
+            UserException target = new UserException("Foo", "Bar");
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+    }
+}
---
+++ ReactiveXaml/UserException.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace ReactiveXaml
+{
+    public enum StockUserErrorIcon {
+        Critical,
+        Question,
+        Warning,
+    };
+
+    class RecoveryOptionEntry {
+        public string LocalizedText { get; set; }
+        public ICommand RecoveryFunc;
+    }
+
+    public class UserException : IEnableLogger
+    {
+        public UserException(
+                string Domain,
+                string LocalizedDescription,
+                string[] LocalizedRecoveryOptionNames = null,
+                ICommand[] RecoveryOptionCommands = null,
+                Dictionary<string, object> ContextInfo = null)
+        {
+            RecoveryOptions = (LocalizedRecoveryOptionNames ?? Enumerable.Empty<string>())
+                .Zip(RecoveryOptionCommands ?? Enumerable.Empty<ICommand>(), 
+                    (name, cmd) => new RecoveryOptionEntry() { LocalizedText = name, RecoveryFunc = cmd })
+                .ToList();
+
+            this.Domain = Domain;
+            this.ContextInfo = ContextInfo ?? new Dictionary<string, object>();
+            this.UserErrorIcon = StockUserErrorIcon.Warning;
+        }
+
+        public string Domain { get; protected set; }
+        public Dictionary<string, object> ContextInfo { get; protected set; }
+
+        public string LocalizedDescription { get; set; }
+        public string LocalizedFailureReason { get; set; }
+        public string LocalizedRecoverySuggestion { get; set; }
+        public object UserErrorIcon { get; set; }
+        public string FilePathError { get; set; }
+        
+        List<RecoveryOptionEntry> RecoveryOptions;
+
+        public void AddRecoveryOption(string LocalizedRecoveryOptionName, ICommand RecoveryOptionCommand)
+        {
+            RecoveryOptions.Add(new RecoveryOptionEntry() { LocalizedText = LocalizedRecoveryOptionName, RecoveryFunc = RecoveryOptionCommand });
+        }
+    }
+}
+
+// vim: tw=120 ts=4 sw=4 et enc=utf8 :
