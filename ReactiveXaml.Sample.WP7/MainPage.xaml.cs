03288d141773c40eb3e2002488fb4518a4d846b0
Paul Betts <paul@paulbetts.org>
2010-09-26 18:45:32 -0700

    Use helper functions to make sample clearer

--- ReactiveXaml.Sample.WP7/MainPage.xaml.cs
+++ ReactiveXaml.Sample.WP7/MainPage.xaml.cs
@@ -42,7 +42,6 @@ namespace ReactiveXaml.Sample.WP7
 
         public ReactiveAsyncCommand TranslateText { get; protected set; }
 
-        const string AppId = "8E94679989E7F3717D0B412E53BEAA73794369B2";
         public TranslatorViewModel()
         {
             TranslateText = new ReactiveAsyncCommand(null, 1);
@@ -56,14 +55,28 @@ namespace ReactiveXaml.Sample.WP7
 
             var client = new LanguageServiceClient() as LanguageService;
 
-            var translation_func = Observable.FromAsyncPattern<TranslateRequest, TranslateResponse>(
-                client.BeginTranslate, client.EndTranslate);
+            var translation_func = Observable.FromAsyncPattern<string, string>(
+                client.BeginTranslateToGerman, client.EndTranslateToGerman);
             
-            var results = TranslateText
-                .RegisterObservableAsyncFunction(x => translation_func(new TranslateRequest(AppId, (string)x, "en", "de")))
-                .Select(x => x.TranslateResult);
+            var results = TranslateText.RegisterObservableAsyncFunction(x => translation_func((string)x));
 
             _TranslatedText = this.ObservableToProperty(results, x => x.TranslatedText);
         }
     }
+
+    public static class LanguageServiceHelper
+    {
+        const string AppId = "8E94679989E7F3717D0B412E53BEAA73794369B2";
+
+        public static IAsyncResult BeginTranslateToGerman(this LanguageService Client, string Text, AsyncCallback Callback, object Context)
+        {
+            return Client.BeginTranslate(new TranslateRequest(AppId, Text, "en", "de"), Callback, Context);
+        }
+
+        public static string EndTranslateToGerman(this LanguageService Client, IAsyncResult Result)
+        {
+            return Client.EndTranslate(Result).TranslateResult;
+        }
+    }
+
 }
