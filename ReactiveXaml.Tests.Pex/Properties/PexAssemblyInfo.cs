bcab071e67e1355f973d53895046e0dc9fab5637
Paul Betts <paul@paulbetts.org>
2010-09-28 22:20:08 -0700

    Add Pex test project

---
+++ ReactiveXaml.Tests.Pex/Properties/PexAssemblyInfo.cs
@@ -0,0 +1,42 @@
+using Microsoft.Pex.Framework.Coverage;
+using Microsoft.Pex.Framework.Creatable;
+using Microsoft.Pex.Framework.Instrumentation;
+using Microsoft.Pex.Framework.Moles;
+using Microsoft.Pex.Framework.Settings;
+using Microsoft.Pex.Framework.Validation;
+using Microsoft.Pex.Linq;
+
+// Microsoft.Pex.Framework.Settings
+[assembly: PexAssemblySettings(TestFramework = "VisualStudioUnitTest")]
+
+// Microsoft.Pex.Framework.Instrumentation
+[assembly: PexAssemblyUnderTest("ReactiveXaml")]
+[assembly: PexInstrumentAssembly("System.Reactive")]
+[assembly: PexInstrumentAssembly("System.Core")]
+[assembly: PexInstrumentAssembly("System.Runtime.Serialization")]
+[assembly: PexInstrumentAssembly("PresentationCore")]
+[assembly: PexInstrumentAssembly("System.CoreEx")]
+[assembly: PexInstrumentAssembly("System.ComponentModel.DataAnnotations")]
+
+// Microsoft.Pex.Framework.Creatable
+[assembly: PexCreatableFactoryForDelegates]
+
+// Microsoft.Pex.Framework.Validation
+[assembly: PexAllowedContractRequiresFailureAtTypeUnderTestSurface]
+[assembly: PexAllowedXmlDocumentedException]
+
+// Microsoft.Pex.Framework.Coverage
+[assembly: PexCoverageFilterAssembly(PexCoverageDomain.UserOrTestCode, "System.Reactive")]
+[assembly: PexCoverageFilterAssembly(PexCoverageDomain.UserOrTestCode, "System.Core")]
+[assembly: PexCoverageFilterAssembly(PexCoverageDomain.UserOrTestCode, "System.Runtime.Serialization")]
+[assembly: PexCoverageFilterAssembly(PexCoverageDomain.UserOrTestCode, "PresentationCore")]
+[assembly: PexCoverageFilterAssembly(PexCoverageDomain.UserOrTestCode, "System.CoreEx")]
+[assembly: PexCoverageFilterAssembly(PexCoverageDomain.UserOrTestCode, "System.ComponentModel.DataAnnotations")]
+
+// Microsoft.Pex.Framework.Moles
+[assembly: PexAssumeContractEnsuresFailureAtBehavedSurface]
+[assembly: PexChooseAsBehavedCurrentBehavior]
+
+// Microsoft.Pex.Linq
+[assembly: PexLinqPackage]
+
--- ReactiveXaml/Properties/AssemblyInfo.cs
+++ ReactiveXaml/Properties/AssemblyInfo.cs
@@ -1,7 +1,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyDescription("An MVVM framework that integrates the Reactive Extensions")]
 [assembly: AssemblyProduct("Reactive XAML")]
 [assembly: AssemblyVersion("1.3.0.0")]
 
+[assembly: InternalsVisibleTo("ReactiveXaml.Explorables")]
