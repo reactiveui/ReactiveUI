961fbbb73b9a46e01cc5864ae681205077972215
Paul Betts <paul@paulbetts.org>
2010-11-20 19:53:23 -0800

    Add RxStorage class, random hacks

---
+++ ReactiveXaml.Serialization/RxStorage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReactiveXaml.Serialization
+{
+    public static class RxStorage
+    {
+        public static IStorageEngine Engine { get; set; }
+
+        static RxStorage()
+        {
+            if (RxApp.InUnitTestRunner()) {
+                Engine = new DictionaryStorageEngine();
+            } else {
+                // TODO: Provide a path to the user's AppData folder
+                Engine = new DictionaryStorageEngine();
+            }
+        }
+    }
+}
+
+// vim: tw=120 ts=4 sw=4 et enc=utf8 :
