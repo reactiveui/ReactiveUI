0a6d69f510e1b3ba32f214c0ba32ad6d2b184aac
Paul Betts <paul@paulbetts.org>
2010-12-02 01:20:23 -0800

    Commit the VirtualTestScheduler stuff

--- ReactiveXaml.Tests/TestUtils.cs
+++ ReactiveXaml.Tests/TestUtils.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Concurrency;
+using System.Disposables;
 using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -17,6 +19,36 @@ namespace ReactiveXaml.Tests
             for(int i=0; i < left.Length; i++) {
                 Assert.AreEqual(left[i], right[i]);
             }
+        }
+    }
+
+    public static class TestUtils
+    {
+        public static IDisposable WithTestScheduler(TestScheduler sched) {
+            var prevDef = RxApp.DeferredScheduler;
+            var prevTask = RxApp.TaskpoolScheduler;
+
+            RxApp.DeferredScheduler = sched;
+            RxApp.TaskpoolScheduler = sched;
+
+            return Disposable.Create(() => {
+                RxApp.DeferredScheduler = prevDef;
+                RxApp.TaskpoolScheduler = prevTask;
+            });
+        }
+
+        public static TRet With<TRet>(this TestScheduler sched, Func<TestScheduler, TRet> block)
+        {
+            TRet ret;
+            using(WithTestScheduler(sched)) {
+                ret = block(sched);
+            }
+            return ret;
+        }
+
+        public static void RunToMilliseconds(this TestScheduler sched, double milliseconds)
+        {
+            sched.RunTo(sched.FromTimeSpan(TimeSpan.FromMilliseconds(milliseconds)));
         }
     }
 }
