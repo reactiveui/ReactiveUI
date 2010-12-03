d946829efaad13a4478baac94655b128e080d0b1
Paul Betts <paul@paulbetts.org>
2010-12-02 21:41:17 -0500

    Add ObservableAsyncMRUCache (still broken right now)

---
+++ ReactiveXaml.Tests/ObservableAsyncMRUCacheTest.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using ReactiveXaml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Concurrency;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReactiveXaml.Tests
+{
+    [TestClass()]
+    public class ObservableAsyncMRUCacheTest : IEnableLogger
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            var input = new[] {1, 1, 1, 1, 1};
+            var sched = new TestScheduler();
+            ObservableAsyncMRUCache<int, int> fixture;
+
+            var delay = TimeSpan.FromSeconds(1.0);
+            fixture = new ObservableAsyncMRUCache<int, int>(x => Observable.Return(x*5).Delay(delay, sched), 5, 2);
+
+            int result = 0;
+            var t = new Task(() => {
+                foreach (int x in input.Select(x => fixture.Get(x))) {
+                    result += x;
+                }
+            });
+            t.Start();
+
+            this.Log().Debug("Running to t=0");
+            sched.RunTo(sched.FromTimeSpan(TimeSpan.FromMilliseconds(500)));
+
+            Thread.Sleep(100);
+            Assert.AreEqual(0, result);
+
+            this.Log().Debug("Running to t=1200");
+            sched.RunTo(sched.FromTimeSpan(TimeSpan.FromMilliseconds(1200)));
+
+            Thread.Sleep(100);
+            Assert.AreEqual(25, result);
+
+            this.Log().Debug("Running to end");
+            sched.Run();
+            t.Wait();
+            Assert.AreEqual(25, result);
+        }
+
+        [TestMethod()]
+        public void AsyncGetTest()
+        {
+            var input = new[] { 1, 1, 1, 1, 1 };
+            var sched = new TestScheduler();
+            var output = new ConcurrentQueue<int>();
+
+            var delay = TimeSpan.FromSeconds(1.0);
+            var fixture = new ObservableAsyncMRUCache<int, int>(x => Observable.Return(x*5).Delay(delay, sched), 5, 2);
+
+            int result = 0;
+            input.ToObservable(sched).SelectMany<int, int>(x => (IObservable<int>)fixture.AsyncGet(x)).Subscribe(x => result += x);
+
+            this.Log().Debug("Running to t=0");
+            sched.RunTo(sched.FromTimeSpan(TimeSpan.FromMilliseconds(500)));
+            Assert.AreEqual(0, result);
+
+            this.Log().Debug("Running to t=1200");
+            sched.RunTo(sched.FromTimeSpan(TimeSpan.FromMilliseconds(1200)));
+            Assert.AreEqual(25, result);
+
+            this.Log().Debug("Running to end");
+            sched.Run();
+            Assert.AreEqual(25, result);
+        }
+
+#if FALSE
+        [TestMethod()]
+        public void CachedValuesTest()
+        {
+            var input = new[] { 1, 2, 1, 3, 1 };
+            var sched = new TestScheduler();
+
+            var delay = TimeSpan.FromSeconds(1.0);
+            var fixture = new ObservableAsyncMRUCache<int, int>(x => Observable.Return(x*5).Delay(delay, sched), 2, 2);
+
+            var results = input.ToObservable().SelectMany(fixture.AsyncGet).CreateCollection();
+            sched.RunTo(sched.FromTimeSpan(TimeSpan.FromMilliseconds(500)));
+
+            Assert.AreEqual(0, fixture.CachedValues().Count());
+
+            sched.RunTo(sched.FromTimeSpan(TimeSpan.FromMilliseconds(1500)));
+
+            var output = fixture.CachedValues().ToArray();
+            Assert.IsTrue(output.Length == 2);
+            Assert.AreEqual(input.Length, results.Count);
+        }
+#endif
+
+        [TestMethod()]
+        public void CacheShouldQueueOnceWeHitOurConcurrentLimit()
+        {
+            var input = new[] { 1, 2, 3, 4, 1 };
+            var sched = new TestScheduler();
+            var output = new ConcurrentQueue<int>();
+
+            var delay = TimeSpan.FromSeconds(1.0);
+            var fixture = new ObservableAsyncMRUCache<int, int>(x => Observable.Return(x*5).Delay(delay, sched), 5, 2);
+
+            int result = 0;
+            input.ToObservable(sched).SelectMany<int, int>(x => (IObservable<int>)fixture.AsyncGet(x)).Subscribe(x => result += x);
+
+            this.Log().Debug("Running to t=500");
+            sched.RunTo(sched.FromTimeSpan(TimeSpan.FromMilliseconds(500)));
+            Assert.AreEqual(0, result);
+
+            this.Log().Debug("Running to t=1500");
+            sched.RunTo(sched.FromTimeSpan(TimeSpan.FromMilliseconds(1500)));
+            Assert.AreEqual(1*5 + 2*5 + 1*5, result);
+
+            this.Log().Debug("Running to t=2500");
+            sched.RunTo(sched.FromTimeSpan(TimeSpan.FromMilliseconds(2500)));
+            Assert.AreEqual(1*5 + 2*5 + 3*5 + 4*5 + 1*5, result);
+
+            this.Log().Debug("Running to t=5000");
+            sched.RunTo(sched.FromTimeSpan(TimeSpan.FromMilliseconds(5000)));
+            Assert.AreEqual(1*5 + 2*5 + 3*5 + 4*5 + 1*5, result);
+        }
+
+        [TestMethod()]
+        public void CacheShouldEatExceptionsAndMarshalThemToObservable()
+        {
+            var input = new[] { 5, 2, 10, 0/*boom!*/, 5 };
+            var sched = new TestScheduler();
+
+            Observable.Throw<int>(new Exception("Foo")).Subscribe(x => {
+                Console.WriteLine(x);
+            }, ex => {
+                Console.WriteLine(ex);
+            }, () => {
+                Console.WriteLine("Completed");
+            });
+
+            var delay = TimeSpan.FromSeconds(1.0);
+            var fixture = new ObservableAsyncMRUCache<int, int>(x => 
+                (x == 0 ? Observable.Throw<int>(new Exception("Boom!")) : Observable.Return(10 * x)).Delay(delay, sched), 5, 2, sched);
+
+            Exception exception = null;
+            int completed = 0;
+            input.ToObservable()
+                .SelectMany(x => (IObservable<int>)fixture.AsyncGet(x))
+                .Do(x => {
+                    this.Log().DebugFormat("Result = {0}", x);
+                    completed++;
+                }, ex => exception = exception ?? ex);
+
+            this.Log().Debug("Running to t=500");
+            sched.RunTo(sched.FromTimeSpan(TimeSpan.FromMilliseconds(1500)));
+            Assert.IsNotNull(exception);
+            Assert.AreEqual(0, completed);
+
+            this.Log().Debug("Running to t=1500");
+            sched.RunTo(sched.FromTimeSpan(TimeSpan.FromMilliseconds(1500)));
+
+            Assert.IsNotNull(exception);
+            Assert.AreEqual(4, completed);
+            this.Log().Debug(exception);
+        }
+
+        [TestMethod()]
+        public void BlockingGetShouldRethrowExceptions()
+        {
+            var input = new[] { 5, 2, 10, 0/*boom!*/, 5 };
+            var fixture = new QueuedAsyncMRUCache<int, int>(x => { Thread.Sleep(1000); return 50 / x; }, 5, 5);
+            int[] output = {0};
+            Assert.Fail();
+
+            bool did_throw = false;
+            try {
+                output = input.Select(x => fixture.Get(x)).ToArray();
+            } catch(Exception ex) {
+                did_throw = true;
+                this.Log().Debug("Exception thrown", ex);
+            }
+
+            output.Run(x => this.Log().Debug(x));
+            Assert.IsTrue(did_throw);
+        }
+
+        void assertStopwatch(TimeSpan max_time, Action block)
+        {
+            DateTime start = DateTime.Now;
+            block();
+            var delta = DateTime.Now - start;
+            Assert.IsTrue(delta < max_time, delta.ToString());
+        }
+    }
+}
---
+++ ReactiveXaml/ObservableAsyncMRUCache.cs
@@ -0,0 +1,420 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Concurrency;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+
+#if WINDOWS_PHONE
+using Microsoft.Phone.Reactive;
+#endif
+
+#if !SILVERLIGHT
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+#endif
+
+namespace ReactiveXaml
+{
+    public class ObservableAsyncMRUCache<TParam, TVal> : IEnableLogger
+    {
+        readonly MemoizingMRUCache<TParam, IObservable<TVal>> _innerCache;
+        readonly SemaphoreSubject<long> _callQueue;
+        readonly Func<TParam, IObservable<TVal>> _fetcher;
+        long currentCall = 0;
+
+        public ObservableAsyncMRUCache(Func<TParam, IObservable<TVal>> func, int maxSize, int maxConcurrent = 1, IScheduler sched = null, Action<TVal> onRelease = null)
+        {
+            _callQueue = new SemaphoreSubject<long>(maxConcurrent, sched);
+            _fetcher = func;
+
+            Action<IObservable<TVal>> release = null;
+            if (onRelease != null) {
+                release = new Action<IObservable<TVal>>(x => onRelease(x.First()));
+            }
+
+            _innerCache = new MemoizingMRUCache<TParam, IObservable<TVal>>((x, val) => {
+                var ret = (IObservable<TVal>)val;
+                return ret;
+            }, maxSize, release);
+        }
+
+        public IObservable<TVal> AsyncGet(TParam key)
+        {
+            IObservable<TVal> result;
+            if (_innerCache.TryGet(key, out result)) {
+                return result;
+            }
+
+            var myCall = Interlocked.Increment(ref currentCall);
+
+            var rs = new ReplaySubject<TVal>();
+            _callQueue.Where(x => x == myCall).Subscribe(_ => {
+                this.Log().DebugFormat("Dispatching '{0}'", key);
+                IObservable<TVal> fetched = null;
+                try {
+                    fetched = _fetcher(key);
+                } catch (Exception ex) {
+                    _callQueue.Release();
+                    rs.OnError(ex);
+                }
+
+                fetched.Subscribe(x => {
+                    rs.OnNext(x);
+                }, ex => {
+                    _callQueue.Release();
+                    rs.OnError(ex);
+                }, () => {
+                    _callQueue.Release();
+                    rs.OnCompleted();
+                });
+            });
+
+
+            /*
+            _callQueue.Where(x => x == myCall).SelectMany(_ => _fetcher(key).Do(
+                x => rs.OnNext(x),
+                ex => {
+                    _callQueue.Release();
+                    rs.OnError(ex);
+                }, () => {
+                    _callQueue.Release();
+                    rs.OnCompleted();
+                }).Catch(e)
+             */
+
+            lock(_innerCache) {
+                _innerCache.Get(key, rs);
+            }
+
+            _callQueue.OnNext(myCall);
+            return rs;
+        }
+
+        public TVal Get(TParam key)
+        {
+            return AsyncGet(key).First();
+        }
+    }
+
+    public sealed class OldObservableAsyncMRUCache<TParam, TVal> : IEnableLogger
+    {
+        MemoizingMRUCache<TParam, IObservable<TVal>> _innerCache;
+        IDictionary<TParam, IObservable<TVal>> _inflightItems;
+        ReactiveSemaphore _concurrentCount;
+        Func<TParam, IObservable<TVal>> _fetcher;
+        IScheduler _sched;
+
+        public OldObservableAsyncMRUCache(Func<TParam, IObservable<TVal>> func, int maxSize, int maxConcurrent = 1, Action<TVal> onRelease = null)
+        {
+            _concurrentCount = new ReactiveSemaphore(maxConcurrent);
+            _fetcher = func;
+#if SILVERLIGHT
+            _inflightItems = new LockedDictionary<TParam, IObservable<TVal>>();
+#else
+            _inflightItems = new ConcurrentDictionary<TParam, IObservable<TVal>>();
+#endif
+
+            Action<IObservable<TVal>> release = null;
+            if (onRelease != null) {
+                release = new Action<IObservable<TVal>>(x => onRelease(x.First()));
+            }
+
+            _innerCache = new MemoizingMRUCache<TParam, IObservable<TVal>>((x, val) => {
+                var ret = (IObservable<TVal>)val;
+                return ret;
+            }, maxSize, release);
+        }
+
+        public IEnumerable<TVal> CachedValues()
+        {
+            lock (_innerCache) {
+                return _innerCache.CachedValues().Select(x => x.First());
+            }
+        }
+
+        int timesCalled = 0;
+        public IObservable<TVal> AsyncGet(TParam key)
+        {
+            IObservable<TVal> ret = null;
+
+            timesCalled++;
+            lock(_innerCache) {
+                if (_innerCache.TryGet(key, out ret))
+                    return ret;
+            }
+
+            if (_inflightItems.TryGetValue(key, out ret)) {
+                return ret;
+            }
+
+            _concurrentCount.WaitOne();
+            if (_inflightItems.ContainsKey(key)) {
+                _concurrentCount.Release();
+                return AsyncGet(key);
+            }
+
+            IObservable<TVal> dontcare;
+            ret = _fetcher(key);
+            ret.Subscribe(x => {
+                lock(_innerCache) {
+                    _innerCache.Get(key, Observable.Return(x));
+                }
+            }, ex => {
+                lock (_innerCache) {
+                    _innerCache.Invalidate(key);
+                    _innerCache.Get(key, Observable.Throw<TVal>(ex));
+                }
+                _inflightItems.Remove(key);
+                _concurrentCount.Release();
+            }, () => {
+                _inflightItems.Remove(key);
+                _concurrentCount.Release();
+            });
+
+            _inflightItems[key] = ret;
+            return ret;
+        }
+
+        public TVal Get(TParam key)
+        {
+            return AsyncGet(key).First();
+        }
+    }
+
+    public class SemaphoreSubject<T> : ISubject<T>, IEnableLogger
+    {
+        readonly Subject<T> _inner;
+        Queue<T> _nextItems = new Queue<T>();
+        long _count;
+        readonly long _maxCount;
+
+        public SemaphoreSubject(int maxCount, IScheduler sched = null)
+        {
+            this.Log().DebugFormat("maxCount is '{0}'", maxCount);
+            _inner = (sched != null ? new Subject<T>(sched) : new Subject<T>());
+            _maxCount = maxCount;
+        }
+
+        public void OnNext(T value)
+        {
+            var queue = Interlocked.CompareExchange(ref _nextItems, null, null);
+            if (queue == null)
+                return;
+
+            lock (queue) {
+                this.Log().DebugFormat("OnNext called for '{0}', count is '{1}'", value, _count);
+                queue.Enqueue(value);
+            }
+            yieldUntilEmptyOrBlocked();
+        }
+
+        public void Release()
+        {
+            Interlocked.Decrement(ref _count);
+
+            this.Log().DebugFormat("Releasing, count is now {0}", _count);
+            yieldUntilEmptyOrBlocked();
+        }
+
+        public void OnCompleted()
+        {
+            var queue = Interlocked.Exchange(ref _nextItems, null);
+            if (queue == null)
+                return;
+
+            T[] items;
+            lock (queue) {
+                items = queue.ToArray();
+            }
+
+            foreach(var v in items) {
+                _inner.OnNext(v);
+            }
+
+            _inner.OnCompleted();
+        }
+
+        public void OnError(Exception error)
+        {
+            var queue = Interlocked.Exchange(ref _nextItems, null);
+            _inner.OnError(error);
+        }
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            return _inner.Subscribe(observer);
+        }
+
+        void yieldUntilEmptyOrBlocked()
+        {
+            var queue = Interlocked.CompareExchange(ref _nextItems, null, null);
+
+            if (queue == null) {
+                return;
+            }
+
+            while(Interlocked.Read(ref _count) < _maxCount) {
+                T next;
+                lock(queue) {
+                    if (queue.Count == 0) {
+                        break;
+                    }
+                    next = queue.Dequeue();
+                }
+
+                this.Log().DebugFormat("Yielding '{0}', _count = {1}, _maxCount = {2}", next, _count, _maxCount);
+                _inner.OnNext(next);
+
+                if (Interlocked.Increment(ref _count) >= _maxCount) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public class ReactiveSemaphore
+    {
+        BehaviorSubject<long> _subject;
+        long _count;
+        int _maxCount;
+
+        public ReactiveSemaphore(int maxCount, IScheduler scheduler = null)
+        {
+            _count = 0;
+            _maxCount = maxCount;
+            if (scheduler == null) {
+                _subject = new BehaviorSubject<long>(0, Scheduler.Immediate);
+            } else {
+                _subject = new BehaviorSubject<long>(0, scheduler);
+            }
+        }
+
+        public void WaitOne()
+        {
+            _subject.Where(x => x < _maxCount).First();
+            _subject.OnNext(Interlocked.Increment(ref _count));
+        }
+
+        public void Release()
+        {
+            _subject.OnNext(Interlocked.Decrement(ref _count));
+        }
+    }
+
+    public class LockedDictionary<TKey, TVal> : IDictionary<TKey, TVal>
+    {
+        Dictionary<TKey, TVal> _inner = new Dictionary<TKey, TVal>();
+
+        public void Add(TKey key, TVal value) {
+            lock (_inner) {
+                _inner.Add(key, value);
+            }
+        }
+
+        public bool ContainsKey(TKey key) {
+            lock (_inner) {
+                return _inner.ContainsKey(key);
+            }
+        }
+
+        public ICollection<TKey> Keys {
+            get {
+                lock (_inner) {
+                    return _inner.Keys.ToArray();
+                }
+            }
+        }
+
+        public bool Remove(TKey key) {
+            lock (_inner) {
+                return _inner.Remove(key); 
+            }
+        }
+
+        public bool TryGetValue(TKey key, out TVal value) {
+            lock (_inner) {
+                return _inner.TryGetValue(key, out value);
+            }
+        }
+
+        public ICollection<TVal> Values {
+            get {
+                lock (_inner) {
+                    return _inner.Values.ToArray();
+                }
+            }
+        }
+
+        public TVal this[TKey key] {
+            get {
+                lock (_inner) {
+                    return _inner[key];
+                }
+            }
+            set {
+                lock (_inner) {
+                    _inner[key] = value;
+                }
+            }
+        }
+
+        public void Add(KeyValuePair<TKey, TVal> item) {
+            lock (_inner) {
+                _inner.Add(item.Key, item.Value); 
+            }
+        }
+
+        public void Clear() {
+            lock (_inner) {
+                _inner.Clear();
+            }
+        }
+
+        public bool Contains(KeyValuePair<TKey, TVal> item) {
+            lock(_inner) {
+                var inner = _inner as IDictionary<TKey, TVal>;
+                return (inner.Contains(item));
+            }
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TVal>[] array, int arrayIndex) {
+            lock(_inner) {
+                var inner = _inner as IDictionary<TKey, TVal>;
+                inner.CopyTo(array, arrayIndex);
+            }
+        }
+
+        public int Count {
+            get {
+                lock (_inner) {
+                    return _inner.Count;
+                }
+            }
+        }
+
+        public bool IsReadOnly {
+            get { return false; }
+        }
+
+        public bool Remove(KeyValuePair<TKey, TVal> item) {
+            lock(_inner) {
+                var inner = _inner as IDictionary<TKey, TVal>;
+                return inner.Remove(item);
+            }
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TVal>> GetEnumerator() {
+            lock (_inner) {
+                return _inner.ToList().GetEnumerator();
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+            lock(_inner) {
+                return _inner.ToArray().GetEnumerator();
+            }
+        }
+    }
+}
