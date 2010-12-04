e9f95a09361fe4026736c2044620440ff4b7a8e3
Paul Betts <paul@paulbetts.org>
2010-12-03 23:23:48 -0500

    Add iOS ReactiveXaml sample

---
+++ ReactiveXaml.Sample.iOS/AppDelegateIPad.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using ReactiveXaml;
+using ReactiveXaml.iOS;
+
+namespace ReactiveXaml.Sample.iOS
+{
+	// The name AppDelegateIPad is referenced in the MainWindowIPad.xib file.
+	public partial class AppDelegateIPad : UIApplicationDelegateRx
+	{
+		// This method is invoked when the application has loaded its UI and its ready to run
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			// If you have defined a view, add it here:
+			// window.AddSubview (navigationController.View);
+			
+			window.MakeKeyAndVisible ();
+			
+			return true;
+		}
+	}
+}
+
