d00253b4687cd438f58f3ed4fa6fec1a796a55c6
Paul Betts <paul@paulbetts.org>
2010-12-11 19:53:28 -0800

    Implement new commit features in DSE and Null engine, add blatantly missing field from interface

--- ReactiveXaml.Serialization/SyncPointInformation.cs
+++ ReactiveXaml.Serialization/SyncPointInformation.cs
@@ -9,39 +9,24 @@ namespace ReactiveXaml.Serialization
 {
     public class SyncPointInformation : ISyncPointInformation
     {
-        Guid _RootObjectHash;
-        public Guid RootObjectHash { 
-            get { return _RootObjectHash; }
-        }
+        public Guid RootObjectHash { get; protected set; }
+        public Guid ParentSyncPoint { get; protected set; }
+        public string RootObjectTypeName { get; protected set; }
+        public string Qualifier { get; protected set; }
+        public DateTimeOffset CreatedOn { get; protected set; }
 
-        string _RootObjectTypeName;
-        public string RootObjectTypeName { 
-            get { return _RootObjectTypeName; }
-        }
+        [IgnoreDataMember]
+        public Guid ContentHash { get; protected set; }
 
-        string _Qualifier;
-        public string Qualifier {
-            get { return _Qualifier; }
-        }
-
-        DateTimeOffset _CreatedOn;
-        public DateTimeOffset CreatedOn {
-            get { return _CreatedOn; }
-        }
-
-        public SyncPointInformation(Guid rootObjectHash, Type rootObjectType, string qualifier, DateTimeOffset createdOn)
+        public SyncPointInformation(Guid rootObjectHash, Guid parentSyncPoint, Type rootObjectType, string qualifier, DateTimeOffset createdOn)
         {
-            _RootObjectHash = rootObjectHash;
-            _RootObjectTypeName = rootObjectType.FullName;
-            _Qualifier = qualifier;
-            _CreatedOn = createdOn;
-            _ContentHash = new Guid(String.Format(CultureInfo.InvariantCulture, "{0}{1}{2}{3}", 
-                _RootObjectHash, _RootObjectTypeName, _Qualifier, _CreatedOn).MD5Hash());
-        }
-
-        Guid _ContentHash;
-        public Guid ContentHash {
-            get { return _ContentHash; }
+            RootObjectHash = rootObjectHash;
+            ParentSyncPoint = parentSyncPoint;
+            RootObjectTypeName = rootObjectType.FullName;
+            Qualifier = qualifier;
+            CreatedOn = createdOn;
+            ContentHash = new Guid(String.Format(CultureInfo.InvariantCulture, "{0}{1}{2}{3}{4}", 
+                RootObjectHash, parentSyncPoint, RootObjectTypeName, Qualifier, CreatedOn).MD5Hash());
         }
 
         public IObservable<object> ItemChanging {
@@ -54,7 +39,7 @@ namespace ReactiveXaml.Serialization
 
         public Guid CalculateHash() 
         {
-            return _ContentHash;
+            return ContentHash;
         }
     }
 }
