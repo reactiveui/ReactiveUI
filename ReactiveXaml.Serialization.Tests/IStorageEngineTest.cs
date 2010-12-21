b8f74f7067a247f6d4339cd799c0482e9a2d0fd5
Paul Betts <paul@paulbetts.org>
2010-12-21 10:23:12 +0100

    Add concrete test class for EsentStorageEngine, add test to ensure data isn't just being cached in memory

--- ReactiveXaml.Serialization.Tests/IStorageEngineTest.cs
+++ ReactiveXaml.Serialization.Tests/IStorageEngineTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Pex.Framework;
+﻿using System.IO;
+using System.Threading;
+using Microsoft.Pex.Framework;
 using ReactiveXaml.Serialization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -9,7 +11,7 @@ namespace ReactiveXaml.Serialization.Tests
     [TestClass]
     public abstract class StorageEngineInterfaceTest
     {
-        protected abstract IStorageEngine createFixture();
+        protected abstract IStorageEngine createFixture(int key = -1);
 
         [TestMethod]
         public void StorageEngineLoadSaveSmokeTest()
@@ -18,6 +20,7 @@ namespace ReactiveXaml.Serialization.Tests
             var fixture = createFixture();
             SubobjectTestObject result;
 
+            using(fixture)
             using(fixture.AsPrimaryEngine()) {
                 fixture.Save(input);
                 fixture.FlushChanges();
@@ -29,14 +32,61 @@ namespace ReactiveXaml.Serialization.Tests
                 Assert.AreEqual(input.SomeProperty, result.SomeProperty);
             }
         }
+
+        [TestMethod]
+        public void StorageEngineShouldActuallySerializeStuff()
+        {
+            var input = new SubobjectTestObject() {SomeProperty = "Foo"};
+            var fixture = createFixture(0xbeef);
+            SubobjectTestObject result;
+
+            using(fixture)
+            using(fixture.AsPrimaryEngine()) {
+                fixture.Save(input);
+                fixture.FlushChanges();
+
+                Assert.IsTrue(fixture.GetAllObjectHashes().Contains(input.ContentHash));
+
+                result = fixture.Load<SubobjectTestObject>(input.ContentHash);
+                Assert.AreEqual(input.ContentHash, result.ContentHash);
+                Assert.AreEqual(input.SomeProperty, result.SomeProperty);
+            }
+
+            fixture = createFixture(0xbeef);
+            using(fixture)
+            using(fixture.AsPrimaryEngine()) {
+                result = fixture.Load<SubobjectTestObject>(input.ContentHash);
+                Assert.AreEqual(input.ContentHash, result.ContentHash);
+                Assert.AreEqual(input.SomeProperty, result.SomeProperty);
+            }
+        }
     }
 
     [TestClass]
     public class DictionaryStorageEngineTest : StorageEngineInterfaceTest
     {
-        protected override IStorageEngine createFixture()
+        protected override IStorageEngine createFixture(int key = -1)
         {
-            return new DictionaryStorageEngine();
+            if (key == -1) {
+                return new DictionaryStorageEngine();
+            }
+
+            var di = new DirectoryInfo(".");
+            var path = di.CreateSubdirectory(key.ToString());
+            return new DictionaryStorageEngine(Path.Combine(path.FullName, "dict.json"));
+        }
+    }
+
+    [TestClass]
+    public class EsentStorageEngineTest : StorageEngineInterfaceTest
+    {
+        int _nextFreeSubdirectory = 1;
+        protected override IStorageEngine createFixture(int key = -1)
+        {
+            var di = new DirectoryInfo(".");
+            int subdir = (key == -1 ? Interlocked.Increment(ref _nextFreeSubdirectory) : key);
+            var path = di.CreateSubdirectory(subdir.ToString());
+            return new Esent.EsentStorageEngine(path.FullName);
         }
     }
 }
