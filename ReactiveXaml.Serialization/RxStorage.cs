216c9b308066991e8426947b8f887e9b50163156
Paul Betts <paul@paulbetts.org>
2010-12-21 08:38:47 +0100

    Ensure that tests that set RxStorage don't stomp on each other when run concurrently

--- ReactiveXaml.Serialization/RxStorage.cs
+++ ReactiveXaml.Serialization/RxStorage.cs
@@ -2,12 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace ReactiveXaml.Serialization
 {
     public static class RxStorage
     {
-        public static IExtendedStorageEngine Engine { get; private set; }
+        public static IExtendedStorageEngine _Engine;
+        public static IExtendedStorageEngine Engine {
+            get {
+                return _UnitTestEngine ?? _Engine;
+            }
+            set {
+                if (RxApp.InUnitTestRunner()) {
+                    _UnitTestEngine = value;
+                    _Engine = _Engine ?? value;
+                } else {
+                    _Engine = value;
+                }
+            }
+        }
+
+        [ThreadStatic] 
+        static IExtendedStorageEngine _UnitTestEngine;
 
         static RxStorage()
         {
