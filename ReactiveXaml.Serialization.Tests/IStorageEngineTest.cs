8f4e9b11f77ce6785a93501ccd9f24d5b1ece804
Paul Betts <paul@paulbetts.org>
2010-12-21 08:35:46 +0100

    Add smoke test and infrastructure for IStorageEngine

---
+++ ReactiveXaml.Serialization.Tests/IStorageEngineTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Pex.Framework;
+using ReactiveXaml.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace ReactiveXaml.Serialization.Tests
+{
+    [TestClass]
+    public abstract class StorageEngineInterfaceTest
+    {
+        protected abstract IStorageEngine createFixture();
+
+        [TestMethod]
+        public void StorageEngineLoadSaveSmokeTest()
+        {
+            var input = new SubobjectTestObject() {SomeProperty = "Foo"};
+            var fixture = createFixture();
+            SubobjectTestObject result;
+
+            using(fixture.AsPrimaryEngine()) {
+                fixture.Save(input);
+                fixture.FlushChanges();
+
+                Assert.IsTrue(fixture.GetAllObjectHashes().Contains(input.ContentHash));
+
+                result = fixture.Load<SubobjectTestObject>(input.ContentHash);
+                Assert.AreEqual(input.ContentHash, result.ContentHash);
+                Assert.AreEqual(input.SomeProperty, result.SomeProperty);
+            }
+        }
+    }
+
+    [TestClass]
+    public class DictionaryStorageEngineTest : StorageEngineInterfaceTest
+    {
+        protected override IStorageEngine createFixture()
+        {
+            return new DictionaryStorageEngine();
+        }
+    }
+}
+
+// vim: tw=120 ts=4 sw=4 et :
