7591a05b6f1b866a6af1e8204a697a6db2e9c3be
Paul Betts <paul@paulbetts.org>
2010-12-25 23:19:00 +0100

    Add support files for SL4/WP7

---
+++ ReactiveXaml.Serialization/ThreadLocal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ReactiveXaml.Serialization
+{
+    internal class ThreadLocal<T>
+    {
+        Func<T> _factory;
+        Dictionary<int, T> _cache = new Dictionary<int,T>();
+
+        public ThreadLocal() : this(() => default(T))
+        {
+        }
+
+        public ThreadLocal(Func<T> valueFactory)
+        {
+            _factory = valueFactory;
+        }
+
+        public T Value {
+            get {
+                lock(_cache) {
+                    int key = Thread.CurrentThread.ManagedThreadId;
+                    if (_cache.ContainsKey(key)) {
+                        return _cache[key];
+                    }
+
+                    return (_cache[key] = _factory());
+                }
+            }
+        }
+    }
+}
