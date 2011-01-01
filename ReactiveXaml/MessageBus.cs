0596f80b93dae51ca0bb39fe83261eb1e94a587d
Paul Betts <paul@paulbetts.org>
2011-01-01 17:21:49 +0100

    Documentation Station

--- ReactiveXaml/MessageBus.cs
+++ ReactiveXaml/MessageBus.cs
@@ -6,7 +6,14 @@ using System.ComponentModel;
 namespace ReactiveXaml
 {
     /// <summary>
-    /// 
+    /// MessageBus represents an object that can act as a "Message Bus", a
+    /// simple way for ViewModels and other objects to communicate with each
+    /// other in a loosely coupled way.
+    ///
+    /// Specifying which messages go where is done via a combination of the Type
+    /// of the message as well as an additional "Contract" parameter; this is a
+    /// unique string used to distinguish between messages of the same Type, and
+    /// is arbitrarily set by the client. 
     /// </summary>
     public class MessageBus : IMessageBus 
     {
@@ -14,10 +21,13 @@ namespace ReactiveXaml
             new Dictionary<Tuple<Type,string>,WeakReference>();
 
         /// <summary>
-        /// 
+        /// Listen provides an Observable that will fire whenever a Message is
+        /// provided for this object via RegisterMessageSource or SendMessage.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="Contract"></param>
+        /// <typeparam name="T">The type of the message to listen to.</typeparam>
+        /// <param name="contract">A unique string to distinguish messages with
+        /// identical types (i.e. "MyCoolViewModel") - if the message type is
+        /// only used for one purpose, leave this as null.</param>
         /// <returns></returns>
         public IObservable<T> Listen<T>(string Contract = null)
         {
@@ -32,11 +42,13 @@ namespace ReactiveXaml
         }
 
         /// <summary>
-        /// 
+        /// Determins if a particular message Type is registered.
         /// </summary>
-        /// <param name="Type"></param>
-        /// <param name="Contract"></param>
-        /// <returns></returns>
+        /// <typeparam name="T">The type of the message.</typeparam>
+        /// <param name="contract">A unique string to distinguish messages with
+        /// identical types (i.e. "MyCoolViewModel") - if the message type is
+        /// only used for one purpose, leave this as null.</param>
+        /// <returns>True if messages have been posted for this message Type.</returns>
         public bool IsRegistered(Type Type, string Contract = null)
         {
             bool ret = false;
@@ -48,11 +60,16 @@ namespace ReactiveXaml
         }
 
         /// <summary>
-        /// 
+        /// Registers an Observable representing the stream of messages to send.
+        /// Another part of the code can then call Listen to retrieve this
+        /// Observable.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="Source"></param>
-        /// <param name="Contract"></param>
+        /// <typeparam name="T">The type of the message to listen to.</typeparam>
+        /// <param name="source">An Observable that will be subscribed to, and a
+        /// message sent out for each value provided.</param>
+        /// <param name="contract">A unique string to distinguish messages with
+        /// identical types (i.e. "MyCoolViewModel") - if the message type is
+        /// only used for one purpose, leave this as null.</param>
         public void RegisterMessageSource<T>(IObservable<T> Source, string Contract = null)
         {
             withMessageBus(typeof(T), Contract, (mb, tuple) => {
@@ -61,11 +78,16 @@ namespace ReactiveXaml
         }
 
         /// <summary>
-        /// 
+        /// Sends a single message using the specified Type and contract.
+        /// Consider using RegisterMessageSource instead if you will be sending
+        /// messages in response to other changes such as property changes
+        /// or events.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="Message"></param>
-        /// <param name="Contract"></param>
+        /// <typeparam name="T">The type of the message to send.</typeparam>
+        /// <param name="message">The actual message to send</param>
+        /// <param name="contract">A unique string to distinguish messages with
+        /// identical types (i.e. "MyCoolViewModel") - if the message type is
+        /// only used for one purpose, leave this as null.</param>
         public void SendMessage<T>(T Message, string Contract = null)
         {
             withMessageBus(typeof(T), Contract, (mb, tuple) => {
@@ -87,7 +109,7 @@ namespace ReactiveXaml
         }
 
         /// <summary>
-        /// 
+        /// Returns the Current MessageBus from the RxApp global object.
         /// </summary>
         public static IMessageBus Current {
             get { return RxApp.MessageBus; }
@@ -108,48 +130,53 @@ namespace ReactiveXaml
     public static class MessageBusMixins
     {
         /// <summary>
-        /// 
+        /// Registers a ViewModel object to send property change
+        /// messages; this allows a ViewModel to listen to another ViewModel's
+        /// changes in a loosely-typed manner.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="This"></param>
-        /// <param name="source"></param>
-        /// <param name="Contract"></param>
-        /// <exception cref="Exception"><c>Exception</c>.</exception>
-        public static void RegisterViewModel<T>(this IMessageBus This, T source, string Contract = null)
+        /// <param name="source">The ViewModel to register</param>
+        /// <param name="contract">A unique string to distinguish messages with
+        /// identical types (i.e. "MyCoolViewModel") - if the message type is
+        /// only used for one purpose, leave this as null.</param>
+        /// <exception cref="Exception"><c>Exception</c>The registered ViewModel
+        /// must be the only instance (i.e. not in an ItemsControl)</exception>
+        public static void RegisterViewModel<T>(this IMessageBus This, T source, string contract = null)
             where T : IReactiveNotifyPropertyChanged
         {
-            string contractName = viewModelContractName(typeof(T), Contract);
-            if (This.IsRegistered(typeof(ObservedChange<T, Unit>), contractName)) {
+            string contractName = viewModelContractName(typeof(T), contract);
+            if (This.IsRegistered(typeof(ObservedChange<T, object>), contractName)) {
                 throw new Exception(typeof(T).FullName + " must be a singleton class or have a unique contract name");
             }
 
             This.RegisterMessageSource(
-                (source as IObservable<PropertyChangedEventArgs>).Select(x => new ObservedChange<T, Unit>() { Sender = source, PropertyName = x.PropertyName }), 
+                (source as IObservable<PropertyChangedEventArgs>).Select(x => new ObservedChange<T, object>() { Sender = source, PropertyName = x.PropertyName }), 
                 contractName);
 
             This.RegisterMessageSource(
-                 Observable.Defer(() => Observable.Return(source)), viewModelCurrentValueContractName(typeof(T), Contract));
+                 Observable.Defer(() => Observable.Return(source)), viewModelCurrentValueContractName(typeof(T), contract));
         }
 
         /// <summary>
-        /// 
+        /// Listens to a registered ViewModel's property change notifications.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="This"></param>
-        /// <param name="contract"></param>
-        /// <returns></returns>
-        public static IObservable<ObservedChange<T, Unit>> ListenToViewModel<T>(this IMessageBus This, string contract = null)
+        /// <param name="contract">A unique string to distinguish messages with
+        /// identical types (i.e. "MyCoolViewModel") - if the message type is
+        /// only used for one purpose, leave this as null.</param>
+        /// <returns>An Observable that fires when an object changes and
+        /// provides the property name that has changed.</returns>
+        public static IObservable<ObservedChange<T, object>> ListenToViewModel<T>(this IMessageBus This, string contract = null)
         {
-            return This.Listen<ObservedChange<T, Unit>>(viewModelContractName(typeof(T), contract));
+            return This.Listen<ObservedChange<T, object>>(viewModelContractName(typeof(T), contract));
         }
 
         /// <summary>
-        /// 
+        /// Return the current instance of the ViewModel with the specified
+        /// type.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="This"></param>
-        /// <param name="contract"></param>
-        /// <returns></returns>
+        /// <param name="contract">A unique string to distinguish messages with
+        /// identical types (i.e. "MyCoolViewModel") - if the message type is
+        /// only used for one purpose, leave this as null.</param>
+        /// <returns>The ViewModel object registered for this type.</returns>
         public static T ViewModelForType<T>(this IMessageBus This, string contract = null)
         {
             return This.Listen<T>(viewModelCurrentValueContractName(typeof(T), contract)).First();
