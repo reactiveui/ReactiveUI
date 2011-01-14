ede1e78995d1a9f7b26c4122dcbb63f2a6588148
Paul Betts <paul@paulbetts.org>
2011-01-13 20:57:08 -0800

    Commit all the deletes

--- ReactiveXaml.Sample.iOS/AppDelegateIPhone.cs
+++ ReactiveXaml.Sample.iOS/AppDelegateIPhone.cs
@@ -13,21 +13,63 @@ namespace ReactiveXaml.Sample.iOS
 	{
 		public AppDelegateIPhone()
 		{
-			
 		}
 		
 		// This method is invoked when the application has loaded its UI and its ready to run
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
+			RxApp.DeferredScheduler = new NSRunloopScheduler(app);
+		
+			window.AddSubview(navigationController.View);
 			window.MakeKeyAndVisible();
-			var sbDelegate = new UITextFieldDelegateRx();
-			this.searchBox.Delegate = sbDelegate;
-			
-			sbDelegate.OnEditingStarted.Subscribe(_ => {
-				throw new Exception("Die!");
-			});
 			return true;
 		}
 	}
+	
+	public partial class PeopleViewController : UITableViewController
+	{
+		public PeopleViewController(IntPtr _) : base(_) {}
+		public PeopleViewController(NSCoder _) : base(_) {}
+		
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			this.TableView.Source = new PeopleDataSource(this);
+		}
+		
+		class PeopleDataSource : UITableViewSource
+		{
+			UITableViewController _controller;
+			string[] _items;
+			public PeopleDataSource(UITableViewController viewController)
+			{
+				_controller = viewController;
+				_items = new[] {
+					"Foo",
+					"Bar",
+					"Baz",
+					"Bamf",
+				};
+			}
+			
+			public override int NumberOfSections (UITableView tableView)
+			{
+				return 1;
+			}
+			
+			public override int RowsInSection (UITableView tableview, int section)
+			{
+				return _items.Length;
+			}
+			
+			public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
+			{
+				var ret = new UITableViewCell();
+				ret.TextLabel.Text = _items[indexPath.Row];
+				ret.Accessory = UITableViewCellAccessory.DetailDisclosureButton;
+				
+				return ret;	
+			}
+		}
+	}
 }
-
