bae23a479b2c5f9ad7c38f1e83b64c723eb3deae
Paul Betts <paul@paulbetts.org>
2011-01-15 19:43:33 -0800

    Blow away accidentally added file

--- ReactiveUI.Xaml/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyDescription("An MVVM framework that integrates the Reactive Extensions")]
-[assembly: AssemblyProduct("ReactiveUI.Xaml")]
-[assembly: AssemblyVersion("2.0.0.2")]
