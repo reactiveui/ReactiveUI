82d6614c8b305800706bc7475618e92b7ff3a8fb
Paul Betts <paul@paulbetts.org>
2011-02-22 18:38:59 -0800

    Add main window, do some renames

---
+++ ReactiveUI.Sample/ReactiveUI.Sample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ReactiveUI.Sample.Models;
+
+namespace ReactiveUI.Sample.ViewModels
+{
+    public class MainWindowViewModel : ReactiveValidatedObject
+    {
+        public AppModel Model { get; protected set; }
+    }
+}
