7a86c2e1adcc2a2c58a8b2c37cf0b49f3899b873
Paul Betts <paul@paulbetts.org>
2011-02-26 18:15:19 -0800

    Ditto

--- ReactiveUI.Serialization.Tests/ModelBaseTest.cs
+++ ReactiveUI.Serialization.Tests/ModelBaseTest.cs
@@ -31,20 +31,21 @@ namespace ReactiveUI.Serialization.Tests
 
             this.Log().InfoFormat("Setting TestString to [{0}]", String.Join(",", setters));
 
-            var sched = new TestScheduler();
-            var output_changed = new List<object>();
-            var output_changing = new List<object>();
-            var fixture = sched.With(_ => new ModelTestFixture());
+            (new TestScheduler()).With(sched => {
+                var output_changed = new List<object>();
+                var output_changing = new List<object>();
+                var fixture =  new ModelTestFixture();
 
-            fixture.Changing.Subscribe(output_changing.Add);
-            fixture.Changed.Subscribe(output_changed.Add);
+                fixture.Changing.Subscribe(output_changing.Add);
+                fixture.Changed.Subscribe(output_changed.Add);
 
-            setters.Run(x => fixture.TestString = x);
+                setters.Run(x => fixture.TestString = x);
 
-            sched.Run();
+                sched.Run();
 
-            PexAssert.AreEqual(setters.Uniq().Count(), output_changed.Count);
-            PexAssert.AreEqual(setters.Uniq().Count(), output_changing.Count);
+                PexAssert.AreEqual(setters.Uniq().Count(), output_changed.Count);
+                PexAssert.AreEqual(setters.Uniq().Count(), output_changing.Count);
+            });
         }
 
         [PexMethod]
@@ -53,40 +54,43 @@ namespace ReactiveUI.Serialization.Tests
             PexAssume.IsNotNull(setters);
             PexAssume.AreElementsNotNull(setters);
 
-            var sched = new TestScheduler();
-            var fixture = sched.With(_ => new ModelTestFixture());
-            var output = new Dictionary<string, Guid>();
+            (new TestScheduler()).With(sched => {
+                var fixture = new ModelTestFixture();
+                var output = new Dictionary<string, Guid>();
 
-            setters.ToObservable(sched).Subscribe(x => {
-                fixture.TestString = x;
-                output[x] = fixture.ContentHash;
+                setters.ToObservable(sched).Subscribe(x => {
+                    fixture.TestString = x;
+                    output[x] = fixture.ContentHash;
+                });
+
+                sched.Run();
+
+                PexAssert.AreDistinctValues(output.Values.ToArray());
             });
-
-            sched.Run();
-
-            PexAssert.AreDistinctValues(output.Values.ToArray());
         }
 
         [Fact]
         public void ModelBaseShouldBeObservableAfterDeserialization()
         {
             var dse = new DictionaryStorageEngine();
-            var sched = new TestScheduler();
-            var input = sched.With(_ => new ModelTestFixture() {TestString = "Foo"});
 
-            dse.Save(input);
-            var fixture = dse.Load<ModelTestFixture>(input.ContentHash);
+            (new TestScheduler()).With(sched => {
+                var input = new ModelTestFixture() {TestString = "Foo"};
 
-            string latest = null;
-            var changed = fixture.Changed;
-            this.Log().InfoFormat("Subscribing to Changed: 0x{0:X}", changed.GetHashCode());
-            changed.Subscribe(Console.WriteLine);
-            changed.Subscribe(x => latest = x.PropertyName);
-            fixture.TestString = "Bar";
+                dse.Save(input);
+                var fixture = dse.Load<ModelTestFixture>(input.ContentHash);
 
-            sched.RunToMilliseconds(1000);
+                string latest = null;
+                var changed = fixture.Changed;
+                this.Log().InfoFormat("Subscribing to Changed: 0x{0:X}", changed.GetHashCode());
+                changed.Subscribe(Console.WriteLine);
+                changed.Subscribe(x => latest = x.PropertyName);
+                fixture.TestString = "Bar";
 
-            Assert.Equal("TestString", latest);
+                sched.RunToMilliseconds(1000);
+
+                Assert.Equal("TestString", latest);
+            });
         }
     }
 }
