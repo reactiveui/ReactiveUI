7226a2657520629c032b74f5286e8ad5be70fe61
Paul Betts <paul@paulbetts.org>
2011-03-22 12:01:34 -0700

    Move ReactiveUI.Testing to its own folder

---
+++ ReactiveUI.Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyDescription("An MVVM framework that integrates the Reactive Extensions")]
+[assembly: AssemblyProduct("ReactiveUI")]
+[assembly: AssemblyVersion("2.2.0.0")]
+
+[assembly: InternalsVisibleTo("ReactiveUI.Explorables")]
+[assembly: InternalsVisibleTo("ReactiveUI.Xaml")]
+[assembly: InternalsVisibleTo("ReactiveUI.Xaml_35")]
+[assembly: InternalsVisibleTo("ReactiveUI.Xaml_SL4")]
+[assembly: InternalsVisibleTo("ReactiveUI.Xaml_WP7")]
+[assembly: InternalsVisibleTo("ReactiveUI.Tests")]
+[assembly: InternalsVisibleTo("ReactiveUI.Tests_SL4")]
