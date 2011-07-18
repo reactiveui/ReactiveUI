ece3bc6ab963e8c605644560196ebe16c8982c92
Paul Betts <paul@paulbetts.org>
2011-07-17 20:02:45 -0700

    Add initial version of RxUI.Routing

---
+++ ReactiveUI.Routing/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReactiveUI.Routing")]
+[assembly: AssemblyDescription("A ViewModel-based Serializable Routing framework for RxUI")]
+[assembly: AssemblyProduct("ReactiveUI.Routing")]
+
+[assembly: AssemblyVersion("2.3.2.0")]
+[assembly: InternalsVisibleTo("ReactiveUI.Routing.Tests")]
+[assembly: InternalsVisibleTo("ReactiveUI.Routing.Explorables")]
