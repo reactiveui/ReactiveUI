526b861ae235e17d8063c5d514b93eb62ba66140
Paul Betts <paul@paulbetts.org>
2011-11-28 10:42:13 -0800

    Version bump to 2.5.2

--- ReactiveUI.Xaml/Properties/AssemblyInfo.cs
+++ ReactiveUI.Xaml/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyDescription("An MVVM framework that integrates the Reactive Extensions")]
 [assembly: AssemblyProduct("ReactiveUI.Xaml")]
-[assembly: AssemblyVersion("2.5.1.0")]
+[assembly: AssemblyVersion("2.5.2.0")]
