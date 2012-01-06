3a2b31dd5f8be2b37b5edd28a16c68117cad4b66
Paul Betts <paul@paulbetts.org>
2012-01-06 15:55:00 -0800

    Add initial framework for handling user-facing errors
    Conflicts:
    
    	ReactiveUI.Tests/ReactiveUI.Tests.csproj
    	ReactiveUI.Xaml/ReactiveUI.Xaml.csproj

---
+++ ReactiveUI.Tests/ErrorsTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ReactiveUI.Xaml;
+using Xunit;
+
+namespace ReactiveUI.Tests
+{
+    public class ErrorsTest
+    { 
+        [Fact]
+        public void UnhandledUserErrorsShouldDie()
+        {
+            // Since we haven't registered any user error handler
+            Assert.Throws<UnhandledUserErrorException>(() => UserError.Throw("Something Bad Has Happened"));
+        }
+
+        [Fact]
+        public void HandledUserErrorsShouldNotThrow() 
+        {
+            using (UserError.RegisterHandler(x => RecoveryOptionResult.RetryOperation)) 
+            {
+                var result = UserError.Throw("This should catch!");
+                Assert.Equal(RecoveryOptionResult.RetryOperation, result);
+            }
+
+            Assert.Throws<UnhandledUserErrorException>(() => UserError.Throw("This should throw!"));
+        }
+
+        [Fact]
+        public void NestedHandlersShouldFireInANestedWay()
+        {
+            RecoveryOptionResult result;
+            using (UserError.RegisterHandler(x => RecoveryOptionResult.CancelOperation)) {
+
+                using (UserError.RegisterHandler(x => RecoveryOptionResult.RetryOperation)) {
+                    result = UserError.Throw("This should catch!");
+                    Assert.Equal(RecoveryOptionResult.RetryOperation, result);
+                }
+
+                result = UserError.Throw("This should catch!");
+                Assert.Equal(RecoveryOptionResult.CancelOperation, result);
+            }
+
+            Assert.Throws<UnhandledUserErrorException>(() => UserError.Throw("This should throw!"));
+        }
+    }
+}
