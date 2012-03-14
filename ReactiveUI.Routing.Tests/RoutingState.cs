437b7c1da448e6229370509ec238b6b8dcba9f52
Paul Betts <paul@paulbetts.org>
2012-03-14 14:32:09 -0600

    Fix All The Build Errors

--- ReactiveUI.Routing.Tests/RoutingState.cs
+++ ReactiveUI.Routing.Tests/RoutingState.cs
@@ -7,7 +7,7 @@ using Xunit;
 
 namespace ReactiveUI.Routing.Tests
 {
-    public class TestViewModel : ModelBase, IRoutableViewModel
+    public class TestViewModel : ReactiveObject, IRoutableViewModel
     {
         string _SomeProp;
         public string SomeProp {
@@ -28,26 +28,6 @@ namespace ReactiveUI.Routing.Tests
     public class RoutingStateTests
     {
         [Fact]
-        public void RoutingStateSerializableRoundTripTest()
-        {
-            var engine = new DictionaryStorageEngine();
-
-            using(var _ = engine.AsPrimaryEngine()) {
-                var input = new TestViewModel() {SomeProp = "Foo"};
-                var fixture = new RoutingState();
-                fixture.NavigationStack.Add(input);
-
-                RxStorage.Engine.CreateSyncPoint(fixture);
-
-                var output = RxStorage.Engine.Load<RoutingState>(fixture.ContentHash);
-
-                Assert.True(output.NavigationStack.Count == 1);
-                Assert.True(output.NavigationStack[0].ContentHash == input.ContentHash);
-                Assert.Equal(input.SomeProp, ((TestViewModel) output.NavigationStack[0]).SomeProp);
-            }
-        }
-
-        [Fact]
         public void NavigationPushPopTest()
         {
             var input = new TestViewModel() {SomeProp = "Foo"};
@@ -57,36 +37,11 @@ namespace ReactiveUI.Routing.Tests
             fixture.NavigateForward.Execute(input);
 
             Assert.Equal(1, fixture.NavigationStack.Count);
-            Assert.Equal(input.ContentHash, fixture.NavigationStack[0].ContentHash);
             Assert.True(fixture.NavigateBack.CanExecute(null));
 
             fixture.NavigateBack.Execute(null);
 
             Assert.Equal(0, fixture.NavigationStack.Count);
-        }
-
-        [Fact]
-        public void NavigationPushPopSerializationTest()
-        {
-            var engine = new DictionaryStorageEngine();
-
-            using (var _ = engine.AsPrimaryEngine()) {
-                var input = new TestViewModel() {SomeProp = "Foo"};
-                var fixture = new RoutingState();
-                fixture.NavigateForward.Execute(input);
-
-                RxStorage.Engine.CreateSyncPoint(fixture);
-
-                var output = RxStorage.Engine.GetLatestRootObject<RoutingState>();
-
-                Assert.True(output.NavigateBack.CanExecute(null));
-
-                output.NavigateBack.Execute(null);
-                RxStorage.Engine.CreateSyncPoint(output);
-                output = RxStorage.Engine.GetLatestRootObject<RoutingState>();
-
-                Assert.False(output.NavigateBack.CanExecute(null));
-            }
         }
     }
 }
