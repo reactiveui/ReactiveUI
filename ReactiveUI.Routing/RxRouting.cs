07344ead05ba7f9a75861b140915d10c6dd788c2
Paul Betts <paul@paulbetts.org>
2012-04-13 13:53:59 -0700

    More improvements to routing

--- ReactiveUI.Routing/RxRouting.cs
+++ ReactiveUI.Routing/RxRouting.cs
@@ -45,14 +45,53 @@ namespace ReactiveUI.Routing
     {
         /// <summary>
         /// This Observable fires whenever the current ViewModel is navigated to.
+        /// Note that this method is difficult to use directly without leaking
+        /// memory, you most likely want to use WhenNavigatedTo.
         /// </summary>
         public static IObservable<Unit> NavigatedToMe(this IRoutableViewModel This)
         {
-            return Observable.Create<Unit>(subj => {
-                return This.HostScreen.Router.ViewModelObservable()
-                    .Where(x => x == This)
-                    .Select(_ => Unit.Default)
-                    .Subscribe(subj);
+            return This.HostScreen.Router.ViewModelObservable()
+                .Where(x => x == This)
+                .Select(_ => Unit.Default);
+        }
+
+        /// <summary>
+        /// This Observable fires whenever the current ViewModel is navigated
+        /// away from.  Note that this method is difficult to use directly
+        /// without leaking memory, you most likely want to use WhenNavigatedTo.
+        /// </summary>
+        public static IObservable<Unit> NavigatedFromMe(this IRoutableViewModel This)
+        {
+            return This.HostScreen.Router.ViewModelObservable()
+                .Where(x => x != This)
+                .Select(_ => Unit.Default);
+        }
+
+        /// <summary>
+        /// This method allows you to set up connections that only operate
+        /// while the ViewModel has focus, and cleans up when the ViewModel
+        /// loses focus.
+        /// </summary>
+        /// <param name="onNavigatedTo">Called when the ViewModel is navigated
+        /// to - return an IDisposable that cleans up all of the things that are
+        /// configured in the method.</param>
+        /// <returns>An IDisposable that lets you disconnect the entire process
+        /// earlier than normal.</returns>
+        public static IDisposable WhenNavigatedTo(this IRoutableViewModel This, Func<IDisposable> onNavigatedTo)
+        {
+            IDisposable inner = null;
+
+            var router = This.HostScreen.Router;
+            return router.NavigationStack.CollectionCountChanged.Subscribe(_ => {
+                if (router.GetCurrentViewModel() == This) {
+                    if (inner != null)  inner.Dispose();
+                    inner = onNavigatedTo();
+                } else {
+                    if (inner != null) {
+                        inner.Dispose();
+                        inner = null;
+                    }
+                }
             });
         }
     }
