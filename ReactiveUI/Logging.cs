a617ea5669449fc54f6f9923c3b8ac04bd7bd7bf
Paul Betts <paul@paulbetts.org>
2012-04-15 00:18:22 -0700

    Add methods to log Observables

--- ReactiveUI/Logging.cs
+++ ReactiveUI/Logging.cs
@@ -52,6 +52,50 @@ namespace ReactiveUI
             return LogManager.GetLogger(typeof(T).FullName);
         }
     }
+
+    public static class ObservableLoggingMixin
+    {
+        public static IObservable<T> Log<T, TObj>(this IObservable<T> This, 
+            TObj klass, 
+            string message = null,
+            Func<T, string> stringifier = null)
+            where TObj : IEnableLogger
+        {
+            message = message ?? "";
+
+            if (stringifier != null) {
+                return This.Do(
+                    x => klass.Log().Info("{0} OnNext: {1}", stringifier(x)),
+                    ex => klass.Log().WarnException(message + " " + "OnError", ex),
+                    () => klass.Log().Info("{0} OnCompleted", message));
+            } else {
+                return This.Do(
+                    x => klass.Log().Info("{0} OnNext: {1}", x),
+                    ex => klass.Log().WarnException(message + " " + "OnError", ex),
+                    () => klass.Log().Info("{0} OnCompleted", message));
+            }
+        }
+
+        public static IObservable<T> LoggedCatch<T, TObj>(this IObservable<T> This, TObj klass, IObservable<T> next = null, string message = null)
+            where TObj : IEnableLogger
+        {
+            next = next ?? Observable.Return(default(T));
+            return This.Catch<T, Exception>(ex => {
+                klass.Log().WarnException(message ?? "", ex);
+                return next;
+            });
+        }
+
+        public static IObservable<T> LoggedCatch<T, TObj, TException>(this IObservable<T> This, TObj klass, Func<TException, IObservable<T>> next, string message = null)
+            where TObj : IEnableLogger
+            where TException : Exception
+        {
+            return This.Catch<T, TException>(ex => {
+                klass.Log().WarnException(message ?? "", ex);
+                return next(ex);
+            });
+        }
+    }
 }
 
 // vim: tw=120 ts=4 sw=4 et :
