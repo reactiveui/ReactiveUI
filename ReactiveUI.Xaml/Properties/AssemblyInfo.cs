3576e84e2a367c6b183350f9b0bcef383a4e444d
Paul Betts <paul@paulbetts.org>
2012-04-17 18:02:14 -0700

    Fix all the bugs in TransitioningContentControl

--- ReactiveUI.Xaml/Properties/AssemblyInfo.cs
+++ ReactiveUI.Xaml/Properties/AssemblyInfo.cs
@@ -1,7 +1,17 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
+using System.Windows;
 
 [assembly: AssemblyDescription("An MVVM framework that integrates the Reactive Extensions")]
 [assembly: AssemblyProduct("ReactiveUI.Xaml")]
 [assembly: AssemblyVersion("3.0.0.0")]
+
+[assembly: ThemeInfo(
+   ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
+   ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
+)]
