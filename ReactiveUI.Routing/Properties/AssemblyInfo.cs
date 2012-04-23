73049a0f957da6f45ff77f060cb231944598e67c
Paul Betts <paul@paulbetts.org>
2012-04-23 16:04:42 -0700

    Version bump to 3.1

--- ReactiveUI.Routing/Properties/AssemblyInfo.cs
+++ ReactiveUI.Routing/Properties/AssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A ViewModel-based Routing framework for RxUI")]
 [assembly: AssemblyProduct("ReactiveUI.Routing")]
 
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
 [assembly: InternalsVisibleTo("ReactiveUI.Routing.Tests")]
 [assembly: InternalsVisibleTo("ReactiveUI.Routing.Explorables")]
