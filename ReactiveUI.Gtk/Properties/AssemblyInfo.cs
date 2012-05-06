5cd7099e0e3fc8dcb58af895c8a45105d4b3ce50
Paul Betts <paul@paulbetts.org>
2012-05-05 21:38:53 -0700

    Create ReactiveUI.Gtk

---
+++ ReactiveUI.Gtk/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReactiveUI.Gtk")]
+[assembly: AssemblyDescription("Gtk# support for ReactiveUI")]
+[assembly: AssemblyProduct("ReactiveUI.Gtk")]
+
+[assembly: AssemblyVersion("3.1.1.0")]
