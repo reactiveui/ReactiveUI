2026708a340ea78833218103663e9bdefc9af250
Paul Betts <paul@paulbetts.org>
2012-07-05 16:50:56 -0700

    Add additional options for View location
    This patch makes RxRouting.ResolveView resolve several new options:
    
    * IFooBarView that implements IViewForViewModel (using ViewModelToViewFunc)
    * IViewForViewModel<IFooBarViewModel>
    * IViewForViewModel<FooBarViewModel> (the original behavior in RxUI 3.1)

---
+++ ReactiveUI.Routing.Tests/RxRouting.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ReactiveUI;
+using ReactiveUI.Routing;
+using Xunit;
+
+namespace Foobar.ViewModels
+{
+    public interface IFooBarViewModel : IRoutableViewModel {}
+
+    public interface IBazViewModel : IRoutableViewModel {}
+
+    public class FooBarViewModel : ReactiveObject, IFooBarViewModel 
+    {
+        public string UrlPathSegment { get { return "foo"; } }
+        public IScreen HostScreen { get; private set; }
+
+        public FooBarViewModel(IScreen hostScreen)
+        {
+            HostScreen = hostScreen;
+        }
+    }
+
+    public class BazViewModel : ReactiveObject, IBazViewModel 
+    {
+        public string UrlPathSegment { get { return "foo"; } }
+        public IScreen HostScreen { get; private set; }
+
+        public BazViewModel(IScreen hostScreen)
+        {
+            HostScreen = hostScreen;
+        }
+    }
+}
+
+namespace Foobar.Views
+{
+    using ViewModels;
+
+    public class FooBarView : IViewForViewModel<IFooBarViewModel> 
+    {
+        object IViewForViewModel.ViewModel { get { return ViewModel; } set { ViewModel = (IFooBarViewModel) value; } }
+        public IFooBarViewModel ViewModel { get; set; }
+    }
+
+    public interface IBazView : IViewForViewModel<IBazViewModel> {}
+
+    public class BazView : IBazView 
+    {
+        object IViewForViewModel.ViewModel { get { return ViewModel; } set { ViewModel = (IBazViewModel)value; } }
+        public IBazViewModel ViewModel { get; set; }
+    }
+}
+
+namespace ReactiveUI.Routing.Tests
+{
+    using Foobar.Views;
+    using Foobar.ViewModels;
+
+    public class RxRoutingTests : IEnableLogger
+    {
+        [Fact]
+        public void ResolveExplicitViewType()
+        {
+            RxApp.ConfigureServiceLocator(
+                (x, _) => (x.Name == "IBazView" ? new BazView() : null), 
+                (x, _) => null);
+
+            var vm = new BazViewModel(null);
+
+            var result = RxRouting.ResolveView(vm);
+            this.Log().Info(result.GetType().FullName);
+            Assert.True(result is BazView);
+        }
+
+        [Fact]
+        public void ResolvePureInterfaceType() 
+        {
+            RxApp.ConfigureServiceLocator(
+                (x, _) => (x == typeof(IViewForViewModel<IFooBarViewModel>) ? new FooBarView() : null), 
+                (x, _) => null);
+
+            var vm = new FooBarViewModel(null);
+
+            var result = RxRouting.ResolveView(vm);
+            this.Log().Info(result.GetType().FullName);
+            Assert.True(result is FooBarView);
+        }
+    }
+}
