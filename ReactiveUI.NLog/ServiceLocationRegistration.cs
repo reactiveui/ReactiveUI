36a16fd1418cdbe163419db0d94d1d48bce2836c
Paul Betts <paul@paulbetts.org>
2012-08-26 23:12:31 -0700

    Create ReactiveUI.NLog
    This commit creates a new project called ReactiveUI.NLog that recreates the
    NLog adapter to work with RxUI 4.0's logger

---
+++ ReactiveUI.NLog/ServiceLocationRegistration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace ReactiveUI.NLog
+{
+    public class ServiceLocationRegistration : IWantsToRegisterStuff
+    {
+        public void Register()
+        {
+            RxApp.LoggerFactory = type => new NLogLogger(global::NLog.LogManager.GetLogger(type.Name));
+        }
+    }
+}
