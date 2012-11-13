cddc30491f45baabf2bcce6f5125c47aee821505
Paul Betts <paul@paulbetts.org>
2012-11-13 13:23:53 -0800

    Add support for using TypeConverters to resolve view binding type mismatches

--- ReactiveUI/RxApp.cs
+++ ReactiveUI/RxApp.cs
@@ -80,6 +80,7 @@ namespace ReactiveUI
             RxApp.Register(typeof(PropertyBinderImplementation), typeof(IPropertyBinderImplementation));
             RxApp.Register(typeof(NullDefaultPropertyBindingProvider), typeof(IDefaultPropertyBindingProvider));
             RxApp.Register(typeof(EqualityTypeConverter), typeof(IBindingTypeConverter));
+            RxApp.Register(typeof(ComponentModelTypeConverter), typeof(IBindingTypeConverter));
 
             var namespaces = attemptToEarlyLoadReactiveUIDLLs();
 
