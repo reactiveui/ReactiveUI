2827456e46be9d01e8a10be9ecf2013404aa921f
Paul Betts <paul@paulbetts.org>
2012-12-08 21:22:59 -0800

    Fix build file derps

--- ReactiveUI.Mobile/Geolocation.cs
+++ ReactiveUI.Mobile/Geolocation.cs
@@ -10,7 +10,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Geolocation;
 
-namespace ReactiveUI.Mobile_WinRT
+namespace ReactiveUI.Mobile
 {
     public interface IReactiveGeolocator
     {
