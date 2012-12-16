b33a87d5264952602d7bd3753f9728de7d7fa9eb
Paul Betts <paul@paulbetts.org>
2012-12-16 01:10:04 -0800

    Fix the AssemblyInfo

--- ReactiveUI.Mobile/Properties/AssemblyInfo.cs
+++ ReactiveUI.Mobile/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ReactiveUI.Mobile_WinRT")]
+[assembly: AssemblyTitle("ReactiveUI.Mobile")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ReactiveUI.Mobile_WinRT")]
+[assembly: AssemblyProduct("ReactiveUI")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.2.0")]
+[assembly: AssemblyFileVersion("4.2.0")]
 [assembly: ComVisible(false)]
