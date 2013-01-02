2dc134feed9dfb6eab76a18c87d2c81db486bdee
Paul Betts <paul@paulbetts.org>
2013-01-02 13:35:07 -0800

    Implement a binding hook to set the template of any ItemsControls we find

---
+++ ReactiveUI.Routing/ServiceLocationRegistration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveUI.Routing
+{
+    public class ServiceLocationRegistration : IWantsToRegisterStuff
+    {
+        public void Register()
+        {
+            RxApp.Register(typeof(AutoDataTemplateBindingHook), typeof(IPropertyBindingHook));
+        }
+    }
+}
