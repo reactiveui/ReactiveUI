6c8fac3edbb0ca467d42973bc89920979e322470
Paul Betts <paul@paulbetts.org>
2013-02-02 21:31:02 -0800

    Add AkavacheDriver to mobile

---
+++ ReactiveUI.Mobile/AkavacheDriver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using Akavache;
+using Newtonsoft.Json;
+
+namespace ReactiveUI.Mobile
+{
+    public class AkavacheDriver : ISuspensionDriver, IEnableLogger
+    {
+        public AkavacheDriver(string applicationName)
+        {
+            BlobCache.ApplicationName = applicationName;
+            BlobCache.SerializerSettings = new JsonSerializerSettings()
+            {
+                ObjectCreationHandling = ObjectCreationHandling.Replace,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                TypeNameHandling = TypeNameHandling.All,
+            };
+        }
+
+        public IObservable<T> LoadState<T>() where T : class, IApplicationRootState
+        {
+            return BlobCache.UserAccount.GetObjectAsync<T>("__AppState");;;;
+        }
+
+        public IObservable<Unit> SaveState<T>(T state) where T : class, IApplicationRootState
+        {
+            return BlobCache.UserAccount.InsertObject("__AppState", state);
+        }
+
+        public IObservable<Unit> InvalidateState()
+        {
+            BlobCache.UserAccount.InvalidateObject<object>("__AppState");
+            return Observable.Return(Unit.Default);
+        }
+    }
+}
