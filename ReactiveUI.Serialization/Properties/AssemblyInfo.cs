525d3407ca4476dd49d07b10006e781053af8367
Paul Betts <paul@paulbetts.org>
2013-02-07 16:38:54 -0800

    Version bump to 4.4.0

---
+++ ReactiveUI.Serialization/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReactiveUI.Serialization")]
+[assembly: AssemblyDescription("A key-value based serialization library for ReactiveUI")]
+[assembly: AssemblyProduct("ReactiveUI.Serialization")]
+
+[assembly: AssemblyVersion("4.4.0")]
+[assembly: InternalsVisibleTo("ReactiveUI.Serialization.Tests")]
+[assembly: InternalsVisibleTo("ReactiveUI.Serialization.Explorables")]
