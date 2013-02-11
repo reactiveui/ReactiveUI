18ecee376a7296f2fcec6d1f47aeb21bb747a260
Paul Betts <paul@paulbetts.org>
2013-02-11 12:57:13 -0800

    Initial hack at the WinRT version

--- MobileSample-WinRT/App.xaml.cs
+++ MobileSample-WinRT/App.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using MobileSample_WinRT.Common;
-
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using MobileSample_WinRT.ViewModels;
+using ReactiveUI;
+using ReactiveUI.Mobile;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
@@ -16,88 +18,21 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Grid App template is documented at http://go.microsoft.com/fwlink/?LinkId=234226
-
 namespace MobileSample_WinRT
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
-    sealed partial class App : Application
+    sealed partial class App : AutoSuspendApplication
     {
-        /// <summary>
-        /// Initializes the singleton Application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             this.InitializeComponent();
-            this.Suspending += OnSuspending;
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used when the application is launched to open a specific file, to display
-        /// search results, and so forth.
-        /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
         protected override async void OnLaunched(LaunchActivatedEventArgs args)
         {
-            Frame rootFrame = Window.Current.Content as Frame;
+            RxApp.Register(typeof(AppBootstrapper), typeof(IApplicationRootState));
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-                //Associate the frame with a SuspensionManager key                                
-                SuspensionManager.RegisterFrame(rootFrame, "AppFrame");
-
-                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    // Restore the saved session state only when appropriate
-                    try
-                    {
-                        await SuspensionManager.RestoreAsync();
-                    }
-                    catch (SuspensionManagerException)
-                    {
-                        //Something went wrong restoring state.
-                        //Assume there is no state and continue
-                    }
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
-            if (rootFrame.Content == null)
-            {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                if (!rootFrame.Navigate(typeof(GroupedItemsPage), "AllGroups"))
-                {
-                    throw new Exception("Failed to create initial page");
-                }
-            }
-            // Ensure the current window is active
-            Window.Current.Activate();
-        }
-
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private async void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            await SuspensionManager.SaveAsync();
-            deferral.Complete();
+            var host = RxApp.GetService<ISuspensionHost>();
+            host.SetupDefaultSuspendResume();
         }
     }
 }
---
+++ MobileSample-WinRT/ViewModels/TestPage1ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+using ReactiveUI;
+using ReactiveUI.Routing;
+
+namespace MobileSample_WinRT.ViewModels
+{
+    [DataContract]
+    public class TestPage1ViewModel : ReactiveObject, IRoutableViewModel
+    {
+        public string UrlPathSegment { get { return "test1"; } }
+        public IScreen HostScreen { get; private set; }
+
+        Guid _RandomGuid;
+        public Guid RandomGuid {
+            get { return _RandomGuid; }
+            set { this.RaiseAndSetIfChanged(ref _RandomGuid, value); }
+        }
+
+        public TestPage1ViewModel(IScreen screen = null)
+        {
+            HostScreen = screen;
+            RandomGuid = Guid.NewGuid();
+        }
+    }
+}
