84bcd20383003fc94502467cda6441bc7f177f92
Paul Betts <paul@paulbetts.org>
2013-02-12 14:32:06 -0800

    Make the default UI scheduler handle early init time
    Depending on the platform, the UI thread scheduler could be initialized fairly
    late in process startup. This makes trying to do things like set up service
    location difficult, since the static RxApp ctor kicks in and then immediately
    blows up trying to find Dispatcher.Current.
    
    Instead, keep trying to switch to the UI scheduler and if it fails, use
    CurrentThread

---
+++ ReactiveUI.Xaml/WaitForDispatcherScheduler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Text;
+
+namespace ReactiveUI.Xaml
+{
+    public class WaitForDispatcherScheduler : IScheduler
+    {
+        IScheduler _innerScheduler;
+        readonly Func<IScheduler> _schedulerFactory;
+
+        public WaitForDispatcherScheduler(Func<IScheduler> schedulerFactory)
+        {
+            _schedulerFactory = schedulerFactory;
+        }
+
+        public IDisposable Schedule<TState>(TState state, Func<IScheduler, TState, IDisposable> action)
+        {
+            return attemptToCreateScheduler().Schedule(state, action);
+        }
+
+        public IDisposable Schedule<TState>(TState state, TimeSpan dueTime, Func<IScheduler, TState, IDisposable> action)
+        {
+            return attemptToCreateScheduler().Schedule(state, dueTime, action);
+        }
+
+        public IDisposable Schedule<TState>(TState state, DateTimeOffset dueTime, Func<IScheduler, TState, IDisposable> action)
+        {
+            return attemptToCreateScheduler().Schedule(state, dueTime, action);
+        }
+
+        public DateTimeOffset Now {
+            get { return attemptToCreateScheduler().Now; }
+        }
+
+        IScheduler attemptToCreateScheduler()
+        {
+            if (_innerScheduler != null) return _innerScheduler;
+            try {
+                _innerScheduler = _schedulerFactory();
+                return _innerScheduler;
+            } catch (Exception) {
+                // NB: Dispatcher's not ready yet. Keep using CurrentThread
+                return Scheduler.CurrentThread;
+            }
+        }
+    }
+}
