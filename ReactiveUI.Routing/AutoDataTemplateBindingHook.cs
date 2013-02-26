e1ad0ecbdfbde912959734b516d249aa92795707
Paul Betts <paul@paulbetts.org>
2013-02-26 00:14:48 -0800

    NInject hates public static read-write properties

--- ReactiveUI.Routing/AutoDataTemplateBindingHook.cs
+++ ReactiveUI.Routing/AutoDataTemplateBindingHook.cs
@@ -41,14 +41,11 @@ namespace ReactiveUI.Routing
                 template.Replace("__ASSEMBLYNAME__", assemblyName));           
         });
 
-        public static bool Disable { get; set; }
-
-
         public bool ExecuteHook(object source, object target, Func<IObservedChange<object, object>[]> getCurrentViewModelProperties, Func<IObservedChange<object, object>[]> getCurrentViewProperties, BindingDirection direction)
         {
             // NB: If ReactiveUI.Routing is registered but they're not actually 
             // using it, we don't want to help them out here.
-            if (!RxApp.IsServiceLocationConfigured() || Disable) return true;
+            if (!RxApp.IsServiceLocationConfigured()) return true;
 
             var viewProperties = getCurrentViewProperties();
 
