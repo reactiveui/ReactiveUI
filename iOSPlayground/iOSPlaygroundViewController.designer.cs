c0ea2a04ea115feb72aca3eec2b50e889dafbdb2
Paul Betts <paul@paulbetts.org>
2013-03-08 17:46:36 -0800

    Add an iOS test app

---
+++ iOSPlayground/iOSPlaygroundViewController.designer.cs
@@ -0,0 +1,17 @@
+//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+
+namespace iOSPlayground
+{
+    [Register ("iOSPlaygroundViewController")]
+    partial class iOSPlaygroundViewController
+    {
+        void ReleaseDesignerOutlets()
+        {
+        }
+    }
+}
