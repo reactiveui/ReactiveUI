bb94f6101f9d3e7bf63ec2aef4ddf26cc890b610
Paul Betts <paul@paulbetts.org>
2013-03-11 16:27:51 -0700

    Project Fixups

---
+++ ReactiveUI.Android/ServiceLocationRegistration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace ReactiveUI.Android
+{
+    /// <summary>
+    /// Ignore me. This class is a secret handshake between RxUI and RxUI.Xaml
+    /// in order to register certain classes on startup that would be difficult
+    /// to register otherwise.
+    /// </summary>
+    public class ServiceLocationRegistration : IWantsToRegisterStuff
+    {
+        public void Register()
+        {
+        }
+    }
+}
