ac09552730c2cea2358ab9e658bda1c121eda8cc
Paul Betts <paul@paulbetts.org>
2013-03-11 17:23:13 -0700

    Fix version number on Android binaries

---
+++ ReactiveUI.Android/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyDescription("An MVVM framework that integrates the Reactive Extensions")]
+[assembly: AssemblyProduct("ReactiveUI.Android")]
+[assembly: AssemblyVersion("4.5.0")]
