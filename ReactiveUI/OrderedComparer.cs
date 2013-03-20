ce80be16d8adeb5abdb39e369a223f6ae656d015
Markus Olsson <j.markus.olsson@gmail.com>
2013-03-20 14:28:58 +0100

    Type inferred starting point for anonymous types
    Create comparers by type inferring them from generic IEnumerables.

--- ReactiveUI/OrderedComparer.cs
+++ ReactiveUI/OrderedComparer.cs
@@ -4,6 +4,106 @@ using System.Collections.Generic;
 namespace ReactiveUI
 {
     /// <summary>
+    /// Convienience interface for providing a starting point for chaining comparers.
+    /// </summary>
+    public interface IComparerBuilder<T>
+    {
+        /// <summary>
+        /// Creates a derived comparer based on the given parent comparer. The returned comparer will sort elements
+        /// using the parent comparer first. If the parent considers the values equal elements will be sorted
+        /// in ascending order based on the values returned by the provided selector. The selector values will be
+        /// compared using the default comparer for the return type of the selector.
+        /// </summary>
+        /// <param name="selector">A function supplying the values for the comparator.</param>
+        IComparer<T> OrderBy<TValue>(Func<T, TValue> selector);
+
+        /// <summary>
+        /// Creates a derived comparer based on the given parent comparer. The returned comparer will sort elements
+        /// using the parent comparer first. If the parent considers the values equal elements will be sorted
+        /// in ascending order based on the values returned by the provided selector. The selector values will be
+        /// compared using the provided comparer or the default comparer for the return type of the selector if no
+        /// comparer is specified.
+        /// </summary>
+        /// <param name="selector">A function supplying the values for the comparator.</param>
+        IComparer<T> OrderBy<TValue>(Func<T, TValue> selector, IComparer<TValue> comparer);
+
+        /// <summary>
+        /// Creates a derived comparer based on the given parent comparer. The returned comparer will sort elements
+        /// using the parent comparer first. If the parent considers the values equal elements will be sorted
+        /// in descending order based on the values returned by the provided selector. The selector values will be
+        /// compared using the default comparer for the return type of the selector.
+        /// </summary>
+        /// <param name="selector">A function supplying the values for the comparator.</param>
+        IComparer<T> OrderByDescending<TValue>(Func<T, TValue> selector);
+
+        /// <summary>
+        /// Creates a derived comparer based on the given parent comparer. The returned comparer will sort elements
+        /// using the parent comparer first. If the parent considers the values equal elements will be sorted
+        /// in descending order based on the values returned by the provided selector. The selector values will be
+        /// compared using the provided comparer or the default comparer for the return type of the selector if no
+        /// comparer is specified.
+        /// </summary>
+        /// <param name="selector">A function supplying the values for the comparator.</param>
+        IComparer<T> OrderByDescending<TValue>(Func<T, TValue> selector, IComparer<TValue> comparer);
+    }
+
+    /// <summary>
+    /// Convienience class providing a starting point for chaining comparers for anonymous types.
+    /// </summary>
+    /// <remarks>
+    /// If the type you're creating a comparer for is known this class is nothing more than an alias for the generic
+    /// OrderedComparer. This class can be used to create comparers for anonymous types
+    /// </remarks>
+    public static class OrderedComparer
+    {
+        private sealed class OrderedComparerTypeWrapper<T> : IComparerBuilder<T>
+        {
+            public static readonly OrderedComparerTypeWrapper<T> Instance = new OrderedComparerTypeWrapper<T>();
+
+            public IComparer<T> OrderBy<TValue>(Func<T, TValue> selector)
+            {
+                return OrderedComparer<T>.OrderBy(selector);
+            }
+
+            public IComparer<T> OrderBy<TValue>(Func<T, TValue> selector, IComparer<TValue> comparer)
+            {
+                return OrderedComparer<T>.OrderBy(selector, comparer);
+            }
+
+            public IComparer<T> OrderByDescending<TValue>(Func<T, TValue> selector)
+            {
+                return OrderedComparer<T>.OrderByDescending(selector);
+            }
+
+            public IComparer<T> OrderByDescending<TValue>(Func<T, TValue> selector, IComparer<TValue> comparer)
+            {
+                return OrderedComparer<T>.OrderByDescending(selector, comparer);
+            }
+        }
+
+        /// <summary>
+        /// Creates a type inferred comparer builder for the element type of the enumerable. Useful for creating
+        /// comparers for anonymous types. Note that the builder is not a comparer in itself, you need to use the
+        /// OrderBy or OrderByDescending methods on the builder to get an actual comparer.
+        /// </summary>
+        public static IComparerBuilder<T> For<T>(IEnumerable<T> enumerable)
+        {
+            return For<T>();
+        }
+
+        /// <summary>
+        /// Creates a comparer builder for the specified type. Note that the builder is not a comparer in itself,
+        /// you need to use the OrderBy or OrderByDescending methods on the builder to get an actual comparer.
+        /// If the type is known at compile time this method is nothing more than an alias for the generic
+        /// OrdedComparer class.
+        /// </summary>
+        public static IComparerBuilder<T> For<T>()
+        {
+            return OrderedComparerTypeWrapper<T>.Instance;
+        }
+    }
+
+    /// <summary>
     /// Convienience class providing a starting point for chaining comparers.
     /// </summary>
     /// <typeparam name="T"></typeparam>
