00cc98adaa17cdd9d1a7b5d04d7ce2b678b1016b
Paul Betts <paul@paulbetts.org>
2013-03-19 21:49:34 -0700

    First hack at getting suspend/resume running

--- iOSPlayground/iOSPlaygroundViewController.cs
+++ iOSPlayground/iOSPlaygroundViewController.cs
@@ -4,11 +4,32 @@ using System.Reactive.Concurrency;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using ReactiveUI;
+using ReactiveUI.Routing;
+using System.ComponentModel;
 
 namespace iOSPlayground
 {
-    public partial class iOSPlaygroundViewController : UIViewController
+    public partial class iOSPlaygroundViewController : UIViewController, IViewFor<iOSPlaygroundViewModel>, INotifyPropertyChanged
     {
+        #region Boring copy-paste code I want to die
+        iOSPlaygroundViewModel _ViewModel;
+        public iOSPlaygroundViewModel ViewModel {
+            get { return _ViewModel; }
+            set {
+                if (_ViewModel == value) return;
+                _ViewModel = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("ViewModel"));
+            }
+        }
+        
+        object IViewFor.ViewModel {
+            get { return ViewModel; }
+            set { ViewModel = (iOSPlaygroundViewModel)value; }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
         public iOSPlaygroundViewController() : base ("iOSPlaygroundViewController", null)
         {
         }
@@ -31,6 +52,20 @@ namespace iOSPlayground
         {
             // Return true for supported orientations
             return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+        }
+    }
+
+    public class iOSPlaygroundViewModel : ReactiveObject, IRoutableViewModel
+    {
+        public string UrlPathSegment {
+            get { return "Initial View"; }
+        }
+
+        public IScreen HostScreen { get; protected set; }
+
+        public iOSPlaygroundViewModel(IScreen hostScreen)
+        {
+            HostScreen = hostScreen ?? RxApp.GetService<IScreen>();
         }
     }
 }
