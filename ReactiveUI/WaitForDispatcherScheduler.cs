0487340d9cf1221cac6278ec8e5161d01698a4fd
Paul Betts <paul@paulbetts.org>
2013-03-27 11:35:22 -0700

    Move more things out of ReactiveUI.Xaml and into ReactiveUI proper

--- ReactiveUI/WaitForDispatcherScheduler.cs
+++ ReactiveUI/WaitForDispatcherScheduler.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Reactive.Concurrency;
 using System.Text;
 
-namespace ReactiveUI.Xaml
+namespace ReactiveUI
 {
     public class WaitForDispatcherScheduler : IScheduler
     {
