6f133ff127f60604c2c14ce67d95efc52db49f4f
Paul Betts <paul@paulbetts.org>
2013-03-30 15:47:46 -0700

    Try to wire up AndroidPlayground to use auto-suspend and activity-based routing

--- AndroidPlayground/AppBootstrapper.cs
+++ AndroidPlayground/AppBootstrapper.cs
@@ -1,11 +1,20 @@
 using System;
+using ReactiveUI.Routing;
+using ReactiveUI.Mobile;
 
 namespace AndroidPlayground
 {
-    public class AppBootstrapper
+    public class AppBootstrapper : IApplicationRootState
     {
-        protected AppBootstrapper()
+        public IRoutingState Router { get; protected set; }
+
+        public AppBootstrapper()
         {
+            Router = new RoutingState();
+            Router.Navigate.Execute(new MainViewModel(this));
+
+            App.Current.Locator.Register(() => this, typeof(IScreen));
+            App.Current.Locator.Register(() => this, typeof(IApplicationRootState));
         }
     }
 }
