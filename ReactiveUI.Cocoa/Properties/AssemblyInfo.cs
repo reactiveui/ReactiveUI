3b0bdcf16669cc83127554c91f920869867e03c9
Paul Betts <paul@paulbetts.org>
2013-04-04 13:26:38 -0700

    Version bump to 5.0.0

--- ReactiveUI.Cocoa/Properties/AssemblyInfo.cs
+++ ReactiveUI.Cocoa/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyDescription("An MVVM framework that integrates the Reactive Extensions")]
 [assembly: AssemblyProduct("ReactiveUI.Cocoa")]
-[assembly: AssemblyVersion("4.5.0")]
+[assembly: AssemblyVersion("5.0.0")]
