d00483de87eebd6184c250f16ea0065b19d7e289
Johan Laanstra <jlaanstra1221@hotmail.com>
2013-04-07 16:54:39 -0700

    Make UnitTestDetector and DesignModeDetector remember value.

--- ReactiveUI/DesignModeDetector.cs
+++ ReactiveUI/DesignModeDetector.cs
@@ -9,42 +9,54 @@ namespace ReactiveUI
 {
     public class DesignModeDetector
     {
+        /// <summary>
+        /// Private constructor to prevent instantiation.
+        /// </summary>
+        private DesignModeDetector() { }
+
+        static bool? isInDesignMode;
+
+        /// <summary>
+        /// Determines is this application is currently running in a designer.
+        /// </summary>
+        /// <returns>true if in designer; otherwise false</returns>
         public static bool IsInDesignMode()
         {
-            // Check Silverlight Design Mode
-            var dm = Type.GetType("System.ComponentModel.DesignerProperties, System.Windows, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", false);
-            if (dm != null)
+            if (!isInDesignMode.HasValue)
             {
-                MethodInfo mInfo = dm.GetMethod("GetIsInDesignMode");
-                Type dependencyObject = Type.GetType("System.Windows.DependencyObject, System.Windows, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", false);
-                if (dependencyObject != null)
+                // Check Silverlight Design Mode
+                var type = Type.GetType("System.ComponentModel.DesignerProperties, System.Windows, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", false);
+                if (type != null)
                 {
-                    return (bool)mInfo.Invoke(null, new object[] { Activator.CreateInstance(dependencyObject) });
+                    MethodInfo mInfo = type.GetMethod("GetIsInDesignMode");
+                    Type dependencyObject = Type.GetType("System.Windows.DependencyObject, System.Windows, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", false);
+                    if (dependencyObject != null)
+                    {
+                        isInDesignMode = (bool)mInfo.Invoke(null, new object[] { Activator.CreateInstance(dependencyObject) });
+                    }
                 }
-                return false;
-            }
-
-            // Check .NET 
-            var cmdm = Type.GetType("System.ComponentModel.DesignerProperties, PresentationFramework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35", false);
-            if (cmdm != null) // loaded the assembly, could be .net 
-            {
-                MethodInfo mInfo = cmdm.GetMethod("GetIsInDesignMode");
-                Type dependencyObject = Type.GetType("System.Windows.DependencyObject, WindowsBase, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35", false);
-                if(dependencyObject != null)
+                else if((type = Type.GetType("System.ComponentModel.DesignerProperties, PresentationFramework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35", false)) != null)
                 {
-                    return (bool)mInfo.Invoke(null, new object[] { Activator.CreateInstance(dependencyObject) });
+                    // loaded the assembly, could be .net 
+                    MethodInfo mInfo = type.GetMethod("GetIsInDesignMode");
+                    Type dependencyObject = Type.GetType("System.Windows.DependencyObject, WindowsBase, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35", false);
+                    if (dependencyObject != null)
+                    {
+                        isInDesignMode = (bool)mInfo.Invoke(null, new object[] { Activator.CreateInstance(dependencyObject) });
+                    }
                 }
-                return false;
+                else if ((type = Type.GetType("Windows.ApplicationModel.DesignMode, Windows, ContentType=WindowsRuntime", false)) != null)
+                {
+                    // check WinRT next
+                    isInDesignMode = (bool)type.GetProperty("IsDesignModeEnabled").GetMethod.Invoke(null, null);
+                }
+                else
+                {
+                    isInDesignMode = false;
+                }
             }
 
-            // check WinRT next
-            var wadm = Type.GetType("Windows.ApplicationModel.DesignMode, Windows, ContentType=WindowsRuntime", false);
-            if (wadm != null)
-            {
-                return (bool)cmdm.GetProperty("IsDesignModeEnabled").GetMethod.Invoke(null, null);
-            }
-
-            return false;
+            return isInDesignMode.GetValueOrDefault(false);
         }
     }
 }
