a3304ebf8a750f688d5a468368aa4f4aed4b9e40
Paul Betts <paul@paulbetts.org>
2013-04-15 07:08:20 -0700

    Write an app to render events to a C# file

--- MobileSample-WinRT/App.xaml.cs
+++ MobileSample-WinRT/App.xaml.cs
@@ -17,6 +17,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Reactive.Linq;
 
 namespace MobileSample_WinRT
 {
@@ -34,6 +35,28 @@ namespace MobileSample_WinRT
             base.OnLaunched(args);
             var host = RxApp.DependencyResolver.GetService<ISuspensionHost>();
             host.SetupDefaultSuspendResume();
+        }
+    }
+
+    public static class ButtonMixin
+    {
+        public static ButtonEvents Events(this Button This)
+        {
+            return new ButtonEvents(This);
+        }
+    }
+
+    public class ButtonEvents
+    {
+        Button This;
+
+        public ButtonEvents(Button This)
+        {
+            this.This = This;
+        }
+
+        public IObservable<RoutedEventArgs> Click {
+            get { return Observable.FromEventPattern<RoutedEventHandler, RoutedEventArgs>(x => This.Click += x, x => This.Click -= x).Select(x => x.EventArgs); }
         }
     }
 }
