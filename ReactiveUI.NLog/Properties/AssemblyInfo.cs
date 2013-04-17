febcda3d7a7974a6e13dabee01cb0fa065c3c618
Paul Betts <paul@paulbetts.org>
2013-04-16 17:33:20 -0700

    Version bump to 4.6.0

--- ReactiveUI.NLog/Properties/AssemblyInfo.cs
+++ ReactiveUI.NLog/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyDescription("NLog Adapter for ReactiveUI Logging")]
 [assembly: AssemblyProduct("ReactiveUI")]
-[assembly: AssemblyVersion("4.5.0")]
+[assembly: AssemblyVersion("4.6.0")]
