0efa2cff4aff1495cbbd0dbb94bcd4e2c0938eab
Paul Betts <paul@paulbetts.org>
2013-05-13 13:27:46 -0700

    Version bump to 4.6.2

--- ReactiveUI.Cocoa/Properties/AssemblyInfo.cs
+++ ReactiveUI.Cocoa/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyDescription("An MVVM framework that integrates the Reactive Extensions")]
 [assembly: AssemblyProduct("ReactiveUI.Cocoa")]
-[assembly: AssemblyVersion("4.6.1")]
+[assembly: AssemblyVersion("4.6.2")]
