747df00e15e04c48506d4bf7e8a9489022a2b7f9
Oliver Weichhold <oliver@weichhold.com>
2013-05-25 18:53:07 +0200

    - Added non KVO dependent iOS specific, extendable ICreatesObservableForProperty implementation supporting some core UIKit controls - Added MonoTouch Linker XML definition file that should be added to MonoTouch projects in release configuration. Otherwise the Monotouch Linker might optimize away properties referenced soley through ReactiveUI (reflection-driven) data-binding

---
+++ ReactiveUI.Platforms/Cocoa/UIKitObservableForProperty.cs
@@ -0,0 +1,37 @@
+using System;
+using ReactiveUI;
+using System.Collections.Generic;
+using MonoTouch.UIKit;
+using System.Linq;
+using MonoTouch.Foundation;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices;
+using System.Reactive.Disposables;
+
+namespace ReactiveUI.Cocoa
+{
+	public class UIKitObservableForProperty : UIKitObservableForPropertyBase
+	{
+		public UIKitObservableForProperty ()
+		{
+			Register(typeof(UIControl), "Value", 9, (s, p)=> ObservableFromUIControlEvent(s, p, UIControlEvent.ValueChanged));
+
+			Register(typeof(UITextField), "Text", 10, (s, p) => ObservableFromNotification(s, p, UITextField.TextFieldTextDidChangeNotification));
+
+			Register(typeof(UIDatePicker), "Date",10,  (s, p)=> ObservableFromUIControlEvent(s, p, UIControlEvent.ValueChanged));
+
+			Register(typeof(UISegmentedControl), "SelectedSegment", 10, (s, p)=> ObservableFromUIControlEvent(s, p, UIControlEvent.ValueChanged));
+
+			Register(typeof(UISwitch), "On", 10, (s, p)=> ObservableFromUIControlEvent(s, p, UIControlEvent.ValueChanged));
+
+			Register(typeof(UISegmentedControl), "SelectedSegment", 10, (s, p)=> ObservableFromUIControlEvent(s, p, UIControlEvent.ValueChanged));
+
+			// Warning: This will stomp the Control's delegate
+			Register(typeof(UITabBar), "SelectedItem", 10, (s, p) => ObservableFromEvent(s, p, "ItemSelected"));
+
+			// Warning: This will stomp the Control's delegate
+			Register(typeof(UISearchBar), "Text", 10, (s, p) => ObservableFromEvent(s, p, "TextChanged"));
+		}
+	}
+}
+
