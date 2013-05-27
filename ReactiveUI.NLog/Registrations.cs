7f355cb703bac961f473c594892375be18b2467a
Paul Betts <paul@paulbetts.org>
2013-05-26 23:12:21 -0700

    Wire up NLog

--- ReactiveUI.NLog/Registrations.cs
+++ ReactiveUI.NLog/Registrations.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NLog;
+using ReactiveUI;
 
 namespace ReactiveUI.NLog
 {
+    class NLogLogManager : ILogManager
+    {
+        public IFullLogger GetLogger(Type type)
+        {
+            return new NLogLogger(global::NLog.LogManager.GetLogger(type.Name));
+        }
+    }
+
     public class Registrations : IWantsToRegisterStuff
     {
         public void Register(Action<Func<object>, Type> registerFunction)
         {
-            RxApp.LoggerFactory = type => new NLogLogger(global::NLog.LogManager.GetLogger(type.Name));
+            var nlogManager = new NLogLogManager();
+            registerFunction(() => nlogManager, typeof(ILogManager));
         }
     }
 }
