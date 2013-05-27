7b94efb2e19eb972ee60b624d6739b584bab61d7
Paul Betts <paul@paulbetts.org>
2013-05-27 00:36:36 -0700

    Grumble style grumble

--- ReactiveUI.Platforms/ComponentModelTypeConverter.cs
+++ ReactiveUI.Platforms/ComponentModelTypeConverter.cs
@@ -10,8 +10,7 @@ namespace ReactiveUI
             // NB: String is a Magical Type(tm) to TypeConverters. If we are
             // converting from string => int, we need the Int converter, not
             // the string converter :-/
-            if (types.Item1 == typeof(string))
-            {
+            if (types.Item1 == typeof(string)) {
                 types = Tuple.Create(types.Item2, types.Item1);
             }
 
@@ -27,8 +26,7 @@ namespace ReactiveUI
 
         public bool TryConvert(object from, Type toType, object conversionHint, out object result)
         {
-            if (from == null)
-            {
+            if (from == null) {
                 result = null;
                 return true;
             }
@@ -36,37 +34,29 @@ namespace ReactiveUI
             var fromType = from.GetType();
             var converter = typeConverterCache.Get(Tuple.Create(fromType, toType));
 
-            if (converter == null)
-            {
+            if (converter == null) {
                 throw new ArgumentException(String.Format("Can't convert {0} to {1}. To fix this, register a IBindingTypeConverter", fromType, toType));
             }
 
-            try
-            {
+            try {
                 // TODO: This should use conversionHint to determine whether this is locale-aware or not
                 result = (fromType == typeof(string)) ?
                     converter.ConvertFrom(from) : converter.ConvertTo(from, toType);
+
                 return true;
-            }
-            catch (FormatException)
-            {
+            } catch (FormatException) {
                 result = null;
                 return false;
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 // Errors from ConvertFrom end up here but wrapped in 
                 // outer exception. Add more types here as required.
                 // IndexOutOfRangeException is given when trying to
                 // convert empty strings with some/all? converters
                 if (e.InnerException is IndexOutOfRangeException ||
-                    e.InnerException is FormatException)
-                {
+                    e.InnerException is FormatException) {
                     result = null;
                     return false;
-                }
-                else
-                {
+                } else {
                     throw;
                 }
             }
