99e043fc467996515748491835e16f0f0368b0fe
Paul Betts <paul@paulbetts.org>
2013-06-11 22:22:05 -0400

    Get tests mostly running on iOS

--- ReactiveUI.Tests/ReactiveCommandTest.cs
+++ ReactiveUI.Tests/ReactiveCommandTest.cs
@@ -7,17 +7,9 @@ using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Reactive.Testing;
 using ReactiveUI.Testing;
-
 using Microsoft.Reactive.Testing;
-using ReactiveUI.Xaml;
 using Xunit;
-#if MONO
-using Mono.Reactive.Testing;
-#else
-
-#endif
 
 namespace ReactiveUI.Tests
 {
