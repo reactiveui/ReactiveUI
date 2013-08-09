56e64f0df0b56342225023b40cd2dab216fc87df
Paul Betts <paul@github.com>
2013-08-09 11:28:38 +0200

    Version bump to 5.1.0

--- ReactiveUI.Platforms/Cocoa/Properties/AssemblyInfo.cs
+++ ReactiveUI.Platforms/Cocoa/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyDescription("An MVVM framework that integrates the Reactive Extensions")]
 [assembly: AssemblyProduct("ReactiveUI.Cocoa")]
-[assembly: AssemblyVersion("5.0.1")]
+[assembly: AssemblyVersion("5.1.0")]
 
 [assembly: InternalsVisibleTo("ReactiveUI_Tests_Monotouch")]
