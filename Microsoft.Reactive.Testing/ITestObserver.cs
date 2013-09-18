eafe2738e1ebcfd871912bff529b1b6c6c78effc
Paul Betts <paul@paulbetts.org>
2013-09-18 14:30:33 -0700

    Build our own version of Microsoft.Reactive.Testing
    Because the official Mono Rx is strong named and we don't have the key,
    yet *don't* ship Microsoft.Reactive.Testing, we need to build our own.

---
+++ Microsoft.Reactive.Testing/ITestObserver.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+
+namespace Microsoft.Reactive.Testing
+{
+    /// <summary>
+    /// Observer that records received notification messages and timestamps those.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the sequence.</typeparam>
+    public interface ITestableObserver<T> : IObserver<T>
+    {
+        /// <summary>
+        /// Gets recorded timestamped notification messages received by the observer.
+        /// </summary>
+        IList<Recorded<Notification<T>>> Messages { get; }
+    }
+}
---
+++ Microsoft.Reactive.Testing/MockObserver.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Reactive;
+using System;
+
+namespace Microsoft.Reactive.Testing
+{
+    class MockObserver<T> : ITestableObserver<T>
+    {
+        TestScheduler scheduler;
+        List<Recorded<Notification<T>>> messages;
+
+        public MockObserver(TestScheduler scheduler)
+        {
+            if (scheduler == null)
+                throw new ArgumentNullException("scheduler");
+
+            this.scheduler = scheduler;
+            this.messages = new List<Recorded<Notification<T>>>();
+        }
+
+        public void OnNext(T value)
+        {
+            messages.Add(new Recorded<Notification<T>>(scheduler.Clock, Notification.CreateOnNext<T>(value)));
+        }
+
+        public void OnError(Exception exception)
+        {
+            messages.Add(new Recorded<Notification<T>>(scheduler.Clock, Notification.CreateOnError<T>(exception)));
+        }
+
+        public void OnCompleted()
+        {
+            messages.Add(new Recorded<Notification<T>>(scheduler.Clock, Notification.CreateOnCompleted<T>()));
+        }
+
+        public IList<Recorded<Notification<T>>> Messages
+        {
+            get { return messages; }
+        }
+    }
+}
