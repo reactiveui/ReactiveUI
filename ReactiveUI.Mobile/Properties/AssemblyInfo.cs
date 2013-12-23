bdae6a8edbc68978bce850cb415271769403b0dd
Oren Novotny <oren@novotny.org>
2013-12-22 23:36:51 -0500

    Move version numbers into CommonAssemblyInfo.cs file as per #453

--- ReactiveUI.Mobile/Properties/AssemblyInfo.cs
+++ ReactiveUI.Mobile/Properties/AssemblyInfo.cs
@@ -24,6 +24,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.4.0")]
-[assembly: AssemblyFileVersion("5.4.0")]
+
 [assembly: ComVisible(false)]
