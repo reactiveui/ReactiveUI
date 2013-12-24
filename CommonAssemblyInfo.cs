c62769d41f305ccf5ace9f7435543cd410068425
Paul Betts <paul@paulbetts.org>
2013-12-24 14:49:48 -0500

    Version Bump to 5.99.0

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("5.4.0")]
-[assembly: AssemblyFileVersion("5.4.0")]
+[assembly: AssemblyVersion("5.99.0")]
+[assembly: AssemblyFileVersion("5.99.0")]
