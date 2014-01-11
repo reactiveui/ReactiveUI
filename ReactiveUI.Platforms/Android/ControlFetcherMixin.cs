5ecee58635d9bfd7d13e5f35aecbc1ab515a48d6
Paul Betts <paul@paulbetts.org>
2014-01-11 13:31:06 -0800

    ViewHost => LayoutViewHost

--- ReactiveUI.Platforms/Android/ControlFetcherMixin.cs
+++ ReactiveUI.Platforms/Android/ControlFetcherMixin.cs
@@ -46,7 +46,7 @@ namespace ReactiveUI.Android
             return GetControl<T>(This.View, propertyName);
         }
 
-        public static void WireUpControls(this IViewHost This)
+        public static void WireUpControls(this ILayoutViewHost This)
         {
             // Auto wire-up
             // Get all the View properties from the activity
