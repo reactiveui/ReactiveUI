bbc0da9d24f8682f5a24dec59de08ec27ceb5991
Paul Betts <paul@paulbetts.org>
2014-02-05 23:01:23 -0800

    Fix QuickUI registrations

---
+++ ReactiveUI.Platforms/QuickUI/Registrations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using ReactiveUI;
+using System.Reactive.Concurrency;
+
+namespace ReactiveUI.QuickUI
+{
+    /// <summary>
+    /// Ignore me. This class is a secret handshake between RxUI and RxUI.Xaml
+    /// in order to register certain classes on startup that would be difficult
+    /// to register otherwise.
+    /// </summary>
+    public class Registrations : IWantsToRegisterStuff
+    {
+        public void Register(Action<Func<object>, Type> registerFunction)
+        {
+            //TODO
+            //registerFunction(() => new PlatformOperations(), typeof(IPlatformOperations));
+
+            registerFunction(() => new ActivationForViewFetcher(), typeof(IActivationForViewFetcher));
+            registerFunction(() => new XamlDefaultPropertyBinding(), typeof(IDefaultPropertyBindingProvider));
+            registerFunction(() => new CreatesCommandBindingViaCommandParameter(), typeof(ICreatesCommandBinding));
+            registerFunction(() => new CreatesCommandBindingViaEvent(), typeof(ICreatesCommandBinding));
+            //TODO:
+            //registerFunction(() => new AutoDataTemplateBindingHook(), typeof(IPropertyBindingHook));
+        }
+    }
+}
--- ReactiveUI/IDependencyResolver.cs
+++ ReactiveUI/IDependencyResolver.cs
@@ -28,6 +28,7 @@ namespace ReactiveUI
                 "ReactiveUI.Gtk", 
                 "ReactiveUI.Cocoa", 
                 "ReactiveUI.Android",
+                "ReactiveUI.QuickUI",
             };
 
             var fdr = typeof(ModernDependencyResolver);
