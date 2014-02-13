ae603a99de5d52b9a31a3514ecaca3937ca170b1
Paul Betts <paul@paulbetts.org>
2014-02-12 23:25:11 -0800

    Rewrite PerfConsoleRunner to run the entire test suite

--- PerfConsoleRunner/Program.cs
+++ PerfConsoleRunner/Program.cs
@@ -1,49 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
-using Xunit;
-using ReactiveUI.Tests;
+using System.Threading.Tasks;
 
 namespace PerfConsoleRunner
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            // HACK: Make sure the test libraries are loaded
-            var foo = typeof (ReactiveUI.Tests.EnumerableTestMixin);
-            var foo2 = "";
-            var dontoptimizeme = foo.ToString() + foo2.ToString();
+            var file = (new StackTrace(true).GetFrame(0)).GetFileName();
+            var solutionDir = new DirectoryInfo(Path.Combine(Path.GetDirectoryName(file), "..")).FullName;
 
-            // Dig through to find all test classes
-            var classes =
-               (from a in AppDomain.CurrentDomain.GetAssemblies()
-                from m in a.GetModules(false)
-                from t in m.SafeGetTypes()
-                where t.GetMethods().Any(x => x.GetCustomAttributes(typeof (FactAttribute), true).Length > 0)
-                select Activator.CreateInstance(t)).ToArray();
+#if DEBUG
+            var relDir = "Debug";
+#else
+            var relDir = "Release";
+#endif
 
-            var actions = classes.SelectMany(klass => {
-                var emptyArr = new object[0];
-                return klass.GetType().GetMethods()
-                    .Where(x => x.GetCustomAttributes(typeof(FactAttribute), true).Length > 0)
-                    .Select(x => new Action(() => x.Invoke(klass, emptyArr)));
-            }).ToArray();
+            var testAssembly = Path.Combine(solutionDir, @"ReactiveUI.Tests\bin\Debug\Net45\ReactiveUI.Tests_Net45.dll")
+                .Replace("Debug", relDir);
 
-            Console.WriteLine("Preparing to run {0} tests from {1} classes", actions.Length, classes.Length);
-            Console.WriteLine("Unpause the Perf / Memory profiler now, and hit Enter");
-            Console.ReadLine();
-
-            foreach(var v in actions) {
-                try {
-                    v();
-                } catch(Exception ex) {
-                    Console.WriteLine(ex);
-                }
-            }
-
-            Console.WriteLine("*** Made it to the end! ***");
+            Xunit.ConsoleClient.Program.Main(new[] { testAssembly });
+            return 0;
         }
     }
 }
