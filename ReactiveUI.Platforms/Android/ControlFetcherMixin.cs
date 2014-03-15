c346cf5c761cdd414e82d969c31e16fc7afe1df4
mhorn69 <horn@semantico.de>
2014-03-15 12:11:35 +0100

    Add the possibility to use a prefix in WireUpControls for layout / view ids

--- ReactiveUI.Platforms/Android/ControlFetcherMixin.cs
+++ ReactiveUI.Platforms/Android/ControlFetcherMixin.cs
@@ -7,6 +7,7 @@ using System.Text;
 using Android.App;
 using Android.Views;
 using Java.Interop;
+using SupportFragment = Android.Support.V4.App.Fragment;
 
 namespace ReactiveUI.Android
 {
@@ -30,31 +31,39 @@ namespace ReactiveUI.Android
                 .ToDictionary(k => k.Name.ToLowerInvariant(), v => (int)v.GetRawConstantValue());
 
             var type = typeof(ControlFetcherMixin);
-            getControlActivity = type.GetMethod("GetControl", new[] { typeof(Activity), typeof(string) });
-            getControlView = type.GetMethod("GetControl", new[] { typeof(View), typeof(string) });
+            getControlActivity = type.GetMethod("GetControl", new[] { typeof(Activity), typeof(string), typeof(string) });
+            getControlView = type.GetMethod("GetControl", new[] { typeof(View), typeof(string), typeof(string) });
         }
 
-        public static T GetControl<T>(this Activity This, [CallerMemberName]string propertyName = null)
+        public static T GetControl<T>(this Activity This, [CallerMemberName]string propertyName = null, string prefix = null)
             where T : View
         {
+            if (!String.IsNullOrEmpty(prefix) && !String.IsNullOrEmpty(propertyName))
+            {
+                propertyName = string.Format("{0}_{1}", prefix, propertyName);
+            }
             return (T)getCachedControl(propertyName, This,
                 () => This.FindViewById(controlIds[propertyName.ToLowerInvariant()]).JavaCast<T>());
         }
 
-        public static T GetControl<T>(this View This, [CallerMemberName]string propertyName = null)
+        public static T GetControl<T>(this View This, [CallerMemberName]string propertyName = null, string prefix = null)
             where T : View
         {
+            if (!String.IsNullOrEmpty(prefix) && !String.IsNullOrEmpty(propertyName))
+            {
+                propertyName = string.Format("{0}_{1}", prefix, propertyName);
+            }
             return (T)getCachedControl(propertyName, This,
                 () => This.FindViewById(controlIds[propertyName.ToLowerInvariant()]).JavaCast<T>());
         }
 
-        public static T GetControl<T>(this Fragment This, [CallerMemberName]string propertyName = null)
+        public static T GetControl<T>(this Fragment This, [CallerMemberName]string propertyName = null, string prefix = null)
             where T : View
         {
-            return GetControl<T>(This.View, propertyName);
+            return GetControl<T>(This.View, propertyName, prefix);
         }
 
-        public static void WireUpControls(this ILayoutViewHost This)
+        public static void WireUpControls(this ILayoutViewHost This, string prefix = null)
         {
             var members = This.GetType().GetRuntimeProperties()
                 .Where(m => m.PropertyType.IsSubclassOf(typeof(View)));
@@ -62,7 +71,7 @@ namespace ReactiveUI.Android
             members.ToList().ForEach(m => {
                 try {
                     // Find the android control with the same name
-                    var view = This.View.getControlInternal(m.PropertyType, m.Name);
+                    var view = This.View.getControlInternal(m.PropertyType, m.Name, prefix);
 
                     // Set the activity field's value to the view with that identifier
                     m.SetValue(This, view);
@@ -74,7 +83,7 @@ namespace ReactiveUI.Android
         }
 
 
-        public static void WireUpControls(this View This)
+        public static void WireUpControls(this View This, string prefix = null)
         {
             var members = This.GetType().GetRuntimeProperties()
                 .Where(m => m.PropertyType.IsSubclassOf(typeof(View)));
@@ -82,7 +91,7 @@ namespace ReactiveUI.Android
             members.ToList().ForEach(m => {
                 try {
                     // Find the android control with the same name
-                    var view = This.getControlInternal(m.PropertyType, m.Name);
+                    var view = This.getControlInternal(m.PropertyType, m.Name, prefix);
 
                     // Set the activity field's value to the view with that identifier
                     m.SetValue(This, view);
@@ -98,7 +107,35 @@ namespace ReactiveUI.Android
         /// </summary>
         /// <param name="This"></param>
         /// <param name="inflatedView"></param>
-        public static void WireUpControls(this Fragment This, View inflatedView)
+        public static void WireUpControls(this SupportFragment This, View inflatedView, string prefix = null)
+        {
+            var members = This.GetType().GetRuntimeProperties()
+                .Where(m => m.PropertyType.IsSubclassOf(typeof(View)));
+
+            members.ToList().ForEach(m =>
+            {
+                try
+                {
+                    // Find the android control with the same name from the view
+                    var view = inflatedView.getControlInternal(m.PropertyType, m.Name, prefix);
+
+                    // Set the activity field's value to the view with that identifier
+                    m.SetValue(This, view);
+                }
+                catch (Exception ex)
+                {
+                    throw new MissingFieldException("Failed to wire up the Property "
+                        + m.Name + " to a View in your layout with a corresponding identifier", ex);
+                }
+            });
+        }
+
+        /// <summary>
+        /// This should be called in the Fragement's OnCreateView, with the newly inflated layout
+        /// </summary>
+        /// <param name="This"></param>
+        /// <param name="inflatedView"></param>
+        public static void WireUpControls(this Fragment This, View inflatedView, string prefix = null)
         {
             var members = This.GetType().GetRuntimeProperties()
                 .Where(m => m.PropertyType.IsSubclassOf(typeof(View)));
@@ -106,7 +143,7 @@ namespace ReactiveUI.Android
             members.ToList().ForEach(m => {
                 try {
                     // Find the android control with the same name from the view
-                    var view = inflatedView.getControlInternal(m.PropertyType, m.Name);
+                    var view = inflatedView.getControlInternal(m.PropertyType, m.Name, prefix);
 
                     // Set the activity field's value to the view with that identifier
                     m.SetValue(This, view);
@@ -117,7 +154,7 @@ namespace ReactiveUI.Android
             });
         }
 
-        public static void WireUpControls(this Activity This)
+        public static void WireUpControls(this Activity This, string prefix = null)
         {
             var members = This.GetType().GetRuntimeProperties()
                 .Where(m => m.PropertyType.IsSubclassOf(typeof(View)));
@@ -125,7 +162,7 @@ namespace ReactiveUI.Android
             members.ToList().ForEach(m => {
                 try {
                     // Find the android control with the same name
-                    var view = This.getControlInternal(m.PropertyType, m.Name);
+                    var view = This.getControlInternal(m.PropertyType, m.Name, prefix);
 
                     // Set the activity field's value to the view with that identifier
                     m.SetValue(This, view);
@@ -136,16 +173,16 @@ namespace ReactiveUI.Android
             });
         }
 
-        static View getControlInternal(this View parent, Type viewType, string name)
+        static View getControlInternal(this View parent, Type viewType, string name, string prefix = null)
         {
             var mi = getControlView.MakeGenericMethod(new[] { viewType });
-            return (View)mi.Invoke(null, new object[] { parent, name });
+            return (View)mi.Invoke(null, new object[] { parent, name, prefix });
         }
 
-        static View getControlInternal(this Activity parent, Type viewType, string name)
+        static View getControlInternal(this Activity parent, Type viewType, string name, string prefix = null)
         {
             var mi = getControlActivity.MakeGenericMethod(new[] { viewType });
-            return (View)mi.Invoke(null, new object[] { parent, name });
+            return (View)mi.Invoke(null, new object[] { parent, name, prefix });
         }
 
         static View getCachedControl(string propertyName, object rootView, Func<View> fetchControlFromView)
