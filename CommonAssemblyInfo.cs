d41ac5806660035162596b4455a9c1a26abe9324
Paul Betts <paul@paulbetts.org>
2014-04-09 13:13:46 -0700

    Version bump to 5.5.1

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("5.5.0")]
-[assembly: AssemblyFileVersion("5.5.0")]
+[assembly: AssemblyVersion("5.5.1")]
+[assembly: AssemblyFileVersion("5.5.1")]
