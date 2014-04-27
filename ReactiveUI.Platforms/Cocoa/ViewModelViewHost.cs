980aef7c904cdce9e7544bbb8b23aa7fd5ec12d7
Michael Teper <michael.teper@elloco.com>
2014-04-27 08:15:08 -0700

    Report view resolution failure

--- ReactiveUI.Platforms/Cocoa/ViewModelViewHost.cs
+++ ReactiveUI.Platforms/Cocoa/ViewModelViewHost.cs
@@ -45,6 +45,16 @@ namespace ReactiveUI.Cocoa
 
                 var viewLocator = ViewLocator ?? ReactiveUI.ViewLocator.Current;
                 var view = viewLocator.ResolveView(x.ViewModel, x.Contract);
+				if (view == null)
+				{
+                    var message = string.Format("Unable to resolve view for \"{0}\"", x.ViewModel.GetType());
+                    if (x.Contract != null)
+                    {
+                        message += string.Format(" and contract \"{1}\"", x.Contract.GetType());
+                    }
+                    message += ".";
+					throw new Exception(message);
+				}
                 view.ViewModel = x.ViewModel;
 
                 viewLastAdded = ((NSViewController)view).View;
