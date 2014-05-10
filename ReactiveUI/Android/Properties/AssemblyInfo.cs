b641f5878b86304537bc6efb43bc8bbbe422fadb
Paul Betts <paul@paulbetts.org>
2014-05-10 10:55:30 -0700

    Fix up Xamarin build

--- ReactiveUI/Android/Properties/AssemblyInfo.cs
+++ ReactiveUI/Android/Properties/AssemblyInfo.cs
@@ -2,7 +2,4 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyProduct("ReactiveUI.Android")]
-[assembly: AssemblyDescription("An MVVM framework that integrates the Reactive Extensions")]
-
 [assembly: InternalsVisibleTo("ReactiveUI.Tests_Monodroid")]
