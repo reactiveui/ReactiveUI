b07a0dd6c590409a0ed40589065a842c4be459a5
Johan Laanstra <jlaanstra1221@outlook.com>
2014-05-19 23:21:40 +0200

    Adds NotifyPropertyChanged wrapper.

--- ReactiveUI/Interfaces.cs
+++ ReactiveUI/Interfaces.cs
@@ -10,11 +10,9 @@ using Splat;
 namespace ReactiveUI
 {
     /// <summary>
-    /// IObservedChange is a generic interface that replaces the non-generic
-    /// PropertyChangedEventArgs. Note that it is used for both Changing (i.e.
-    /// 'before change') and Changed Observables. In the future, this interface
-    /// will be Covariant which will allow simpler casting between specific and
-    /// generic changes.
+    /// IObservedChange is a generic interface that is returned from WhenAny()
+    /// Note that it is used for both Changing (i.e.'before change') 
+    /// and Changed Observables.
     /// </summary>
     public interface IObservedChange<out TSender, out TValue>
     {
@@ -115,6 +113,59 @@ namespace ReactiveUI
     public interface IReactiveCommand<T> : IObservable<T>, IReactiveCommand
     {
         IObservable<T> ExecuteAsync(object parameter = null);
+    }
+
+    /// <summary>
+    /// IReactivePropertyChangedEventArgs is a generic interface that 
+    /// is used to wrap the NotifyPropertyChangedEventArgs and gives 
+    /// information about changed properties. It includes also 
+    /// the sender of the notification.
+    /// Note that it is used for both Changing (i.e.'before change') 
+    /// and Changed Observables.
+    /// </summary>
+    public interface IReactivePropertyChangedEventArgs<out TSender>
+    {
+        /// <summary>
+        /// The name of the property that has changed on Sender.
+        /// </summary>
+        string PropertyName { get; }
+
+        /// <summary>
+        /// The object that has raised the change.
+        /// </summary>
+        TSender Sender { get; }
+    }
+
+    public class ReactivePropertyChangingEventArgs<TSender> : PropertyChangingEventArgs, IReactivePropertyChangedEventArgs<TSender>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReactivePropertyChangingEventArgs{TSender}"/> class.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        public ReactivePropertyChangingEventArgs(TSender sender, string propertyName)
+            : base(propertyName)
+        {
+            this.Sender = sender;
+        }
+
+        public TSender Sender { get; private set; }
+    }
+
+    public class ReactivePropertyChangedEventArgs<TSender> : PropertyChangedEventArgs, IReactivePropertyChangedEventArgs<TSender>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReactivePropertyChangedEventArgs{TSender}"/> class.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        public ReactivePropertyChangedEventArgs(TSender sender, string propertyName)
+            : base(propertyName)
+        {
+            this.Sender = sender;
+        }
+
+        public TSender Sender { get; private set; }
     }
 
     /// <summary>
