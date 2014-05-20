737bd11be24869e38dd975964074f066be3a959e
Paul Betts <paul@paulbetts.org>
2014-05-20 16:31:59 -0700

    Style fixups

--- ReactiveUI.Winforms/Winforms/ViewModelViewHost.cs
+++ ReactiveUI.Winforms/Winforms/ViewModelViewHost.cs
@@ -6,7 +6,6 @@ using System.Reactive.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Forms;
 using ReactiveUI;
-
 using Splat;
 
 namespace ReactiveUI.Winforms
@@ -21,29 +20,27 @@ namespace ReactiveUI.Winforms
         IObservable<string> viewContractObservable;
         object viewModel;
 
-        object content;
-
         public static bool DefaultCacheViewsEnabled { get; set; }
 
-        bool cacheViews;
 
         public ViewModelViewHost()
         {
             this.InitializeComponent();
             this.cacheViews = DefaultCacheViewsEnabled;
-            foreach (var subscription in this.SetupBindings()) {
+
+            foreach (var subscription in this.setupBindings()) {
                 disposables.Add(subscription);
             }
         }
 
-        private IEnumerable<IDisposable> SetupBindings()
+        IEnumerable<IDisposable> setupBindings()
         {
             var viewChanges =
                 this.WhenAnyValue(x => x.Content).Select(x => x as Control).Where(x => x != null).Subscribe(x => {
-                    //change the view in the ui
+                    // change the view in the ui
                     this.SuspendLayout();
 
-                    //clear out existing visible control view
+                    // clear out existing visible control view
                     foreach (Control c in this.Controls) {
                         c.Dispose();
                         this.Controls.Remove(c);
@@ -69,31 +66,26 @@ namespace ReactiveUI.Winforms
                     .CombineLatest(this.WhenAnyObservable(x => x.ViewContractObservable),
                         (vm, contract) => new { ViewModel = vm, Contract = contract });
 
-
-
-           yield return vmAndContract.Subscribe(x =>
-            {
-
-                //set content to default when viewmodel is null
-                if (this.ViewModel == null)
-                {
-                    if (this.DefaultContent != null)
-                    {
+            yield return vmAndContract.Subscribe(x => {
+                // set content to default when viewmodel is null
+                if (this.ViewModel == null) {
+                    if (this.DefaultContent != null) {
                         this.Content = DefaultContent;
                     }
+
                     return;
                 }
 
-                if (CacheViews)
-                {
-                    //when caching views, check the current viewmodel and type
+                if (CacheViews) {
+                    // when caching views, check the current viewmodel and type
                     var c = content as IViewFor;
+
                     if (c != null && c.ViewModel != null
-                        && c.ViewModel.GetType() == x.ViewModel.GetType())
-                    {
+                        && c.ViewModel.GetType() == x.ViewModel.GetType()) {
                         c.ViewModel = x.ViewModel;
-                        //return early here after setting the viewmodel
-                        //allowing the view to update it's bindings
+
+                        // return early here after setting the viewmodel
+                        // allowing the view to update it's bindings
                         return;
                     }
                 }
@@ -106,8 +98,7 @@ namespace ReactiveUI.Winforms
             }, RxApp.DefaultExceptionHandler.OnNext);
         }
 
-        public event PropertyChangingEventHandler PropertyChanging
-        {
+        public event PropertyChangingEventHandler PropertyChanging {
             add { PropertyChangingEventManager.AddHandler(this, value); }
             remove { PropertyChangingEventManager.RemoveHandler(this, value); }
         }
@@ -117,8 +108,7 @@ namespace ReactiveUI.Winforms
             PropertyChangingEventManager.DeliverEvent(this, args);
         }
 
-        public event PropertyChangedEventHandler PropertyChanged
-        {
+        public event PropertyChangedEventHandler PropertyChanged {
             add { PropertyChangedEventManager.AddHandler(this, value); }
             remove { PropertyChangedEventManager.RemoveHandler(this, value); }
         }
@@ -128,22 +118,19 @@ namespace ReactiveUI.Winforms
             PropertyChangedEventManager.DeliverEvent(this, args);
         }
 
-        public Control CurrentView
-        {
+        public Control CurrentView {
             get { return this.content as Control; }
         }
 
         [Category("ReactiveUI")]
         [Description("The default control when no viewmodel is specified")]
-        public Control DefaultContent
-        {
+        public Control DefaultContent {
             get { return this.defaultContent; }
             set { this.RaiseAndSetIfChanged(ref this.defaultContent, value); }
         }
 
         [Browsable(false)]
-        public IObservable<string> ViewContractObservable
-        {
+        public IObservable<string> ViewContractObservable {
             get { return this.viewContractObservable; }
             set { this.RaiseAndSetIfChanged(ref this.viewContractObservable, value); }
         }
@@ -154,27 +141,28 @@ namespace ReactiveUI.Winforms
         [Category("ReactiveUI")]
         [Description("The viewmodel to host.")]
         [Bindable(true)]
-        public object ViewModel
-        {
+        public object ViewModel {
             get { return this.viewModel; }
             set { this.RaiseAndSetIfChanged(ref this.viewModel, value); }
         }
 
+        object content;
+
         [Category("ReactiveUI")]
         [Description("The Current View")]
         [Bindable(true)]
-        public object Content
-        {
+        public object Content {
             get { return this.content; }
             protected set { this.RaiseAndSetIfChanged(ref this.content, value); }
         }
+
+        bool cacheViews;
 
         [Category("ReactiveUI")]
         [Description("Cache Views")]
         [Bindable(true)]
         [DefaultValue(true)]
-        public bool CacheViews
-        {
+        public bool CacheViews {
             get { return this.cacheViews; }
             set { this.RaiseAndSetIfChanged(ref this.cacheViews, value); }
         }
