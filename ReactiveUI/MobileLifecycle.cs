b0158794878845304cc3208f66f942f23fb32644
Paul Betts <paul@paulbetts.org>
2014-05-22 16:48:47 -0700

    Slowly cut SuspensionHost into something that doesn't suck

--- ReactiveUI/MobileLifecycle.cs
+++ ReactiveUI/MobileLifecycle.cs
@@ -3,21 +3,58 @@ using System.Reactive;
 using System.Reactive.Linq;
 using Splat;
 using System.Reactive.Disposables;
+using System.Reactive.Subjects;
 
 namespace ReactiveUI.Mobile
 {
-    public class SuspensionHost : ReactiveObject, ISuspensionHost
+    internal class SuspensionHost : ReactiveObject, ISuspensionHost
     {
-        public IObservable<Unit> IsLaunchingNew { get; set; }
-        public IObservable<Unit> IsResuming { get; set; }
-        public IObservable<Unit> IsUnpausing { get; set; }
-        public IObservable<IDisposable> ShouldPersistState { get; set; }
-        public IObservable<Unit> ShouldInvalidateState { get; set; }
-        public Action<ISuspensionDriver> SetupDefaultSuspendResumeFunc { get; set; }
+        readonly Subject<Unit> isLaunchingNewProxy = new Subject<Unit>();
+        readonly SerialDisposable isLaunchingNewSub = new SerialDisposable();
+        public IObservable<Unit> IsLaunchingNew {
+            get { return isLaunchingNewProxy; }
+            set { isLaunchingNewSub.Disposable = value.Subscribe(isLaunchingNewProxy); }
+        }
+
+        readonly Subject<Unit> isResumingProxy = new Subject<Unit>();
+        readonly SerialDisposable isResumingSub = new SerialDisposable();
+        public IObservable<Unit> IsResuming {
+            get { return isResumingProxy; }
+            set { isResumingSub.Disposable = value.Subscribe(isResumingProxy); }
+        }
+
+        readonly Subject<Unit> isUnpausingProxy = new Subject<Unit>();
+        readonly SerialDisposable isUnpausingSub = new SerialDisposable();
+        public IObservable<Unit> IsUnpausing {
+            get { return isUnpausingProxy; }
+            set { isLaunchingNewSub.Disposable = value.Subscribe(isUnpausingProxy); }
+        }
+
+        readonly Subject<IDisposable> shouldPersistStateProxy = new Subject<IDisposable>();
+        readonly SerialDisposable shouldPersistStateSub = new SerialDisposable();
+        public IObservable<IDisposable> ShouldPersistState {
+            get { return shouldPersistStateProxy; }
+            set { shouldPersistStateSub.Disposable = value.Subscribe(shouldPersistStateProxy); }
+        }
+
+        readonly Subject<Unit> shouldInvalidateStateProxy = new Subject<Unit>();
+        readonly SerialDisposable shouldInvalidateStateSub = new SerialDisposable();
+        public IObservable<Unit> ShouldInvalidateState {
+            get { return shouldInvalidateStateProxy; }
+            set { shouldInvalidateStateSub.Disposable = value.Subscribe(shouldInvalidateStateProxy); }
+        }
+
+        public Func<IApplicationRootState> CreateNewAppState { get; set; }
+
+        IApplicationRootState appState;
+        public IApplicationRootState AppState {
+            get { return appState; }
+            set { this.RaiseAndSetIfChanged(ref appState, value); }
+        }
 
         public SuspensionHost()
         {
-#if UIKIT
+#if COCOA
             var message = "Your AppDelegate class needs to derive from AutoSuspendAppDelegate";
 #elif ANDROID
             var message = "Your Activities need to instantiate AutoSuspendActivityHelper";
@@ -27,68 +64,48 @@ namespace ReactiveUI.Mobile
 
             IsLaunchingNew = IsResuming = IsUnpausing = ShouldInvalidateState =
                 Observable.Throw<Unit>(new Exception(message));
+
             ShouldPersistState = Observable.Throw<IDisposable>(new Exception(message));
         }
 
-        public void SetupDefaultSuspendResume(ISuspensionDriver driver = null)
+        public IObservable<T> ObserveAppState<T>()
         {
-            SetupDefaultSuspendResumeFunc(driver);
+            return this.WhenAny(x => x.AppState, x => (T)x.Value);
         }
-    }
 
-    public static class SuspensionHostExtensions
-    {
-        public static IDisposable SetupDefaultSuspendResume(this ISuspensionHost This, ISuspensionDriver driver = null)
+        public T GetAppState<T>()
+        {
+            return (T)AppState;
+        }
+                
+        public IDisposable SetupDefaultSuspendResume(ISuspensionDriver driver = null)
         {
             var ret = new CompositeDisposable();
             driver = driver ?? Locator.Current.GetService<ISuspensionDriver>();
 
-            ret.Add(This.ShouldInvalidateState
+            ret.Add(this.ShouldInvalidateState
                 .SelectMany(_ => driver.InvalidateState())
                 .LoggedCatch(this, Observable.Return(Unit.Default), "Tried to invalidate app state")
-                .Subscribe(_ => This.Log().Info("Invalidated app state")));
+                .Subscribe(_ => this.Log().Info("Invalidated app state")));
 
-            ret.Add(This.ShouldPersistState
-                .SelectMany(x => driver.SaveState(viewModel).Finally(x.Dispose))
+            ret.Add(this.ShouldPersistState
+                .SelectMany(x => driver.SaveState(AppState).Finally(x.Dispose))
                 .LoggedCatch(this, Observable.Return(Unit.Default), "Tried to persist app state")
-                .Subscribe(_ => This.Log().Info("Persisted application state")));
+                .Subscribe(_ => this.Log().Info("Persisted application state")));
 
-            ret.Add(This.IsResuming
+            ret.Add(this.IsResuming
                 .SelectMany(x => driver.LoadState<IApplicationRootState>())
                 .LoggedCatch(this,
-                    Observable.Defer(() => Observable.Return(Locator.Current.GetService<IApplicationRootState>())),
+                    Observable.Defer(() => Observable.Return(CreateNewAppState())),
                     "Failed to restore app state from storage, creating from scratch")
                 .ObserveOn(RxApp.MainThreadScheduler)
-                .Subscribe(x => viewModel = x));
+                .Subscribe(x => AppState = x));
 
-            ret.Add(This.IsLaunchingNew.Subscribe(_ => {
-                viewModel = Locator.Current.GetService<IApplicationRootState>();
+            ret.Add(this.IsLaunchingNew.Subscribe(_ => {
+                AppState = CreateNewAppState();
             }));
-        }
-    }
 
-    public class DummySuspensionHost : ISuspensionHost
-    {
-        public void SetupDefaultSuspendResume(ISuspensionDriver driver = null) { }
-
-        public IObservable<Unit> IsLaunchingNew {
-            get { return Observable.Return(Unit.Default); }
-        }
-
-        public IObservable<Unit> IsResuming {
-            get { return Observable.Never<Unit>(); }
-        }
-
-        public IObservable<Unit> IsUnpausing {
-            get { return Observable.Never<Unit>(); }
-        }
-
-        public IObservable<IDisposable> ShouldPersistState {
-            get { return Observable.Never<IDisposable>(); }
-        }
-
-        public IObservable<Unit> ShouldInvalidateState {
-            get { return Observable.Never<Unit>(); }
+            return ret;
         }
     }
 
@@ -109,6 +126,4 @@ namespace ReactiveUI.Mobile
             return Observable.Return(Unit.Default);
         }
     }
-
-
 }
