8c787daaba875790fd72b347b8d0aecf04ca83a0
Rik Bosch <r.bosch@rgbplus.nl>
2014-05-27 14:39:28 +0200

    Adding Activation Support for Winforms Views. (ref #604)
    ref #610 - ActivationForViewFetcher does not raise activate  or deactivate twice

---
+++ ReactiveUI.Tests/Winforms/ActivationTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+using ReactiveUI.Winforms;
+
+using Xunit;
+
+namespace ReactiveUI.Tests.Winforms
+{
+    public class ActivationTests
+    {
+        [Fact]
+        public void ActivationForViewFetcherSupportsDefaultWinformsComponents()
+        {
+            var target = new ActivationForViewFetcher();
+            var supportedComponents = new[] { typeof(Control), typeof(UserControl), typeof(Form) };
+            foreach (var c in supportedComponents) {
+                Assert.Equal(10, target.GetAffinityForView(c));
+            }
+        }
+
+        [Fact]
+        public void CanFetchActivatorForForm()
+        {
+            var form = new TestForm();
+            var target = new ActivationForViewFetcher();
+            var formActivator = target.GetActivationForView(form);
+
+            Assert.NotNull(formActivator);
+            Assert.NotNull(formActivator.Item1);
+            Assert.NotNull(formActivator.Item2);
+        }
+      
+
+        [Fact]
+        public void CanFetchActivatorForControl()
+        {
+            var control = new TestControl();
+            var target = new ActivationForViewFetcher();
+            var activator = target.GetActivationForView(control);
+
+            Assert.NotNull(activator);
+            Assert.NotNull(activator.Item1);
+            Assert.NotNull(activator.Item2);
+        }
+
+        [Fact]
+        public void SmokeTestWindowsForm()
+        {
+            var target = new ActivationForViewFetcher();
+            using (var form = new TestForm()) {
+                var formActivator = target.GetActivationForView(form);
+
+                int formActivateCount = 0, formDeActivateCount = 0;
+                formActivator.Item1.Subscribe(_ => formActivateCount++);
+                formActivator.Item2.Subscribe(_ => formDeActivateCount++);
+
+                Assert.Equal(0, formActivateCount);
+                Assert.Equal(0, formDeActivateCount);
+
+                form.Visible = true;
+                Assert.Equal(1, formActivateCount);
+
+                form.Visible = false;
+                Assert.Equal(1, formActivateCount);
+                Assert.Equal(1, formDeActivateCount);
+
+                form.Visible = true;
+                Assert.Equal(2, formActivateCount);
+
+                form.Close();
+                Assert.Equal(2, formDeActivateCount);
+            }
+        }
+
+        [Fact]
+        public void SmokeTestUserControl()
+        {
+            var target = new ActivationForViewFetcher();
+            using(var userControl = new TestControl())
+            using (var parent = new TestForm()) {
+
+                var userControlActivator = target.GetActivationForView(userControl);
+
+                int userControlActivateCount = 0, userControlDeActivateCount = 0;
+                userControlActivator.Item1.Subscribe(_ => userControlActivateCount++);
+                userControlActivator.Item2.Subscribe(_ => userControlDeActivateCount++);
+
+
+                parent.Visible = true;
+                parent.Controls.Add(userControl);
+
+                userControl.Visible = true;
+                Assert.Equal(1, userControlActivateCount);
+                userControl.Visible = false;
+                Assert.Equal(1, userControlDeActivateCount);
+
+                userControl.Visible = true;
+                Assert.Equal(2, userControlActivateCount);
+
+                //closing the form deactivated the usercontrol
+                parent.Close();
+                Assert.Equal(2, userControlDeActivateCount);
+            }
+        }
+
+        class TestControl : System.Windows.Forms.Control, IActivatable { }
+
+        class TestForm : System.Windows.Forms.Form, IActivatable { }
+    }
+}
---
+++ ReactiveUI.Winforms/Winforms/ActivationForViewFetcher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+using Splat;
+
+namespace ReactiveUI.Winforms
+{
+    public class ActivationForViewFetcher : IActivationForViewFetcher, IEnableLogger
+    {
+
+        public int GetAffinityForView(Type view)
+        {
+            return (typeof(Control).GetTypeInfo().IsAssignableFrom(view.GetTypeInfo())) ? 10 : 0;
+        }
+
+        public Tuple<IObservable<Unit>, IObservable<Unit>> GetActivationForView(IActivatable view)
+        {
+            var control = view as Control;
+            if (control == null) {
+                //show a friendly warning in the log that this view will never be activated
+                this.Log().Warn("Expected a view of type System.Windows.Forms.Control but it is {0}.\r\nYou need to implement your own IActivationForViewFetcher for {0}.", view.GetType());
+                return Tuple.Create(Observable.Empty<Unit>(), Observable.Empty<Unit>());
+            }
+
+            // create an observable stream of booleans
+            // true representing the control is active, false representing the control is not active
+            // by using DistinctUntilChanged, a control can either be active or not active
+            // this should also fix #610 for winforms
+
+            var controlVisible = Observable.FromEventPattern(control, "VisibleChanged").Select(_ => control.Visible);
+            var handleDestroyed = Observable.FromEventPattern(control, "HandleDestroyed").Select(_ => false);
+            var handleCreated = Observable.FromEventPattern(control, "HandleCreated").Select(_ => true);
+
+            var controlActive = Observable.Merge(controlVisible, handleDestroyed, handleCreated)
+                .DistinctUntilChanged();
+
+
+            var controlActivated = controlActive.Where(x => x).Select(_ => Unit.Default);
+            var controlDeactivated = controlActive.Where(x => !x).Select(_ => Unit.Default);
+
+            var form = view as Form;
+            if (form != null) {
+                var formActive = Observable.FromEventPattern(form, "Closed").Select(_ => false);
+                var formDeactivated = controlActive.Merge(formActive).DistinctUntilChanged().Where(x => !x).Select(_ => Unit.Default);
+                return Tuple.Create(controlActivated, formDeactivated);
+            }
+
+
+
+            return Tuple.Create(controlActivated, controlDeactivated);
+        }
+    }
+}
