96e35822f5fd308991f403acd64df2536c5ebad0
Paul Betts <paul@paulbetts.org>
2014-05-26 18:45:48 -0700

    Burn down Playground-WinRT, replace with Universal app

---
+++ Playground-Wpa81/Playground-Wpa81.Shared/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Playground_Wpa81
+{
+    public class MainPageViewModel : ReactiveObject
+    {
+        public ReactiveCommand<Object> DoIt { get; protected set; }
+
+        public MainPageViewModel()
+        {
+            DoIt = ReactiveCommand.Create();
+        }
+    }
+}
