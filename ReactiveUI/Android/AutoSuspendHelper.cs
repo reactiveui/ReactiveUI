51ef4c98f5a3e742cc044ab9b1686cf15707992c
Paul Betts <paul@paulbetts.org>
2014-05-26 23:28:34 -0700

    Refactor AutoSuspendActivityHelper a bit

--- ReactiveUI/Android/AutoSuspendHelper.cs
+++ ReactiveUI/Android/AutoSuspendHelper.cs
@@ -11,7 +11,7 @@ using System.Reactive.Disposables;
 
 namespace ReactiveUI.Mobile
 {
-    public class AutoSuspendActivityHelper : IEnableLogger
+    public class AutoSuspendHelper : IEnableLogger
     {
         readonly Subject<Bundle> onCreate = new Subject<Bundle>();
         readonly Subject<Unit> onRestart = new Subject<Unit>();
@@ -22,27 +22,15 @@ namespace ReactiveUI.Mobile
 
         public static readonly Subject<Unit> untimelyDemise = new Subject<Unit>();
 
-        static AutoSuspendActivityHelper()
+        static AutoSuspendHelper()
         {
             AppDomain.CurrentDomain.UnhandledException += (o, e) => untimelyDemise.OnNext(Unit.Default);
         }
 
-        public AutoSuspendActivityHelper(Activity hostActivity)
+        public AutoSuspendHelper(Activity hostActivity)
         {
-            var methodsToCheck = new[] {
-                "OnRestart", "OnSaveInstanceState", "OnCreate",
-            };
-
-            var missingMethod = methodsToCheck
-                .Select(x => {
-                    var methods = hostActivity.GetType().GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-                    return Tuple.Create(x, methods.FirstOrDefault(y => y.Name == x));
-                })
-                .FirstOrDefault(x => x.Item2 == null);
-
-            if (missingMethod != null) {
-                throw new Exception(String.Format("Your activity must implement {0} and call AutoSuspendActivityHelper.{0}", missingMethod.Item1));
-            }
+            Reflection.ThrowIfMethodsNotOverloaded("AutoSuspendHelper", hostActivity,
+                "OnRestart", "OnSaveInstanceState", "OnCreate");
 
             Observable.Merge(onCreate, onSaveInstanceState).Subscribe(x => LatestBundle = x);
 
