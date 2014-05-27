d9732ec04927b28963361b7561ae1e94578b61d1
Paul Betts <paul@paulbetts.org>
2014-05-27 02:04:30 -0700

    Style

--- ReactiveUI.Winforms/Winforms/ObservableCollectionChangedToListChangedTransformer.cs
+++ ReactiveUI.Winforms/Winforms/ObservableCollectionChangedToListChangedTransformer.cs
@@ -19,26 +19,26 @@ namespace ReactiveUI.Winforms
             }
 
             switch (ea.Action) {
-                case NotifyCollectionChangedAction.Reset:
-                    yield return new ListChangedEventArgs(ListChangedType.Reset, -1);
-                    break;
-                case NotifyCollectionChangedAction.Replace:
-                    yield return new ListChangedEventArgs(ListChangedType.ItemChanged, ea.NewStartingIndex);
-                    break;
-                case NotifyCollectionChangedAction.Remove:
-                    foreach (int index in Enumerable.Range(ea.OldStartingIndex, ea.OldItems.Count)) {
-                        yield return new ListChangedEventArgs(ListChangedType.ItemDeleted, index);
-                    }
-                    break;
-                case NotifyCollectionChangedAction.Add:
-                    foreach (int index in Enumerable.Range(ea.NewStartingIndex, ea.NewItems.Count)) {
-                        yield return new ListChangedEventArgs(ListChangedType.ItemAdded, index);
-                    }
-                    break;
-                case NotifyCollectionChangedAction.Move:
-                    // http://msdn.microsoft.com/en-us/library/acskc6xz(v=vs.110).aspx
-                    yield return new ListChangedEventArgs(ListChangedType.ItemMoved, ea.NewStartingIndex, ea.OldStartingIndex);
-                    break;
+            case NotifyCollectionChangedAction.Reset:
+                yield return new ListChangedEventArgs(ListChangedType.Reset, -1);
+                break;
+            case NotifyCollectionChangedAction.Replace:
+                yield return new ListChangedEventArgs(ListChangedType.ItemChanged, ea.NewStartingIndex);
+                break;
+            case NotifyCollectionChangedAction.Remove:
+                foreach (int index in Enumerable.Range(ea.OldStartingIndex, ea.OldItems.Count)) {
+                    yield return new ListChangedEventArgs(ListChangedType.ItemDeleted, index);
+                }
+                break;
+            case NotifyCollectionChangedAction.Add:
+                foreach (int index in Enumerable.Range(ea.NewStartingIndex, ea.NewItems.Count)) {
+                    yield return new ListChangedEventArgs(ListChangedType.ItemAdded, index);
+                }
+                break;
+            case NotifyCollectionChangedAction.Move:
+                // http://msdn.microsoft.com/en-us/library/acskc6xz(v=vs.110).aspx
+                yield return new ListChangedEventArgs(ListChangedType.ItemMoved, ea.NewStartingIndex, ea.OldStartingIndex);
+                break;
             }
         }
     }
