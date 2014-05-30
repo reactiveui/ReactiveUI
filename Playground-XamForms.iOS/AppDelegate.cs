48bb9c42c576eb60bf293e740849c106440ed1cd
Paul Betts <paul@paulbetts.org>
2014-05-30 16:36:38 -0700

    Add a Xamarin.Forms playground

---
+++ Playground-XamForms.iOS/AppDelegate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using Xamarin.Forms;
+
+namespace PlaygroundXamForms.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		UIWindow window;
+
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			Forms.Init ();
+
+			window = new UIWindow (UIScreen.MainScreen.Bounds);
+			
+			window.RootViewController = App.GetMainPage ().CreateViewController ();
+			window.MakeKeyAndVisible ();
+			
+			return true;
+		}
+	}
+}
+
---
+++ Playground-XamForms/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PlaygroundXamForms
+{    
+    public partial class MainPage : ContentPage
+    {    
+        public MainPage ()
+        {
+            InitializeComponent ();
+        }
+    }
+}
+
