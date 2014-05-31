2a3cd311bbfb468debbdfe2bbb84d09802ad2342
Paul Betts <paul@paulbetts.org>
2014-05-30 17:01:34 -0700

    Bindings == working

---
+++ Playground-XamForms/Views/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using ReactiveUI;
+using Xamarin.Forms;
+
+namespace PlaygroundXamForms
+{    
+    public partial class MainPage : ContentPage, IViewFor<MainPageViewModel>
+    {    
+        public MainPage ()
+        {
+            InitializeComponent ();
+            ViewModel = new MainPageViewModel();
+
+            this.OneWayBind(ViewModel, x => x.SavedGuid, x => x.savedGuid.Text);
+        }
+
+        public static readonly BindableProperty ViewModelProperty = BindableProperty.Create<MainPage, MainPageViewModel>(
+            x => x.ViewModel, null, BindingMode.OneWay);
+
+        public MainPageViewModel ViewModel {
+            get { return (MainPageViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        object IViewFor.ViewModel {
+            get { return ViewModel; }
+            set { ViewModel = (MainPageViewModel)value; }
+        }
+    }
+}
+
