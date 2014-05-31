8c9e78948551d13b5669e775fde9cae4fbe2f4e6
Paul Betts <paul@paulbetts.org>
2014-05-30 17:01:34 -0700

    Bindings == working

---
+++ Playground-XamForms/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ReactiveUI;
+
+namespace PlaygroundXamForms
+{
+    public class MainPageViewModel : ReactiveObject
+    {
+        public MainPageViewModel()
+        {
+            SavedGuid = Guid.NewGuid();
+        }
+
+        Guid savedGuid;
+        public Guid SavedGuid {
+            get { return savedGuid; }
+            set { this.RaiseAndSetIfChanged(ref savedGuid, value); }
+        }
+    }
+}
+
