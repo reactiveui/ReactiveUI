e194526f022c348c420622fdab03f84723d7f144
Paul Betts <paul@paulbetts.org>
2014-07-02 23:13:56 -0700

    Finish up documenting RxUI Core

--- ReactiveUI/WaitForDispatcherScheduler.cs
+++ ReactiveUI/WaitForDispatcherScheduler.cs
@@ -21,9 +21,9 @@ namespace ReactiveUI
         {
             _schedulerFactory = schedulerFactory;
 
-            // NB: Creating a scheduler will fail on WinRT if we attempt to do 
+            // NB: Creating a scheduler will fail on WinRT if we attempt to do
             // so on a non-UI thread, even if the underlying Dispatcher exists.
-            // We assume (hope?) that WaitForDispatcherScheduler will be created 
+            // We assume (hope?) that WaitForDispatcherScheduler will be created
             // early enough that this won't be the case.
             attemptToCreateScheduler();
         }
