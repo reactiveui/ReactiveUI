e41120eb067a1680f67dbf14fa3ae56f0b0b5fec
Paul Betts <paul@paulbetts.org>
2014-07-07 21:56:32 -0700

    Version bump to 6.0

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("5.99.6")]
-[assembly: AssemblyFileVersion("5.99.6")]
+[assembly: AssemblyVersion("6.0.0")]
+[assembly: AssemblyFileVersion("6.0.0")]
