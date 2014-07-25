bc29c84ee48a8b9b2b56e1c1efb14d4a626b81ea
David Bordoley <bordoley@gmail.com>
2014-07-24 19:33:07 -0700

    Add a scheduler for Android looper and handler classes. Add ReactiveAndroidApplication for applications with a top level view model.

---
+++ ReactiveUI/Android/AndroidHandlerScheduler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using Android.OS;
+
+namespace ReactiveUI
+{
+	public sealed class AndroidHandlerScheduler : IScheduler
+	{
+		private readonly Handler handler;
+
+		public AndroidHandlerScheduler(Handler handler)
+		{
+			this.handler = handler;
+		}
+
+		public AndroidHandlerScheduler(Looper looper) : this(new Handler(looper)) {}
+
+		public AndroidHandlerScheduler() : this(new Handler()) {}
+
+		public DateTimeOffset Now {
+			get { return DateTimeOffset.Now; }
+		}
+
+		public IDisposable Schedule<TState>(TState state, Func<IScheduler, TState, IDisposable> action)
+		{
+			IDisposable innerDisp = Disposable.Empty;
+			handler.Post (new Action (() => innerDisp = action (this, state)));
+			return innerDisp;
+		}
+
+		public IDisposable Schedule<TState>(TState state, DateTimeOffset dueTime, Func<IScheduler, TState, IDisposable> action)
+		{
+			if (dueTime <= Now) {
+				return Schedule(state, action);
+			}
+
+			return Schedule(state, dueTime - Now, action);
+		}
+
+		public IDisposable Schedule<TState>(TState state, TimeSpan dueTime, Func<IScheduler, TState, IDisposable> action)
+		{
+			bool isCancelled = false;
+
+			var disp = TaskPoolScheduler.Default.Schedule(state, dueTime, (sched, st) => {
+				IDisposable innerDisp = Disposable.Empty;
+
+				if (!isCancelled) { 
+					handler.Post(() => {
+						if (!isCancelled) innerDisp = action(this, state);
+					});
+				}
+
+				return Disposable.Create(() => {
+					isCancelled = true;
+					innerDisp.Dispose();
+				});    
+			});
+
+			return Disposable.Create(() => {
+				isCancelled = true;
+				disp.Dispose();
+			});
+		}
+	}
+}
+
---
+++ ReactiveUI/Android/ReactiveAndroidApplication.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive;
+using System.Reactive.Subjects;
+using Android.App;
+using ReactiveUI;
+
+namespace ReactiveUI
+{
+	public class ReactiveAndroidApplication<TViewModel> : ReactiveAndroidApplication, IViewFor<TViewModel>, ICanActivate
+		where TViewModel : class
+	{
+		protected ReactiveAndroidApplication(IntPtr javaReference, Android.Runtime.JniHandleOwnership transfer) : base(javaReference, transfer)
+		{ }
+
+		TViewModel _ViewModel;
+		public TViewModel ViewModel {
+			get { return _ViewModel; }
+			set { this.RaiseAndSetIfChanged(ref _ViewModel, value); }
+		}
+
+		object IViewFor.ViewModel {
+			get { return _ViewModel; }
+			set { _ViewModel = (TViewModel)value; }
+		}
+	}	
+
+	public class ReactiveAndroidApplication : Application, IReactiveObject, IReactiveNotifyPropertyChanged<ReactiveAndroidApplication>, IHandleObservableErrors
+	{
+		protected ReactiveAndroidApplication(IntPtr javaReference, Android.Runtime.JniHandleOwnership transfer) : base(javaReference, transfer)
+		{
+			RxApp.MainThreadScheduler = new WaitForDispatcherScheduler(() => new AndroidHandlerScheduler());
+		}
+
+		public event PropertyChangingEventHandler PropertyChanging {
+			add { PropertyChangingEventManager.AddHandler(this, value); }
+			remove { PropertyChangingEventManager.RemoveHandler(this, value); }
+		}
+
+		void IReactiveObject.RaisePropertyChanging(PropertyChangingEventArgs args)
+		{
+			PropertyChangingEventManager.DeliverEvent(this, args);
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged {
+			add { PropertyChangedEventManager.AddHandler(this, value); }
+			remove { PropertyChangedEventManager.RemoveHandler(this, value); }
+		}
+
+		void IReactiveObject.RaisePropertyChanged(PropertyChangedEventArgs args)
+		{
+			PropertyChangedEventManager.DeliverEvent(this, args);
+		}
+			
+		public IObservable<IReactivePropertyChangedEventArgs<ReactiveAndroidApplication>> Changing {
+			get { return this.getChangingObservable(); }
+		}
+			
+		public IObservable<IReactivePropertyChangedEventArgs<ReactiveAndroidApplication>> Changed {
+			get { return this.getChangedObservable(); }
+		}
+
+		public IDisposable SuppressChangeNotifications()
+		{
+			return this.suppressChangeNotifications();
+		}
+
+		public IObservable<Exception> ThrownExceptions { get { return this.getThrownExceptionsObservable(); } }
+
+		readonly Subject<Unit> activated = new Subject<Unit>();
+		public IObservable<Unit> Activated { get { return activated; } }
+
+		readonly Subject<Unit> deactivated = new Subject<Unit>();
+		public IObservable<Unit> Deactivated { get { return deactivated; } }
+
+	}
+}
+
