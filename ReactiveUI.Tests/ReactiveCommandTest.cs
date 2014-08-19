8fd62ac1dbf60e17bcce1b7b6271d3f092d7b5f0
Paul Betts <paul@paulbetts.org>
2014-08-19 11:30:19 -0700

    Fix up the test suite

--- ReactiveUI.Tests/ReactiveCommandTest.cs
+++ ReactiveUI.Tests/ReactiveCommandTest.cs
@@ -465,22 +465,22 @@ namespace ReactiveUI.Tests
                 // NB: The first two canExecuteOutputs are because of the initial value
                 // that shows up because we finally ran the scheduler
                 sched.AdvanceToMs(50.0);
-                Assert.Equal(3, canExecuteOutput.Count);
-                Assert.Equal(true, canExecuteOutput[1]);
-                Assert.Equal(false, canExecuteOutput[2]);
+                Assert.Equal(2, canExecuteOutput.Count);
+                Assert.Equal(true, canExecuteOutput[0]);
+                Assert.Equal(false, canExecuteOutput[1]);
                 Assert.Equal(false, fixture.CanExecute(null));
                 Assert.Equal(0, result1.Count);
                 Assert.Equal(0, result2.Count);
 
                 sched.AdvanceToMs(250.0);
-                Assert.Equal(3, canExecuteOutput.Count);
+                Assert.Equal(2, canExecuteOutput.Count);
                 Assert.Equal(false, fixture.CanExecute(null));
                 Assert.Equal(1, result1.Count);
                 Assert.Equal(0, result2.Count);
                                 
                 sched.AdvanceToMs(500.0);
-                Assert.Equal(4, canExecuteOutput.Count);
-                Assert.Equal(true, canExecuteOutput[3]);
+                Assert.Equal(3, canExecuteOutput.Count);
+                Assert.Equal(true, canExecuteOutput[2]);
                 Assert.Equal(true, fixture.CanExecute(null));
                 Assert.Equal(1, result1.Count);
                 Assert.Equal(1, result2.Count);
