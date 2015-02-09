c972fd0b5478225c6627b4bde70e320a55dd8a81
mteper <michael.teper@elloco.com>
2015-02-09 14:57:06 -0800

    Fixed up MakeRelease to point to Xamarin.Mac10

--- MakeRelease.ps1
+++ MakeRelease.ps1
@@ -2,7 +2,7 @@
 
 $Archs = {"Portable-Net45+WinRT45+WP8+MonoAndroid10+MonoTouch10", "Portable-Net45+Win8+WP8+WPA81", "Net45", "WP8",
     "WP81", "Win8", "Win81", "Mono", "Monoandroid", "Monotouch", "Monomac", "Portable-Win81+Wpa81", "WPA81",
-    "Xamarin.iOS10", "Xamarin.Mac"}
+    "Xamarin.iOS10", "Xamarin.Mac10"}
 
 $Projects = {
     "ReactiveUI", "ReactiveUI.Testing", "ReactiveUI.Blend", "ReactiveUI.Winforms", 
