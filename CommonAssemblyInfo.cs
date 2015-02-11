e3cebff4526a60ef416dbbf524c7dff642c02980
Paul Betts <paul@paulbetts.org>
2015-02-11 15:49:49 -0800

    Version bump to 6.4.0 Oops Edition

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("6.4.0")]
-[assembly: AssemblyFileVersion("6.4.0")]
+[assembly: AssemblyVersion("6.4.0.1")]
+[assembly: AssemblyFileVersion("6.4.0.1")]
