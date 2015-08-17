1f120acf522fa3cb788a20ec96a6dbedd8eae6a0
Kent Boogaart <kent.boogaart@gmail.com>
2015-08-17 10:52:03 +0930

    Add reactive cell base classes and corresponding activation behavior.

---
+++ ReactiveUI.XamForms/XamForms/ReactiveTextCell.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+using Xamarin.Forms;
+
+namespace ReactiveUI.XamForms
+{
+    public class ReactiveTextCell<TViewModel> : TextCell, IViewFor<TViewModel>
+        where TViewModel : class
+    {
+        public static readonly BindableProperty ViewModelProperty = BindableProperty.Create<ReactiveTextCell<TViewModel>, TViewModel>(
+            x => x.ViewModel,
+            null,
+            BindingMode.OneWay);
+
+        public TViewModel ViewModel
+        {
+            get { return (TViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        object IViewFor.ViewModel
+        {
+            get { return this.ViewModel; }
+            set { this.ViewModel = (TViewModel)value; }
+        }
+    }
+}
