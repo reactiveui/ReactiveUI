74a75764a6b4d2c40245d39fc67349b4ebd702b5
moswald <moswald@gmail.com>
2015-09-13 13:42:20 -0500

    nope

--- ReactiveUI/Properties/AssemblyInfo.cs
+++ ReactiveUI/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Windows;
 )]
 #endif
 
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyDescription("An MVVM framework that integrates the Reactive Extensions")]
+[assembly: AssemblyProduct("ReactiveUI")]
 [assembly: InternalsVisibleTo("ReactiveUI.Tests_Net45")]
 [assembly: InternalsVisibleTo("ReactiveUI.Tests_WinRT")]
 [assembly: InternalsVisibleTo("ReactiveUI_Tests_iOS")] // NB: iOS apps can't have '.' in the name
