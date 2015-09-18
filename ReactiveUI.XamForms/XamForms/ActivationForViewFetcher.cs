350fd02c0e136720dca7e09f7c0a712f73c58c56
Kent Boogaart <kent@codeheroes.com.au>
2015-09-18 18:41:44 +0930

    Add support for View activation.

--- ReactiveUI.XamForms/XamForms/ActivationForViewFetcher.cs
+++ ReactiveUI.XamForms/XamForms/ActivationForViewFetcher.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Reflection;
 using Xamarin.Forms;
 
@@ -17,6 +14,7 @@ namespace ReactiveUI.XamForms
         {
             return
                 (typeof(Page).GetTypeInfo().IsAssignableFrom(view.GetTypeInfo())) ||
+                (typeof(View).GetTypeInfo().IsAssignableFrom(view.GetTypeInfo())) ||
                 (typeof(Cell).GetTypeInfo().IsAssignableFrom(view.GetTypeInfo()))
                 ? 10 : 0;
         }
@@ -24,6 +22,7 @@ namespace ReactiveUI.XamForms
         public IObservable<bool> GetActivationForView(IActivatable view)
         {
             var ret = Observable.Never<bool>();
+            
 
             var page = view as Page;
 
@@ -32,17 +31,61 @@ namespace ReactiveUI.XamForms
                     Observable.FromEventPattern<EventHandler, EventArgs>(x => page.Appearing += x, x => page.Appearing -= x).Select(_ => true),
                     Observable.FromEventPattern<EventHandler, EventArgs>(x => page.Disappearing += x, x => page.Disappearing -= x).Select(_ => false));
             } else {
-                var cell = view as Cell;
+                var xfView = view as View;
 
-                if (cell != null) {
-                    ret = Observable
-                        .Merge(
-                            Observable.FromEventPattern<EventHandler, EventArgs>(x => cell.Appearing += x, x => cell.Appearing -= x).Select(_ => true),
-                            Observable.FromEventPattern<EventHandler, EventArgs>(x => cell.Disappearing += x, x => cell.Disappearing -= x).Select(_ => false));
+                if (xfView != null) {
+                    var propertyChanged = Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
+                        x => xfView.PropertyChanged += x,
+                        x => xfView.PropertyChanged -= x);
+                    var parentChanged = propertyChanged
+                        .Where(x => x.EventArgs.PropertyName == "Parent")
+                        .Select(_ => Unit.Default);
+
+                    return parentChanged
+                        .StartWith(Unit.Default)
+                        .Select(_ => GetPageFor(xfView))
+                        .Select(x =>
+                            x == null ?
+                                Observable.Return(false) :
+                                Observable
+                                    .Merge(
+                                        Observable.FromEventPattern<EventHandler, EventArgs>(y => x.Appearing += y, y => x.Appearing -= y).Select(_ => true),
+                                        Observable.FromEventPattern<EventHandler, EventArgs>(y => x.Disappearing += y, y => x.Disappearing -= y).Select(_ => false))
+                                    .StartWith(true))
+                        .Switch();
+                } else {
+                    var cell = view as Cell;
+
+                    if (cell != null)
+                    {
+                        ret = Observable
+                            .Merge(
+                                Observable.FromEventPattern<EventHandler, EventArgs>(x => cell.Appearing += x, x => cell.Appearing -= x).Select(_ => true),
+                                Observable.FromEventPattern<EventHandler, EventArgs>(x => cell.Disappearing += x, x => cell.Disappearing -= x).Select(_ => false));
+                    }
                 }
             }
 
             return ret.DistinctUntilChanged();
+        }
+
+        private static Page GetPageFor(Element element)
+        {
+            Page page = null;
+
+            while (element != null)
+            {
+                page = element as Page;
+
+                if (page != null)
+                {
+                    return page;
+                }
+
+                element = element.Parent;
+            }
+
+            return null;
         }
     }
 }
