851876c55b99c6e7c6b94c1228f4dc930e3c27c0
Kent Boogaart <kent.boogaart@gmail.com>
2015-10-22 17:08:36 +1030

    first spike of AsynchronousReactiveCommand<TParam, TResult>

---
+++ ReactiveUI.Tests/NewReactiveCommandTests.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using Microsoft.Reactive.Testing;
+using ReactiveUI.Testing;
+using Xunit;
+
+namespace ReactiveUI.Tests
+{
+    public class AsynchronousReactiveCommandTests
+    {
+        [Fact]
+        public void ConstructorThrowsIfCanExecuteIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new AsynchronousReactiveCommand<Unit, Unit>(null, _ => Observable.Return(Unit.Default)));
+        }
+
+        [Fact]
+        public void ConstructorThrowsIfExecuteAsyncIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new AsynchronousReactiveCommand<Unit, Unit>(null));
+        }
+
+        [Fact]
+        public void ConstructorThrowsIfMaxInFlightExecutionsIsInvalid()
+        {
+            Assert.Throws<ArgumentException>(() => new AsynchronousReactiveCommand<Unit, Unit>(_ => Observable.Return(Unit.Default), maxInFlightExecutions: 0));
+            Assert.Throws<ArgumentException>(() => new AsynchronousReactiveCommand<Unit, Unit>(_ => Observable.Return(Unit.Default), maxInFlightExecutions: -1));
+            Assert.Throws<ArgumentException>(() => new AsynchronousReactiveCommand<Unit, Unit>(_ => Observable.Return(Unit.Default), maxInFlightExecutions: -21));
+        }
+
+        [Fact]
+        public void CanExecuteIsBehavioral()
+        {
+            var fixture = new AsynchronousReactiveCommand<Unit, Unit>(_ => Observable.Return(Unit.Default));
+            var canExecute = fixture
+                .CanExecute
+                .CreateCollection();
+
+            Assert.Equal(1, canExecute.Count);
+            Assert.True(canExecute[0]);
+        }
+
+        [Fact]
+        public void CanExecuteOnlyTicksDistinctValues()
+        {
+            var canExecuteSubject = new Subject<bool>();
+            var fixture = new AsynchronousReactiveCommand<Unit, Unit>(canExecuteSubject, _ => Observable.Return(Unit.Default));
+            var canExecute = fixture
+                .CanExecute
+                .CreateCollection();
+
+            canExecuteSubject.OnNext(false);
+            canExecuteSubject.OnNext(false);
+            canExecuteSubject.OnNext(false);
+            canExecuteSubject.OnNext(false);
+            canExecuteSubject.OnNext(true);
+            canExecuteSubject.OnNext(true);
+
+            Assert.Equal(3, canExecute.Count);
+            Assert.True(canExecute[0]);
+            Assert.False(canExecute[1]);
+            Assert.True(canExecute[2]);
+        }
+
+        [Fact]
+        public void CanExecuteIsFalseIfCallerDictatesAsSuch()
+        {
+            var canExecuteSubject = new Subject<bool>();
+            var fixture = new AsynchronousReactiveCommand<Unit, Unit>(canExecuteSubject, _ => Observable.Return(Unit.Default));
+            var canExecute = fixture
+                .CanExecute
+                .CreateCollection();
+
+            canExecuteSubject.OnNext(false);
+
+            Assert.Equal(2, canExecute.Count);
+            Assert.True(canExecute[0]);
+            Assert.False(canExecute[1]);
+        }
+
+        [Fact]
+        public void CanExecuteIsFalseIfMaxInFlightExecutionsIsReached()
+        {
+            (new TestScheduler()).With(sched =>
+            {
+                var execute = Observable.Return(Unit.Default).Delay(TimeSpan.FromSeconds(1), sched);
+                var fixture = new AsynchronousReactiveCommand<Unit, Unit>(_ => execute, sched, maxInFlightExecutions: 2);
+                var canExecute = fixture
+                    .CanExecute
+                    .CreateCollection();
+
+                fixture.ExecuteAsync();
+                sched.AdvanceByMs(100);
+
+                Assert.Equal(1, canExecute.Count);
+                Assert.True(canExecute[0]);
+
+                fixture.ExecuteAsync();
+                sched.AdvanceByMs(100);
+
+                Assert.Equal(2, canExecute.Count);
+                Assert.False(canExecute[1]);
+
+                sched.AdvanceByMs(900);
+
+                Assert.Equal(3, canExecute.Count);
+                Assert.True(canExecute[2]);
+            });
+        }
+
+        [Fact]
+        public void CanExecuteTicksFailuresThroughThrownExceptions()
+        {
+            var canExecuteSubject = new Subject<bool>();
+            var fixture = new AsynchronousReactiveCommand<Unit, Unit>(canExecuteSubject, _ => Observable.Return(Unit.Default));
+            var thrownExceptions = fixture
+                .ThrownExceptions
+                .CreateCollection();
+
+            canExecuteSubject.OnError(new InvalidOperationException("oops"));
+
+            Assert.Equal(1, thrownExceptions.Count);
+            Assert.Equal("oops", thrownExceptions[0].Message);
+        }
+
+        [Fact]
+        public void MaxInFlightExecutionsDefaultsToOne()
+        {
+            var fixture = new AsynchronousReactiveCommand<Unit, Unit>(_ => Observable.Return(Unit.Default));
+            Assert.Equal(1, fixture.MaxInFlightExecutions);
+        }
+
+        [Fact]
+        public void InFlightExecutionsIsBehavioral()
+        {
+            var fixture = new AsynchronousReactiveCommand<Unit, Unit>(_ => Observable.Return(Unit.Default));
+            var inFlightExecutions = fixture
+                .InFlightExecutions
+                .CreateCollection();
+
+            Assert.Equal(1, inFlightExecutions.Count);
+            Assert.Equal(0, inFlightExecutions[0]);
+        }
+
+        [Fact]
+        public void InFlightExecutionsTicksAsExecutionsStartAndEnd()
+        {
+            (new TestScheduler()).With(sched =>
+            {
+                var execute = Observable.Return(Unit.Default).Delay(TimeSpan.FromSeconds(1), sched);
+                var fixture = new AsynchronousReactiveCommand<Unit, Unit>(_ => execute, sched, maxInFlightExecutions: 2);
+                var inFlightExecutions = fixture
+                    .InFlightExecutions
+                    .CreateCollection();
+
+                fixture.ExecuteAsync();
+                sched.AdvanceByMs(100);
+
+                Assert.Equal(2, inFlightExecutions.Count);
+                Assert.Equal(0, inFlightExecutions[0]);
+                Assert.Equal(1, inFlightExecutions[1]);
+
+                fixture.ExecuteAsync();
+                sched.AdvanceByMs(100);
+
+                Assert.Equal(3, inFlightExecutions.Count);
+                Assert.Equal(2, inFlightExecutions[2]);
+
+                sched.AdvanceByMs(900);
+
+                Assert.Equal(4, inFlightExecutions.Count);
+                Assert.Equal(1, inFlightExecutions[3]);
+
+                sched.AdvanceByMs(900);
+
+                Assert.Equal(5, inFlightExecutions.Count);
+                Assert.Equal(0, inFlightExecutions[4]);
+            });
+        }
+
+        [Fact]
+        public void IsExecutingIsBehavioral()
+        {
+            var fixture = new AsynchronousReactiveCommand<Unit, Unit>(_ => Observable.Return(Unit.Default));
+            var isExecuting = fixture
+                .IsExecuting
+                .CreateCollection();
+
+            Assert.Equal(1, isExecuting.Count);
+            Assert.False(isExecuting[0]);
+        }
+
+        [Fact]
+        public void IsExecutingTicksAsExecutionsProgress()
+        {
+            (new TestScheduler()).With(sched =>
+            {
+                var execute = Observable.Return(Unit.Default).Delay(TimeSpan.FromSeconds(1), sched);
+                var fixture = new AsynchronousReactiveCommand<Unit, Unit>(_ => execute, sched, maxInFlightExecutions: 2);
+                var isExecuting = fixture
+                    .IsExecuting
+                    .CreateCollection();
+
+                fixture.ExecuteAsync();
+                sched.AdvanceByMs(100);
+
+                Assert.Equal(2, isExecuting.Count);
+                Assert.False(isExecuting[0]);
+                Assert.True(isExecuting[1]);
+
+                fixture.ExecuteAsync();
+                sched.AdvanceByMs(100);
+
+                Assert.Equal(2, isExecuting.Count);
+
+                sched.AdvanceByMs(900);
+
+                Assert.Equal(2, isExecuting.Count);
+
+                sched.AdvanceByMs(900);
+
+                Assert.Equal(3, isExecuting.Count);
+                Assert.False(isExecuting[2]);
+            });
+        }
+
+        [Fact]
+        public void ExecuteAsyncPassesThroughParameter()
+        {
+            var parameters = new List<int>();
+            var fixture = new AsynchronousReactiveCommand<int, Unit>(
+                param =>
+                {
+                    parameters.Add(param);
+                    return Observable.Return(Unit.Default);
+                });
+
+            fixture.ExecuteAsync(1);
+            fixture.ExecuteAsync(42);
+            fixture.ExecuteAsync(348);
+
+            Assert.Equal(3, parameters.Count);
+            Assert.Equal(1, parameters[0]);
+            Assert.Equal(42, parameters[1]);
+            Assert.Equal(348, parameters[2]);
+        }
+
+        [Fact]
+        public void ExecuteAsyncFailsIfThereAreTooManyExecutionsInFlight()
+        {
+            (new TestScheduler()).With(sched =>
+            {
+                var execute = Observable.Return(Unit.Default).Delay(TimeSpan.FromSeconds(1), sched);
+                var fixture = new AsynchronousReactiveCommand<Unit, Unit>(_ => execute, sched, maxInFlightExecutions: 2);
+                var thrownExceptions = fixture
+                    .ThrownExceptions
+                    .CreateCollection();
+
+                fixture.ExecuteAsync();
+                sched.AdvanceByMs(100);
+
+                fixture.ExecuteAsync();
+                sched.AdvanceByMs(100);
+
+                Assert.Empty(thrownExceptions);
+
+                fixture.ExecuteAsync();
+                sched.AdvanceByMs(1);
+
+                Assert.Equal(1, thrownExceptions.Count);
+                Assert.IsType<InvalidOperationException>(thrownExceptions[0]);
+                Assert.Equal("No more executions can be performed because the maximum number of in-flight executions (2) has been reached.", thrownExceptions[0].Message);
+            });
+        }
+
+        [Fact]
+        public void ExecuteAsyncExecutesOnTheSpecifiedScheduler()
+        {
+            (new TestScheduler()).With(sched =>
+            {
+                var execute = Observable.Return(Unit.Default).Delay(TimeSpan.FromSeconds(1), sched);
+                var fixture = new AsynchronousReactiveCommand<Unit, Unit>(_ => execute, sched);
+                var isExecuting = fixture
+                    .IsExecuting
+                    .CreateCollection();
+
+                fixture.ExecuteAsync();
+                sched.AdvanceByMs(999);
+
+                Assert.Equal(2, isExecuting.Count);
+                Assert.False(isExecuting[0]);
+                Assert.True(isExecuting[1]);
+
+                sched.AdvanceByMs(2);
+
+                Assert.Equal(3, isExecuting.Count);
+                Assert.False(isExecuting[2]);
+            });
+        }
+
+        [Fact]
+        public void ExecuteAsyncExecutesEvenWithoutASubscription()
+        {
+            (new TestScheduler()).With(sched =>
+            {
+                var execute = Observable.Return(Unit.Default).Delay(TimeSpan.FromSeconds(1), sched);
+                var fixture = new AsynchronousReactiveCommand<Unit, Unit>(_ => execute, sched);
+                var isExecuting = fixture
+                    .IsExecuting
+                    .CreateCollection();
+
+                fixture.ExecuteAsync();
+                sched.AdvanceByMs(1);
+
+                Assert.Equal(2, isExecuting.Count);
+                Assert.False(isExecuting[0]);
+                Assert.True(isExecuting[1]);
+            });
+        }
+
+        [Fact]
+        public void ExecuteAsyncTicksThroughTheResult()
+        {
+            var num = 0;
+            var fixture = new AsynchronousReactiveCommand<Unit, int>(_ => Observable.Return(num)/*, sched*/);
+            var results = fixture
+                .CreateCollection();
+
+            num = 1;
+            fixture.ExecuteAsync();
+            num = 10;
+            fixture.ExecuteAsync();
+            num = 30;
+            fixture.ExecuteAsync();
+
+            Assert.Equal(3, results.Count);
+            Assert.Equal(1, results[0]);
+            Assert.Equal(10, results[1]);
+            Assert.Equal(30, results[2]);
+        }
+
+        [Fact]
+        public void ExecuteAsyncTicksErrorsThroughThrownExceptions()
+        {
+            var fixture = new AsynchronousReactiveCommand<Unit, Unit>(_ => Observable.Throw<Unit>(new InvalidOperationException("oops")));
+            var thrownExceptions = fixture
+                .ThrownExceptions
+                .CreateCollection();
+
+            fixture.ExecuteAsync();
+
+            Assert.Equal(1, thrownExceptions.Count);
+            Assert.Equal("oops", thrownExceptions[0].Message);
+        }
+
+        [Fact]
+        public void ExecuteAsyncReenablesExecutionEvenAfterFailure()
+        {
+            var fixture = new AsynchronousReactiveCommand<Unit, Unit>(_ => Observable.Throw<Unit>(new InvalidOperationException("oops")));
+            var canExecute = fixture
+                .CanExecute
+                .CreateCollection();
+            var thrownExceptions = fixture
+                .ThrownExceptions
+                .CreateCollection();
+
+            fixture.ExecuteAsync();
+
+            Assert.Equal(1, thrownExceptions.Count);
+            Assert.Equal("oops", thrownExceptions[0].Message);
+
+            Assert.Equal(3, canExecute.Count);
+            Assert.True(canExecute[0]);
+            Assert.False(canExecute[1]);
+            Assert.True(canExecute[2]);
+        }
+    }
+}
---
+++ ReactiveUI/NewReactiveCommand.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace ReactiveUI
+{
+    // DO AN IDEAL IMPLEMENTATION FIRST, THEN SCALE BACK AS REQUIRED
+    // too much type explosion?
+
+    public abstract class NewReactiveCommand
+    {
+    }
+
+    public abstract class NewReactiveCommand<TResult> : NewReactiveCommand, IObservable<TResult>
+    {
+        public abstract IDisposable Subscribe(IObserver<TResult> observer);
+    }
+
+    public class SynchronousReactiveCommand<TParam, TResult> : NewReactiveCommand<TResult>
+    {
+        public override IDisposable Subscribe(IObserver<TResult> observer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // incorporates a given reactive pipeline into each call to ExecuteAsync
+    // if the number of in-flight executions is maxInFlightExecutions, an exception is thrown instead
+    // 
+    // will have SynchronousReactiveCommand counterpart
+    public class AsynchronousReactiveCommand<TParam, TResult> : NewReactiveCommand<TResult>
+    {
+        private readonly Func<TParam, IObservable<TResult>> executeAsync;
+        private readonly IScheduler scheduler;
+        private readonly int maxInFlightExecutions;
+        private readonly BehaviorSubject<int> inFlightExecutions;
+        private readonly BehaviorSubject<bool> canExecute;
+        private readonly Subject<TResult> results = new Subject<TResult>();
+        private readonly ScheduledSubject<Exception> exceptions;
+        private readonly IDisposable canExecuteSubscription;
+
+        public AsynchronousReactiveCommand(
+                Func<TParam, IObservable<TResult>> executeAsync,
+                IScheduler scheduler = null,
+                int maxInFlightExecutions = 1)
+            : this(Observable.Return(true), executeAsync, scheduler, maxInFlightExecutions)
+        {
+        }
+
+        public AsynchronousReactiveCommand(
+            IObservable<bool> canExecute,
+            Func<TParam, IObservable<TResult>> executeAsync,
+            IScheduler scheduler = null,
+            int maxInFlightExecutions = 1)
+        {
+            if (canExecute == null)
+            {
+                throw new ArgumentNullException("canExecute");
+            }
+
+            if (executeAsync == null)
+            {
+                throw new ArgumentNullException("executeAsync");
+            }
+
+            if (maxInFlightExecutions < 1)
+            {
+                throw new ArgumentException("maxInFlightExecutions must be greater than zero.");
+            }
+
+            this.executeAsync = executeAsync;
+            this.scheduler = scheduler ?? RxApp.MainThreadScheduler;
+            this.maxInFlightExecutions = maxInFlightExecutions;
+            this.inFlightExecutions = new BehaviorSubject<int>(0);
+            this.canExecute = new BehaviorSubject<bool>(true);
+            this.results = new Subject<TResult>();
+
+            this.canExecuteSubscription = canExecute
+                .CombineLatest(this.inFlightExecutions, (canEx, inFlight) => canEx && inFlight < this.maxInFlightExecutions)
+                .Catch<bool, Exception>(
+                    ex =>
+                    {
+                        exceptions.OnNext(ex);
+                        return Observable.Return(false);
+                    })
+                .DistinctUntilChanged()
+                .Subscribe(x => this.canExecute.OnNext(x));
+
+            this.ThrownExceptions = this.exceptions = new ScheduledSubject<Exception>(CurrentThreadScheduler.Instance, RxApp.DefaultExceptionHandler);
+        }
+
+        public int MaxInFlightExecutions => this.maxInFlightExecutions;
+
+        public IObservable<bool> CanExecute => this.canExecute;
+
+        public IObservable<int> InFlightExecutions => this.inFlightExecutions;
+
+        public IObservable<bool> IsExecuting => this.inFlightExecutions.Select(x => x > 0).DistinctUntilChanged();
+
+        public IObservable<Exception> ThrownExceptions
+        {
+            get;
+            protected set;
+        }
+
+        public override IDisposable Subscribe(IObserver<TResult> observer) =>
+            results.Subscribe(observer);
+
+        public IObservable<TResult> ExecuteAsync(TParam parameter = default(TParam))
+        {
+            var execution = Observable
+                .Start(
+                    () =>
+                    {
+                        var inFlightExecutions = this.inFlightExecutions.Value;
+
+                        if (inFlightExecutions < this.maxInFlightExecutions)
+                        {
+                            ++inFlightExecutions;
+                            this.inFlightExecutions.OnNext(inFlightExecutions);
+                            return this.ExecuteCoreAsync(parameter);
+                        }
+
+                        return Observable.Throw<TResult>(
+                            new InvalidOperationException(
+                                string.Format("No more executions can be performed because the maximum number of in-flight executions ({0}) has been reached.", this.maxInFlightExecutions)));
+                    },
+                    this.scheduler)
+                .Switch()
+                .Publish()
+                .RefCount();
+
+            execution
+                .Subscribe(
+                    _ => { },
+                    ex => exceptions.OnNext(ex));
+
+            return execution;
+        }
+
+        private IObservable<TResult> ExecuteCoreAsync(TParam parameter) =>
+            this
+                .executeAsync(parameter)
+                .Do(result => this.results.OnNext(result))
+                .Finally(
+                    () =>
+                    {
+                        var inFlightExecutions = this.inFlightExecutions.Value;
+                        --inFlightExecutions;
+                        this.inFlightExecutions.OnNext(inFlightExecutions);
+                    });
+    }
+}
