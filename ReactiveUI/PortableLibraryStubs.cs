c8a8292fde7a0617338110c55bfbf8c7d6947e1d
Olexandr Leuschenko <ole@ciklum.com>
2015-12-26 22:16:08 +0200

    PlatformRegistrations should work from unit tests

--- ReactiveUI/PortableLibraryStubs.cs
+++ ReactiveUI/PortableLibraryStubs.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace ReactiveUI
 {
@@ -10,7 +8,16 @@ namespace ReactiveUI
     {
         public void Register(Action<Func<object>, Type> registerFunction)
         {
+            if (IsRunningFromNUnit()) return;
             throw new Exception("You are referencing the Portable version of ReactiveUI in an App. Reference the platform-specific version.");
+        }
+
+        private bool IsRunningFromNUnit()
+        {
+            var currentdomain = typeof(string).GetTypeInfo().Assembly.GetType("System.AppDomain").GetRuntimeProperty("CurrentDomain").GetMethod.Invoke(null, new object[] { });
+            var getassemblies = currentdomain.GetType().GetRuntimeMethod("GetAssemblies", new Type[]{ });
+            var assemblies = getassemblies.Invoke(currentdomain, new object[]{ }) as Assembly[];
+            return assemblies.Any(assembly => assembly.FullName.ToLowerInvariant().StartsWith("nunit.framework"));
         }
     }
 }
