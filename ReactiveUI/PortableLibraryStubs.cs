42a6061a7574948884b17698d8adfda30be0a279
Olexandr Leuschenko <ole@ciklum.com>
2015-12-27 13:59:30 +0200

    unit test runner detector replaced by Splat.ModeDetector

--- ReactiveUI/PortableLibraryStubs.cs
+++ ReactiveUI/PortableLibraryStubs.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Reflection;
 
 namespace ReactiveUI
 {
@@ -8,16 +6,8 @@ namespace ReactiveUI
     {
         public void Register(Action<Func<object>, Type> registerFunction)
         {
-            if (IsRunningFromNUnit()) return;
+            if (Splat.ModeDetector.InUnitTestRunner()) return;
             throw new Exception("You are referencing the Portable version of ReactiveUI in an App. Reference the platform-specific version.");
-        }
-
-        private bool IsRunningFromNUnit()
-        {
-            var currentdomain = typeof(string).GetTypeInfo().Assembly.GetType("System.AppDomain").GetRuntimeProperty("CurrentDomain").GetMethod.Invoke(null, new object[] { });
-            var getassemblies = currentdomain.GetType().GetRuntimeMethod("GetAssemblies", new Type[]{ });
-            var assemblies = getassemblies.Invoke(currentdomain, new object[]{ }) as Assembly[];
-            return assemblies.Any(assembly => assembly.FullName.ToLowerInvariant().StartsWith("nunit.framework"));
         }
     }
 }
