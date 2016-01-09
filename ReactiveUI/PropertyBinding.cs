467051f4704ed3dca73ba474e68b11381375d80e
Matt Richard <matt@richardsoftware.com>
2016-01-09 00:18:37 -0600

    Add friendly error when you Do It Wrong on BindTo
    If you observable.BindTo(null, ...), when that binding executes you'll
    get a "Non-static method requires a target" exception which can be
    difficult to debug.  Instead, throw an ArgumentNullException if the user
    tries to create such a binding.

--- ReactiveUI/PropertyBinding.cs
+++ ReactiveUI/PropertyBinding.cs
@@ -906,6 +906,11 @@ namespace ReactiveUI
             object conversionHint = null,
             IBindingTypeConverter vmToViewConverterOverride = null)
         {
+
+            if (target == null) {
+                throw new ArgumentNullException("target", "Can't bind to a property on null.  To fix this, you probably want BindTo(this, ...) isntead. ");
+            }
+
             var viewExpression = Reflection.Rewrite(property.Body);
 
             var ret = evalBindingHooks(This, target, null, viewExpression, BindingDirection.OneWay);
