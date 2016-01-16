c6db45a89a7c6239a80b358fdaee6a3f2c82e8bb
Michael Stonis <michael.stonis@eightbot.com>
2016-01-15 22:46:17 -0600

    Adds Reactive implementations of remaining Xamarin.Forms pages

---
+++ ReactiveUI.XamForms/XamForms/ReactiveCarouselPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ReactiveUI;
+using Xamarin.Forms;
+
+namespace ReactiveUI.XamForms
+{
+    public class ReactiveCarouselPage<TViewModel> : CarouselPage, IViewFor<TViewModel>
+        where TViewModel : class
+    {
+        /// <summary>
+        /// The ViewModel to display
+        /// </summary>
+        public TViewModel ViewModel {
+            get { return (TViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+        public static readonly BindableProperty ViewModelProperty = 
+            BindableProperty.Create<ReactiveContentPage<TViewModel>, TViewModel>(x => x.ViewModel, default(TViewModel), BindingMode.OneWay);
+
+        object IViewFor.ViewModel {
+            get { return ViewModel; }
+            set { ViewModel = (TViewModel)value; }
+        }
+    }
+}
