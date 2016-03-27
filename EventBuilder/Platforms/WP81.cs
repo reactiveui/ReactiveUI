959895085bcc0d55fb5dc21a864f785224d681a2
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-03-27 20:46:45 +1100

    generating and building events for wp81 (aka silverlight) working

--- EventBuilder/Platforms/WP81.cs
+++ EventBuilder/Platforms/WP81.cs
@@ -2,13 +2,13 @@
 
 namespace EventBuilder.Platforms
 {
-    public class WP8 : BasePlatform
+    public class WP81 : BasePlatform
     {
-        public WP8()
+        public WP81()
         {
             if (PlatformHelper.IsRunningOnMono())
             {
-                throw new NotSupportedException("Building events for WP8 on Mac is not implemented for obvious reasons.");
+                throw new NotSupportedException("Building events for WP81 on Mac is not implemented yet.");
             }
             Assemblies.Add(
                 @"C:\Program Files (x86)\Windows Phone Silverlight Kits\8.1\Windows MetaData\Windows.winmd");
