ecdf0bf5c6ccfd1e69be87f23f28a02b256ad593
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-03-28 21:17:49 +1100

    removed duplicate assemblyinfo

--- src/ReactiveUI.Winforms/Properties/AssemblyInfo.cs
+++ src/ReactiveUI.Winforms/Properties/AssemblyInfo.cs
@@ -3,6 +3,4 @@ using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 using System.Windows;
 
-[assembly: AssemblyDescription("An MVVM framework that integrates the Reactive Extensions")]
-[assembly: AssemblyProduct("ReactiveUI.Winforms")]
 [assembly: InternalsVisibleTo("ReactiveUI.Tests_Net45")]
