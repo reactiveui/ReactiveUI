43c1857c09762f26aa8beeacac7db0b7a06e30c9
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-03-30 19:03:13 +1100

    migrated iOS away from monotouch to xamarin.ios10 (unified)

--- src/ReactiveUI.Tests/BindingTypeConvertersTest.cs
+++ src/ReactiveUI.Tests/BindingTypeConvertersTest.cs
@@ -1,4 +1,4 @@
-﻿using Xunit;
+using Xunit;
 
 namespace ReactiveUI.Tests
 {
--- src/ReactiveUI.Tests/InteractionsTest.cs
+++ src/ReactiveUI.Tests/InteractionsTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reactive;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
