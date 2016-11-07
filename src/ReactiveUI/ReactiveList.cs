984247dead9591e44023fb34857ecf2ac8bb32a3
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-11-07 22:53:05 +1100

    revert: mintarget is no longer 460 as 452 is still supported (#1195)
    This reverts commit 9738872c2daa3b2fd2add11200a5c91a68879272.

--- src/ReactiveUI/ReactiveList.cs
+++ src/ReactiveUI/ReactiveList.cs
@@ -23,7 +23,7 @@ namespace ReactiveUI
     [DebuggerTypeProxy(typeof(CollectionDebugView<>))]
     public class ReactiveList<T> : IReactiveList<T>, IReadOnlyReactiveList<T>, IList
     {
-#if NET_46
+#if NET_45
         public event NotifyCollectionChangedEventHandler CollectionChanging;
 
         protected virtual void raiseCollectionChanging(NotifyCollectionChangedEventArgs args)
