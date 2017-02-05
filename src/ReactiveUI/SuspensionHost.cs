a7c7f9cec78d6d23092279598a89c74aa5dd862a
Olly Levett <oliver.levett@softwire.com>
2017-02-05 01:15:46 +0000

    chore: rename MobileLifecycle.cs to SuspensionHost.cs (#1252)
    fixes #1246

--- src/ReactiveUI/SuspensionHost.cs
+++ src/ReactiveUI/SuspensionHost.cs
@@ -1,40 +1,45 @@
 using System;
 using System.Reactive;
-using System.Reactive.Linq;
-using Splat;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using Splat;
 
 namespace ReactiveUI
 {
     internal class SuspensionHost : ReactiveObject, ISuspensionHost
     {
         readonly ReplaySubject<IObservable<Unit>> isLaunchingNew = new ReplaySubject<IObservable<Unit>>(1);
-        public IObservable<Unit> IsLaunchingNew {
+        public IObservable<Unit> IsLaunchingNew
+        {
             get { return isLaunchingNew.Switch(); }
             set { isLaunchingNew.OnNext(value); }
         }
 
         readonly ReplaySubject<IObservable<Unit>> isResuming = new ReplaySubject<IObservable<Unit>>(1);
-        public IObservable<Unit> IsResuming {
+        public IObservable<Unit> IsResuming
+        {
             get { return isResuming.Switch(); }
             set { isResuming.OnNext(value); }
         }
 
         readonly ReplaySubject<IObservable<Unit>> isUnpausing = new ReplaySubject<IObservable<Unit>>(1);
-        public IObservable<Unit> IsUnpausing {
+        public IObservable<Unit> IsUnpausing
+        {
             get { return isUnpausing.Switch(); }
             set { isUnpausing.OnNext(value); }
         }
 
         readonly ReplaySubject<IObservable<IDisposable>> shouldPersistState = new ReplaySubject<IObservable<IDisposable>>(1);
-        public IObservable<IDisposable> ShouldPersistState {
+        public IObservable<IDisposable> ShouldPersistState
+        {
             get { return shouldPersistState.Switch(); }
             set { shouldPersistState.OnNext(value); }
         }
 
         readonly ReplaySubject<IObservable<Unit>> shouldInvalidateState = new ReplaySubject<IObservable<Unit>>(1);
-        public IObservable<Unit> ShouldInvalidateState {
+        public IObservable<Unit> ShouldInvalidateState
+        {
             get { return shouldInvalidateState.Switch(); }
             set { shouldInvalidateState.OnNext(value); }
         }
@@ -49,7 +54,8 @@ namespace ReactiveUI
         /// <summary>
         ///
         /// </summary>
-        public object AppState {
+        public object AppState
+        {
             get { return appState; }
             set { this.RaiseAndSetIfChanged(ref appState, value); }
         }
@@ -95,7 +101,7 @@ namespace ReactiveUI
         {
             return (T)This.AppState;
         }
-                
+
         /// <summary>
         ///
         /// </summary>
