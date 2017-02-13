42dfbaf1ebff4267886ee674ce61af5158874cd7
Kent Boogaart <kent.boogaart@gmail.com>
2017-02-13 19:19:33 +1030

    Fix typo

--- src/ReactiveUI.XamForms/XamForms/BindingContextHelper.cs
+++ src/ReactiveUI.XamForms/XamForms/BindingContextHelper.cs
@@ -5,7 +5,7 @@
         public static void AssignViewModelFromBindingContext<TViewModel>(this IViewFor<TViewModel> This, object bindingContext)
             where TViewModel : class
         {
-            // only 't assign the VM if the binding context is explicitly null, or is an object of type TViewModel. This allows for
+            // only assign the VM if the binding context is explicitly null, or is an object of type TViewModel. This allows for
             // the ViewModel property to be set to one object, whilst the BindingContext can be set to something else.
 
             if (bindingContext == null) {
