2d8c30fe846615a709bb85f38ff5a8cd5af54c76
Olly Levett <oliver.levett@softwire.com>
2017-03-14 07:40:47 +0000

    refactor: removed unused extension method and make class internal (#1317)

--- src/ReactiveUI/CompatMixins.cs
+++ src/ReactiveUI/CompatMixins.cs
@@ -1,28 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Subjects;
 
 namespace ReactiveUI
 {
-    public static class CompatMixins
+    internal static class CompatMixins
     {
         internal static void ForEach<T>(this IEnumerable<T> This, Action<T> block)
         {
             foreach (var v in This) {
-                block(v); 
+                block(v);
             }
         }
 
         internal static IEnumerable<T> SkipLast<T>(this IEnumerable<T> This, int count)
         {
             return This.Take(This.Count() - count);
-        }
-
-        internal static IObservable<T> PermaRef<T>(this IConnectableObservable<T> This)
-        {
-            This.Connect();
-            return This;
         }
     }
 
