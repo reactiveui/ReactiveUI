c6534f9b0dcf4420f07326c4d650215df1b2aff1
Olly Levett <oliver.levett@softwire.com>
2017-03-14 13:52:12 +0000

    chore: tidy up XMLDOC comments for ObservedChangedMixin

--- src/ReactiveUI/ObservedChangedMixin.cs
+++ src/ReactiveUI/ObservedChangedMixin.cs
@@ -5,12 +5,17 @@ using System.Reactive.Linq;
 
 namespace ReactiveUI
 {
+    /// <summary>
+    /// A collection of helpers for <see cref="IObservedChange{TSender, TValue}"/>.
+    /// </summary>
     public static class ObservedChangedMixin
     {
         /// <summary>
         /// Returns the name of a property which has been changed.
         /// </summary>
-        /// <returns>The name of the property which has change</returns>
+        /// <returns>
+        /// The name of the property which has changed.
+        /// </returns>
         public static string GetPropertyName<TSender, TValue>(this IObservedChange<TSender, TValue> This)
         {
             return Reflection.ExpressionToPropertyNames(This.Expression);
@@ -20,7 +25,12 @@ namespace ReactiveUI
         /// Returns the current value of a property given a notification that
         /// it has changed.
         /// </summary>
-        /// <returns>The current value of the property</returns>
+        /// <param name="This">
+        /// The <see cref="IObservedChange{TSender, TValue}"/> instance to get the value of.
+        /// </param>
+        /// <returns>
+        /// The current value of the property
+        /// </returns>
         public static TValue GetValue<TSender, TValue>(this IObservedChange<TSender, TValue> This)
         {
             TValue ret;
@@ -35,10 +45,15 @@ namespace ReactiveUI
         /// notification that it has changed. If any property in the
         /// property expression is null, false is returned.
         /// </summary>
-        /// <param name="changeValue">The value of the property
-        /// expression.</param>
-        /// <returns>True if the entire expression was able to be followed,
-        /// false otherwise</returns>
+        /// <param name="This">
+        /// The <see cref="IObservedChange{TSender, TValue}"/> instance to get the value of
+        /// </param>
+        /// <param name="changeValue">
+        /// The value of the property expression.
+        /// </param>
+        /// <returns>
+        /// True if the entire expression was able to be followed, false otherwise.
+        /// </returns>
         internal static bool TryGetValue<TSender, TValue>(this IObservedChange<TSender, TValue> This, out TValue changeValue)
         {
             if (!Equals(This.Value, default(TValue))) {
@@ -53,10 +68,18 @@ namespace ReactiveUI
         /// Given a fully filled-out IObservedChange object, SetValueToProperty
         /// will apply it to the specified object (i.e. it will ensure that
         /// target.property == This.GetValue() and "replay" the observed change
-        /// onto another object)
+        /// onto another object).
         /// </summary>
-        /// <param name="target">The target object to apply the change to.</param>
-        /// <param name="property">The target property to apply the change to.</param>
+        /// <param name="This">
+        /// The <see cref="IObservedChange{TSender, TValue}"/> instance to use as a 
+        /// value to apply.
+        /// </param>
+        /// <param name="target">
+        /// The target object to apply the change to.
+        /// </param>
+        /// <param name="property">
+        /// The target property to apply the change to.
+        /// </param>
         internal static void SetValueToProperty<TSender, TValue, TTarget>(
             this IObservedChange<TSender, TValue> This,
             TTarget target,
@@ -69,8 +92,13 @@ namespace ReactiveUI
         /// Given a stream of notification changes, this method will convert 
         /// the property changes to the current value of the property.
         /// </summary>
-        /// <returns>An Observable representing the stream of current values of
-        /// the given change notification stream.</returns>
+        /// <param name="This">
+        /// The change notification stream to get the values of.
+        /// </param>
+        /// <returns>
+        /// An Observable representing the stream of current values of
+        /// the given change notification stream.
+        /// </returns>
         public static IObservable<TValue> Value<TSender, TValue>(
             this IObservable<IObservedChange<TSender, TValue>> This)
         {
