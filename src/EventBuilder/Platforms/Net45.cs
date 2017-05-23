6c19cac7d50416f7e660e6de7ec9015e230002ac
Geoffrey Huntley <ghuntley@ghuntley.com>
2017-05-23 16:40:52 +0800

    fix: add windows.forms events to reactiveui-events

--- src/EventBuilder/Platforms/Net45.cs
+++ src/EventBuilder/Platforms/Net45.cs
@@ -20,6 +20,7 @@ namespace EventBuilder.Platforms
                 Assemblies.Add(@"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\WindowsBase.dll");
                 Assemblies.Add(@"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\PresentationCore.dll");
                 Assemblies.Add(@"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\PresentationFramework.dll");
+                Assemblies.Add(@"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.Windows.Forms.dll");
 
                 CecilSearchDirectories.Add(@"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5");
 
