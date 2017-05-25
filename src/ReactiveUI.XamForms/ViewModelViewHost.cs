1f04c3daaf591c9b75601fdc200d659d17d59c8f
Kent Boogaart <kent.boogaart@gmail.com>
2017-05-25 20:40:19 +0930

    docs: fix formatting and improve API docs for XF's VMVH (#1362)

--- src/ReactiveUI.XamForms/ViewModelViewHost.cs
+++ src/ReactiveUI.XamForms/ViewModelViewHost.cs
@@ -1,27 +1,29 @@
 using System;
-using System.Reactive;
 using System.Reactive.Linq;
-using System.Reactive.Subjects;
-using System.Windows;
 using Splat;
 using Xamarin.Forms;
 
 namespace ReactiveUI.XamForms
 {
     /// <summary>
-    /// This content control will automatically load the View associated with
-    /// the ViewModel property and display it. This control is very useful
-    /// inside a DataTemplate to display the View associated with a ViewModel.
+    /// This content view will automatically load and host the view for the given view model. The view model whose view is
+    /// to be displayed should be assigned to the <see cref="ViewModel"/> property. Optionally, the chosen view can be
+    /// customized by specifying a contract via <see cref="ViewContractObservable"/>.
     /// </summary>
     public class ViewModelViewHost : ContentView, IViewFor
     {
         /// <summary>
-        /// The ViewModel to display
+        /// The view model whose associated view is to be displayed.
         /// </summary>
-        public object ViewModel {
+        public object ViewModel
+        {
             get { return GetValue(ViewModelProperty); }
             set { SetValue(ViewModelProperty, value); }
         }
+
+        /// <summary>
+        /// Identifies the <see cref="ViewModel"/> property.
+        /// </summary>
         public static readonly BindableProperty ViewModelProperty = BindableProperty.Create(
             nameof(ViewModel),
             typeof(object),
@@ -30,12 +32,17 @@ namespace ReactiveUI.XamForms
             BindingMode.OneWay);
 
         /// <summary>
-        /// If no ViewModel is displayed, this content (i.e. a control) will be displayed.
+        /// The content to display when <see cref="ViewModel"/> is <see langword="null"/>.
         /// </summary>
-        public View DefaultContent {
+        public View DefaultContent
+        {
             get { return (View)GetValue(DefaultContentProperty); }
             set { SetValue(DefaultContentProperty, value); }
         }
+
+        /// <summary>
+        /// Identifies the <see cref="DefaultContent"/> property.
+        /// </summary>
         public static readonly BindableProperty DefaultContentProperty = BindableProperty.Create(
             nameof(DefaultContent),
             typeof(View),
@@ -43,10 +50,18 @@ namespace ReactiveUI.XamForms
             default(View),
             BindingMode.OneWay);
 
-        public IObservable<string> ViewContractObservable {
+        /// <summary>
+        /// The contract to use when resolving the view for the given view model.
+        /// </summary>
+        public IObservable<string> ViewContractObservable
+        {
             get { return (IObservable<string>)GetValue(ViewContractObservableProperty); }
             set { SetValue(ViewContractObservableProperty, value); }
         }
+
+        /// <summary>
+        /// Identifies the <see cref="ViewContractObservable"/> property.
+        /// </summary>
         public static readonly BindableProperty ViewContractObservableProperty = BindableProperty.Create(
             nameof(ViewContractObservable),
             typeof(IObservable<string>),
@@ -54,6 +69,9 @@ namespace ReactiveUI.XamForms
             Observable<string>.Never,
             BindingMode.OneWay);
 
+        /// <summary>
+        /// Can be used to override the view locator to use when resolving the view. If unspecified, <see cref="ViewLocator.Current"/> will be used.
+        /// </summary>
         public IViewLocator ViewLocator { get; set; }
 
         public ViewModelViewHost()
@@ -70,6 +88,7 @@ namespace ReactiveUI.XamForms
                 (vm, contract) => new { ViewModel = vm, Contract = contract, });
 
             var platform = Locator.Current.GetService<IPlatformOperations>();
+
             if (platform == null) {
                 throw new Exception("Couldn't find an IPlatformOperations. This should never happen, your dependency resolver is broken");
             }
@@ -81,29 +100,29 @@ namespace ReactiveUI.XamForms
                 .Select(x => x != null ? x.ToString() : default(string));
 
             (this as IViewFor).WhenActivated(() => {
-                return new[] { vmAndContract.Subscribe(x => {
-                    if (x.ViewModel == null) {
-                        this.Content = this.DefaultContent;
-                        return;
-                    }
+                return new[] {
+                    vmAndContract.Subscribe(x => {
+                        if (x.ViewModel == null) {
+                            this.Content = this.DefaultContent;
+                            return;
+                        }
 
-                    var viewLocator = ViewLocator ?? ReactiveUI.ViewLocator.Current;
-                    var view = viewLocator.ResolveView(x.ViewModel, x.Contract) ?? viewLocator.ResolveView(x.ViewModel, null);
+                        var viewLocator = ViewLocator ?? ReactiveUI.ViewLocator.Current;
+                        var view = viewLocator.ResolveView(x.ViewModel, x.Contract) ?? viewLocator.ResolveView(x.ViewModel, null);
 
-                    if (view == null) {
-                        throw new Exception(String.Format("Couldn't find view for '{0}'.", x.ViewModel));
-                    }
+                        if (view == null) {
+                            throw new Exception(String.Format("Couldn't find view for '{0}'.", x.ViewModel));
+                        }
 
-                    var castView = view as View;
+                        var castView = view as View;
 
-                    if (castView == null) {
-                        throw new Exception(String.Format("View '{0}' is not a subclass of '{1}'.", view.GetType().FullName, typeof(View).FullName));
-                    }
+                        if (castView == null) {
+                            throw new Exception(String.Format("View '{0}' is not a subclass of '{1}'.", view.GetType().FullName, typeof(View).FullName));
+                        }
 
-                    view.ViewModel = x.ViewModel;
-
-                    this.Content = castView;
-                })};
+                        view.ViewModel = x.ViewModel;
+                        this.Content = castView;
+                    })};
             });
         }
     }
