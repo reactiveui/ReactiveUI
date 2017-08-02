fbd477644a05e33037e03901aaad8abf078a5c35
Geoffrey Huntley <ghuntley@ghuntley.com>
2017-08-02 18:51:08 +1000

    file -> new project baseline

---
+++ samples/xamarin-forms/App.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace App.Droid
+{
+    [Activity(Label = "App.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
---
+++ samples/xamarin-forms/CommonAssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyProduct("ReactiveTwitter")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyCopyright("Copyright (c) Geoffrey Huntley")]
