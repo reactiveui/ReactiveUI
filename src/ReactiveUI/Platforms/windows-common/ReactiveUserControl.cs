f3104f2c63bc273bbd5c1ee59205fd36d605b4bf
Geoffrey Huntley <ghuntley@ghuntley.com>
2017-08-03 18:35:49 -0700

    housekeeping: added .net foundation license headers (#1408)

--- src/ReactiveUI/Platforms/windows-common/ReactiveUserControl.cs
+++ src/ReactiveUI/Platforms/windows-common/ReactiveUserControl.cs
@@ -1,4 +1,8 @@
-﻿namespace ReactiveUI
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+namespace ReactiveUI
 {
 #if NETFX_CORE
     using Windows.UI.Xaml;
