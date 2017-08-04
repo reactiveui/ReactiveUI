2e4f2a2d0f4c2cd35830485ff52f54caa65f6572
Olly Levett <oliver.levett@softwire.com>
2017-08-04 13:08:07 +0100

    chore: split platform registrations into platform specific files (#1355)
    Fixes #1337, also fixes #1338 and closes #1339

--- src/ReactiveUI/Platforms/netstandard1.1/PlatformRegistrations.cs
+++ src/ReactiveUI/Platforms/netstandard1.1/PlatformRegistrations.cs
@@ -1,12 +1,8 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReactiveUI
 {
