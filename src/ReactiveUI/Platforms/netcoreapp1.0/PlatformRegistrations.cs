c1285aa5848cd0bea6ffcef98ec1b878ff185d24
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-08-26 08:56:52 -0500

    feature: add netcoreapp build (#1418)

--- src/ReactiveUI/Platforms/netcoreapp1.0/PlatformRegistrations.cs
+++ src/ReactiveUI/Platforms/netcoreapp1.0/PlatformRegistrations.cs
@@ -1,6 +1,6 @@
-﻿// Licensed to the .NET Foundation under one or more agreements. 
-// The .NET Foundation licenses this file to you under the MS-PL license. 
-// See the LICENSE file in the project root for more information. 
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
 
 using System;
 using System.Reactive.Concurrency;
@@ -11,7 +11,6 @@ namespace ReactiveUI
     {
         public void Register(Action<Func<object>, Type> registerFunction)
         {
-            registerFunction(() => new ComponentModelTypeConverter(), typeof(IBindingTypeConverter));
             RxApp.TaskpoolScheduler = TaskPoolScheduler.Default;
             RxApp.MainThreadScheduler = DefaultScheduler.Instance;
         }
