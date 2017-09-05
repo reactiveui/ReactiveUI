ea9a632daa1fe42383d9d986dd7177e6d08257a8
Geoffrey Huntley <ghuntley@ghuntley.com>
2017-09-06 03:04:30 +1000

    docs: add cinephile sample application (#1459)
    promote cinephile application into the main repository. Right now the application uses Xamarin.Forms but over time we will remove all Xamarin.Forms references and instead build native user interfaces for android, ios, and uwp

---
+++ samples/android-ios-uwp/Cinephile.Core/Rest/Dtos/Movies/GenreDto.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+namespace Cinephile.Core.Rest.Dtos.Movies
+{
+    public class GenreDto
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get; set;
+        }
+
+        public GenreDto()
+        {
+        }
+    }
+}
---
+++ samples/android-ios-uwp/Cinephile.Core/Rest/IApiService.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+namespace Cinephile.Core.Rest
+{
+    public interface IApiService
+    {
+        IRestApiClient Speculative { get; }
+        IRestApiClient UserInitiated { get; }
+        IRestApiClient Background { get; }
+    }
+}
