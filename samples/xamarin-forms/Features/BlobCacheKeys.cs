4335cf2fe2bce01388de1903812476a107ae206f
Geoffrey Huntley <ghuntley@ghuntley.com>
2017-09-06 00:45:03 +1000

    documentation: added baseline for xamarin.forms sample application (#1457)

---
+++ samples/xamarin-forms/Features/BlobCacheKeys.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public static class BlobCacheKeys
+    {
+        public static string GetKeyForSearch(string query) => string.Format("searchQuery-{0}", query);
+    }
+}
---
+++ samples/xamarin-forms/Tests/Services/State/Builders/StateServiceBuilder.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using Akavache;
+using Services.Connected.State;
+using NSubstitute;
+
+namespace UnitTests.Services.State.Builders
+{
+
+    internal sealed class StateServiceBuilder : IBuilder
+    {
+        private IBlobCache _blobCache;
+
+        public StateServiceBuilder()
+        {
+            _blobCache = Substitute.For<IBlobCache>();
+        }
+
+        public StateServiceBuilder WithBlobCache(IBlobCache blobCache) =>
+            this.With(ref _blobCache, blobCache);
+
+        public StateService Build() =>
+            new StateService(_blobCache);
+
+        public static implicit operator StateService(StateServiceBuilder builder) => builder.Build();
+    }
+}
