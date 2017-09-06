09a36d2069cdb26fd5b0c7833edec2aa541065d1
David Nelson <eatdrinksleepcode@gmail.com>
2017-09-06 09:34:52 -0500

    housekeeping: remove build.sh (#1470)
    Currently, the only end-to-end way for all platforms to build ReactiveUI is on
    Windows via build.cmd.

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mono tools/nuget/nuget.exe update -self
-mono tools/nuget/nuget.exe install xunit.runner.console -OutputDirectory tools -ExcludeVersion
-mono tools/nuget/nuget.exe install Cake -OutputDirectory tools -ExcludeVersion
-
-mono tools/Cake/Cake.exe build.cake
