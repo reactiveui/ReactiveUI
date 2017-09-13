a1f6899c1bae90431ea4aaa23e9f3d27de607119
Olly Levett <oliver.levett@softwire.com>
2017-09-13 16:06:33 +0100

    housekeeping: specify cake version in bootstrap script

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo off
 tools\nuget\nuget.exe update -self
 tools\nuget\nuget.exe install xunit.runner.console -OutputDirectory tools -ExcludeVersion
-tools\nuget\nuget.exe install Cake -OutputDirectory tools -ExcludeVersion
+tools\nuget\nuget.exe install Cake -OutputDirectory tools -Version 0.21.1
 
 tools\Cake\Cake.exe build.cake --target=%1
 
