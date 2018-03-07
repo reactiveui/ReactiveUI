32e5af339e07296c7e9cfd3d8c4a7a96ca120769
Geoffrey Huntley <ghuntley@ghuntley.com>
2018-03-07 15:25:12 -0800

    make it releasable

--- src/EventBuilder/CommandLineOptions.cs
+++ src/EventBuilder/CommandLineOptions.cs
@@ -1,4 +1,4 @@
-g// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
@@ -49,4 +49,4 @@ namespace EventBuilder
                 current => HelpText.DefaultParsingErrorsHandler(this, current));
         }
     }
-g}
+}
