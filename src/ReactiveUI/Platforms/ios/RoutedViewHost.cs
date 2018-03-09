63abdca9a188404382b181d59ab5ddb01b7716df
Olly Levett <oliver.levett@softwire.com>
2018-03-09 08:00:45 +0000

    refactor: remove unused UIKIT ifdefs (#1555)

--- src/ReactiveUI/Platforms/ios/RoutedViewHost.cs
+++ src/ReactiveUI/Platforms/ios/RoutedViewHost.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
@@ -6,14 +6,9 @@ using System;
 using System.Reactive.Linq;
 using System.Collections.Specialized;
 using System.Reactive.Disposables;
-
-#if UIKIT
 using UIKit;
 using NSView = UIKit.UIView;
 using NSViewController = UIKit.UIViewController;
-#else
-using AppKit;
-#endif
 
 namespace ReactiveUI
 {
