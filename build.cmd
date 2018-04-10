b53ac27ec02eb5d42eeefd44a2b91b115f188e65
Geoffrey Huntley <ghuntley@ghuntley.com>
2018-04-10 21:45:30 +1000

    more verbose, use 0.22.1

--- build.cmd
+++ build.cmd
@@ -1,8 +1,8 @@
 @echo off
 tools\nuget\nuget.exe update -self
 tools\nuget\nuget.exe install xunit.runner.console -OutputDirectory tools -Version 2.2.0 -ExcludeVersion
-tools\nuget\nuget.exe install Cake -OutputDirectory tools -ExcludeVersion -Version 0.21.1
+tools\nuget\nuget.exe install Cake -OutputDirectory tools -ExcludeVersion -Version 0.22.1 
 
-tools\Cake\Cake.exe build.cake --target=%1
+tools\Cake\Cake.exe build.cake --target=%1 --verbosity=diagnostic
 
 exit /b %errorlevel%
