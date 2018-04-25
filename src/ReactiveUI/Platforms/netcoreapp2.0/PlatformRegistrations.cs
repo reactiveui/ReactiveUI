283f749f784d62614bf0d810f755f918b7882391
Olly Levett <oliver.levett@softwire.com>
2018-04-25 19:49:20 +0100

    Revert "drop netcoreapp and use netstandard instead"
    This reverts commit 1ba77ccfdc7c373b00134b6141d0f0825bfece9f.

--- src/ReactiveUI/Platforms/netcoreapp2.0/PlatformRegistrations.cs
+++ src/ReactiveUI/Platforms/netcoreapp2.0/PlatformRegistrations.cs
@@ -1,8 +1,9 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MS-PL license.
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Reactive.Concurrency;
 
 namespace ReactiveUI
 {
@@ -10,7 +11,8 @@ namespace ReactiveUI
     {
         public void Register(Action<Func<object>, Type> registerFunction)
         {
-            throw new Exception("You are referencing the Portable version of ReactiveUI in an App. Reference the platform-specific version.");
+            RxApp.TaskpoolScheduler = TaskPoolScheduler.Default;
+            RxApp.MainThreadScheduler = DefaultScheduler.Instance;
         }
     }
 }
