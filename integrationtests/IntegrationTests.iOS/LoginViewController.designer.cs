3694bce991d3624f4b4a89cd83de363f5e59a696
Stefan Moonen <stefan.moonen@freshheads.com>
2018-04-30 18:46:30 +0200

    Implemented basic LoginViewController

---
+++ integrationtests/IntegrationTests.iOS/LoginViewController.designer.cs
@@ -0,0 +1,50 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace IntegrationTests.iOS
+{
+	[Register ("LoginViewController")]
+	partial class LoginViewController
+	{
+		[Outlet]
+		UIKit.UIButton CancelButton { get; set; }
+
+		[Outlet]
+		UIKit.UIButton LoginButton { get; set; }
+
+		[Outlet]
+		UIKit.UITextField PasswordField { get; set; }
+
+		[Outlet]
+		UIKit.UITextField UsernameField { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (UsernameField != null) {
+				UsernameField.Dispose ();
+				UsernameField = null;
+			}
+
+			if (PasswordField != null) {
+				PasswordField.Dispose ();
+				PasswordField = null;
+			}
+
+			if (LoginButton != null) {
+				LoginButton.Dispose ();
+				LoginButton = null;
+			}
+
+			if (CancelButton != null) {
+				CancelButton.Dispose ();
+				CancelButton = null;
+			}
+		}
+	}
+}
