66c207e0e76ff01e0d16e7f3df96f6fb303f9466
Geoffrey Huntley <ghuntley@ghuntley.com>
2018-05-12 04:14:53 +1000

    Migrate to VSTS (#1613)

--- build.cmd
+++ build.cmd
@@ -1,7 +1,6 @@
 @echo off
-tools\nuget\nuget.exe update -self
 tools\nuget\nuget.exe install Cake -OutputDirectory tools -ExcludeVersion -Version 0.22.1 
 
-tools\Cake\Cake.exe build.cake --target=%1 --verbosity=diagnostic
+tools\Cake\Cake.exe build.cake --target=%1 
 
 exit /b %errorlevel%
