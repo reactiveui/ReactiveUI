f81c9b0a405332d307fb9ab553d11f410a903470
Olly Levett <oliver.levett@softwire.com>
2018-07-12 19:23:21 +0100

    fix: wait for scheduler guards against exception in early initialisation (#1312)

---
+++ src/ReactiveUI.Tests/WaitForDispatcherSchedulerTests.cs
@@ -0,0 +1,89 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using Xunit;
+
+namespace ReactiveUI.Tests
+{
+    public class WaitForDispatcherSchedulerTests
+    {
+        [Fact]
+        public void CallSchedulerFactoryOnCreation()
+        {
+            var schedulerFactoryCalls = 0;
+            var schedulerFactory = new Func<IScheduler>(() => { schedulerFactoryCalls++; return null; });
+
+            var sut = new WaitForDispatcherScheduler(schedulerFactory);
+
+            Assert.Equal(1, schedulerFactoryCalls);
+        }
+
+        [Fact]
+        public void FactoryThrowsException_ReCallsOnSchedule()
+        {
+            var schedulerFactoryCalls = 0;
+            var schedulerFactory = new Func<IScheduler>(() => {
+                schedulerFactoryCalls++;
+                throw new InvalidOperationException();
+            });
+
+            var sut = new WaitForDispatcherScheduler(schedulerFactory);
+            sut.Schedule(() => { });
+
+            Assert.Equal(2, schedulerFactoryCalls);
+        }
+
+        [Fact]
+        public void SuccessfulFactory_UsesCachedScheduler()
+        {
+            var schedulerFactoryCalls = 0;
+            var schedulerFactory = new Func<IScheduler>(() => {
+                schedulerFactoryCalls++;
+                return CurrentThreadScheduler.Instance;
+            });
+
+            var sut = new WaitForDispatcherScheduler(schedulerFactory);
+            sut.Schedule(() => { });
+
+            Assert.Equal(1, schedulerFactoryCalls);
+        }
+
+        [Fact]
+        public void FactoryThrowsInvalidOperationException_FallsBackToCurrentThread()
+        {
+            IScheduler schedulerExecutedOn = null;
+            var schedulerFactory = new Func<IScheduler>(() => {
+                throw new InvalidOperationException();
+            });
+
+            var sut = new WaitForDispatcherScheduler(schedulerFactory);
+            sut.Schedule<object>(null, (scheduler, state) => {
+                schedulerExecutedOn = scheduler;
+                return Disposable.Empty;
+            });
+
+            Assert.Equal(CurrentThreadScheduler.Instance, schedulerExecutedOn);
+        }
+
+        [Fact]
+        public void FactoryThrowsArgumentNullException_FallsBackToCurrentThread()
+        {
+            IScheduler schedulerExecutedOn = null;
+            var schedulerFactory = new Func<IScheduler>(() => {
+                throw new ArgumentNullException();
+            });
+
+            var sut = new WaitForDispatcherScheduler(schedulerFactory);
+            sut.Schedule<object>(null, (scheduler, state) => {
+                schedulerExecutedOn = scheduler;
+                return Disposable.Empty;
+            });
+
+            Assert.Equal(CurrentThreadScheduler.Instance, schedulerExecutedOn);
+        }
+    }
+}
