e05371e8d209ed6ad40d78036d12af0ff03bc969
Sebastian <7798041+Asesjix@users.noreply.github.com>
2018-08-09 18:19:01 +0200

    feature: Bring winforms into parity with the WPF project.
    - Change TargetFrameworks net461 > TargetFramework net461 in csproj
    - Create ReactiveUserControl (used wpf as reference)
    - ViewModelViewHost now set the viewmodel before the view is set to content (like wpf)

---
+++ src/ReactiveUI.Winforms/ReactiveUserControl.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace ReactiveUI.Winforms
+{
+    public partial class ReactiveUserControl<TViewModel> : UserControl, IViewFor<TViewModel>
+        where TViewModel : class
+    {
+        public ReactiveUserControl()
+        {
+            InitializeComponent();
+        }
+
+        [Category("ReactiveUI")]
+        [Description("The ViewModel.")]
+        [Bindable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public TViewModel ViewModel { get; set; }
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (TViewModel)value;
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
