6d61bba3c9c8278e6c7a06efbf12cbebb07203aa
Rodney Littles II <rodney.littles@gmail.com>
2018-08-24 14:19:29 -0500

    Added Benchmark Harness (#1729)
    - Added NavigationStack Benchmarks
    - Added AutoPersist Benchmarks
    - Added ReactiveList Operations Benchmarks
    - Added RoutableViewModelMixin Benchmarks

---
+++ benchmarks/ReactiveUI.Benchmarks/AutoPersistBenchmark.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Exporters;
+using BenchmarkDotNet.Attributes.Jobs;
+
+namespace ReactiveUI.Benchmarks
+{
+    [CoreJob]
+    [MarkdownExporterAttribute.GitHub]
+    public class AutoPersistBenchmark
+    {
+        private ReactiveList<string> _collection;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _collection = new ReactiveList<string>(new[]
+            {
+                "ReactiveUI",
+                "ReactiveUI.XamForms",
+                "ReactiveUI.WPF",
+                "ReactiveUI.Events"
+            });
+        }
+
+        [Benchmark]
+        public void AutoPersistCollection() => _collection.AutoPersist(x => Observable.Return(Unit.Default), TimeSpan.FromMilliseconds(200));
+    }
+}
