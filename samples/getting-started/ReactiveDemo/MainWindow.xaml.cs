a1dc66314e1ddd2799106d47ec01924937af856e
Artjom Graf <prizrak.nra@gmail.com>
2018-09-02 13:11:41 +0300

    feature: Add app for the getting started guide

---
+++ samples/getting-started/ReactiveDemo/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Reactive.Disposables;
+using System.Windows;
+using ReactiveUI;
+
+namespace ReactiveDemo
+{
+    public partial class MainWindow : IViewFor<AppViewModel>
+    {
+        // Using a DependencyProperty as the backing store for ViewModel.  
+        // This enables animation, styling, binding, etc.
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel",
+                typeof(AppViewModel), typeof(MainWindow),
+                new PropertyMetadata(null));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new AppViewModel();
+
+            // We create our bindings here. These are the code behind bindings which allow 
+            // type safety. The bindings will only become active when the Window is being shown.
+            // We register our subscription in our disposableRegistration, this will cause 
+            // the binding subscription to become inactive when the Window is closed.
+            // The disposableRegistration is a CompositeDisposable which is a container of 
+            // other Disposables. We use the DisposeWith() extension method which simply adds 
+            // the subscription disposable to the CompositeDisposable.
+            this.WhenActivated(disposableRegistration =>
+            {
+                // Notice we don't have to provide a converter, on WPF a global converter is
+                // registered which knows how to convert a boolean into visibility.
+                this.OneWayBind(ViewModel,
+                    viewModel => viewModel.IsAvailable,
+                    view => view.searchResultsListBox.Visibility)
+                    .DisposeWith(disposableRegistration);
+
+                this.OneWayBind(ViewModel,
+                    viewModel => viewModel.SearchResults,
+                    view => view.searchResultsListBox.ItemsSource)
+                    .DisposeWith(disposableRegistration);
+
+                this.Bind(ViewModel,
+                    viewModel => viewModel.SearchTerm,
+                    view => view.searchTextBox.Text)
+                    .DisposeWith(disposableRegistration);
+            });
+        }
+
+        // Our main view model instance.
+        public AppViewModel ViewModel
+        {
+            get => (AppViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        // This is required by the interface IViewFor, you always just set it to use the 
+        // main ViewModel property. Note on XAML based platforms we have a control called
+        // ReactiveUserControl that abstracts this.
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (AppViewModel)value;
+        }
+    }
+}
---
+++ samples/getting-started/ReactiveDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("ReactiveDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReactiveDemo")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
