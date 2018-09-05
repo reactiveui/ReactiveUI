a3a62cba71e999b8f622c36365837892c95930df
Colt <cab452005@gmail.com>
2018-09-05 09:39:19 +0900

    feature: Add a MasterDetailPage sample for Xamarin (#1741)
    * Move Cinephile project to subfolder
    
    * Add MasterDetailPage sample
    
    * Delete unused file and commented out code

---
+++ samples/xamarin-forms/MasterDetail/MasterDetail/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using ReactiveUI;
+using ReactiveUI.XamForms;
+using Xamarin.Forms;
+
+namespace MasterDetail
+{
+    public partial class MainPage : ReactiveMasterDetailPage<MainViewModel>
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ViewModel = new MainViewModel();
+            Detail = new NavigationPage(new MyDetailPage(ViewModel.Detail));
+
+            this.WhenActivated(
+                disposables =>
+                {
+                    this
+                        .OneWayBind(ViewModel, vm => vm.MyList, v => v.MyListView.ItemsSource)
+                        .DisposeWith(disposables);
+                    this
+                        .Bind(ViewModel, vm => vm.Selected, v => v.MyListView.SelectedItem)
+                        .DisposeWith(disposables);
+                    this
+                        .WhenAnyValue(x => x.ViewModel.Selected)
+                        .Where(x => x != null)
+                        .Subscribe(
+                            model =>
+                            {
+                                MyListView.SelectedItem = null;
+                                IsPresented = false;
+                            })
+                        .DisposeWith(disposables);
+                });
+        }
+    }
+}
