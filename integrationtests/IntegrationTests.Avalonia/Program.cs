e32a470e1184661de8a3e04bf636e0ef7a277a5b
Artjom Graf <prizrak.nra@gmail.com>
2018-10-06 23:02:08 +0300

    Add IntegrationTests.Avalonia project

---
+++ integrationtests/IntegrationTests.Avalonia/Program.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Logging.Serilog;
+using ReactiveUI;
+using ReactiveUI.Avalonia;
+
+namespace IntegrationTests.Avalonia
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>();
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug();
+    }
+}
