329ba71a55b7cb2477cc21529f2687335d716148
Rodney Littles II <rodney.littles@gmail.com>
2018-10-26 01:17:56 -0500

    housekeeping: Fixed analyzers breaking other solutions (#1823)
    - Benchmarks
    - Integration Tests

--- integrationtests/IntegrationTests.Shared/LoginViewModel.cs
+++ integrationtests/IntegrationTests.Shared/LoginViewModel.cs
@@ -1,20 +1,28 @@
-﻿using Genesis.Ensure;
-using ReactiveUI;
-using System;
+﻿using System;
 using System.Reactive;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Genesis.Ensure;
+using ReactiveUI;
 
 namespace IntegrationTests.Shared
 {
+    /// <summary>
+    /// View model for login functionality.
+    /// </summary>
+    /// <seealso cref="ReactiveUI.ReactiveObject" />
     public class LoginViewModel : ReactiveObject
     {
         private string _userName;
         private string _password;
         private IScheduler _mainScheduler;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoginViewModel"/> class.
+        /// </summary>
+        /// <param name="mainScheduler">The main scheduler.</param>
         public LoginViewModel(IScheduler mainScheduler)
         {
             Ensure.ArgumentNotNull(mainScheduler, nameof(mainScheduler));
@@ -25,8 +33,7 @@ namespace IntegrationTests.Shared
                 .WhenAnyValue(
                     vm => vm.UserName,
                     vm => vm.Password,
-                    (user, password) => !string.IsNullOrWhiteSpace(user) && !string.IsNullOrWhiteSpace(password)
-                );
+                    (user, password) => !string.IsNullOrWhiteSpace(user) && !string.IsNullOrWhiteSpace(password));
 
             Login = ReactiveCommand.CreateFromObservable(
                 () =>
@@ -38,16 +45,28 @@ namespace IntegrationTests.Shared
             Cancel = ReactiveCommand.Create(() => { }, Login.IsExecuting, _mainScheduler);
         }
 
+        /// <summary>
+        /// Gets the login command.
+        /// </summary>
         public ReactiveCommand<Unit, bool?> Login { get; }
 
+        /// <summary>
+        /// Gets the cancel command.
+        /// </summary>
         public ReactiveCommand<Unit, Unit> Cancel { get; }
 
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
         public string UserName
         {
             get => _userName;
             set => this.RaiseAndSetIfChanged(ref _userName, value);
         }
 
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
         public string Password
         {
             get => _password;
