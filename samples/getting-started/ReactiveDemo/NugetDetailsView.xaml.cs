a8393bdb4b5528b8cff86030cb8f307a2d26eae7
Daniel Barral <daniel.barral@ingeteam.com>
2018-11-12 17:43:17 +0100

    ReactiveDemo hack around Visual Studio xaml designer issue with inherits from generic classes

--- samples/getting-started/ReactiveDemo/NugetDetailsView.xaml.cs
+++ samples/getting-started/ReactiveDemo/NugetDetailsView.xaml.cs
@@ -4,11 +4,14 @@ using System.Windows.Media.Imaging;
 
 namespace ReactiveDemo
 {
-    // The class derives off ReactiveUserControl which contains the ViewModel property.
+    /// Nongeneric class that allows us to edit NugetDetailsView in Design view
+    public class NugetDetailsViewBase : ReactiveUserControl<NugetDetailsViewModel> { }
+
+    // Second level derived class off ReactiveUserControl which contains the ViewModel property.
     // In our MainWindow when we register the ListBox with the collection of 
     // NugetDetailsViewModels if no ItemTemplate has been declared it will search for 
     // a class derived off IViewFor<NugetDetailsViewModel> and show that for the item.
-    public partial class NugetDetailsView : ReactiveUserControl<NugetDetailsViewModel>
+    public partial class NugetDetailsView : NugetDetailsViewBase
     {
         public NugetDetailsView()
         {
