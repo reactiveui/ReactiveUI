f3f371b3d742faf58dc5e01c4c6490ed13a569c9
Daniel Barral <Nilox@users.noreply.github.com>
2018-11-14 08:37:04 +0100

    Housekeeping: ReactiveDemo hack around VS xaml designer issue with inherits from generic classes (#1837)
    * ReactiveDemo hack around Visual Studio xaml designer issue with inherits from generic classes

--- samples/getting-started/ReactiveDemo/NugetDetailsView.xaml.cs
+++ samples/getting-started/ReactiveDemo/NugetDetailsView.xaml.cs
@@ -4,7 +4,7 @@ using System.Windows.Media.Imaging;
 
 namespace ReactiveDemo
 {
-    // The class derives off ReactiveUserControl which contains the ViewModel property.
+    // Second level derived class off ReactiveUserControl which contains the ViewModel property.
     // In our MainWindow when we register the ListBox with the collection of 
     // NugetDetailsViewModels if no ItemTemplate has been declared it will search for 
     // a class derived off IViewFor<NugetDetailsViewModel> and show that for the item.
