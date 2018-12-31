34a2c94f14d23ba7135e1fb53951d957fdd1f3af
Glenn Watson <glenn@glennwatson.net>
2018-12-31 15:13:24 -0800

    housekeeping: separate out tests into new azure pipeline (#1879)
    * housekeeping: Add additional information for coveralls upload
    
    * housekeeping: separate out tests into new azure pipeline

--- build.cmd
+++ build.cmd
@@ -1,6 +1,2 @@
 @echo off
-tools\nuget\nuget.exe install Cake -OutputDirectory tools -ExcludeVersion -Version 0.31.0
-
-tools\Cake\Cake.exe build.cake --target=%1 
-
-exit /b %errorlevel%
+powershell -ExecutionPolicy Unrestricted ./build.ps1 %CAKE_ARGS% %*
