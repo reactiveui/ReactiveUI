7e5a41bbe13435f2d4e76a07f30c4799dc6d82c1
Glenn Watson <glenn@glennwatson.net>
2019-01-02 21:59:32 -0800

    housekeeping: change where the signing happens based on recent changes

--- SignPackages.ps1
+++ SignPackages.ps1
@@ -11,7 +11,7 @@ dotnet tool install --tool-path . SignClient
 # Setup Variables we need to pass into the sign client tool
 $appSettings = "$currentDirectory\SignPackages.json"
 
-$nupgks = gci $Env:ArtifactDirectory\*.nupkg | Select -ExpandProperty FullName
+$nupgks = gci $Env:ArtifactDirectory\packages\*.nupkg | Select -ExpandProperty FullName
 
 foreach ($nupkg in $nupgks){
     Write-Host "Submitting $nupkg for signing"
