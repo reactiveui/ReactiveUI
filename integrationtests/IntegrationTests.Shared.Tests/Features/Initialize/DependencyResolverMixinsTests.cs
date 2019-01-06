884742fa98514ef0c8e76de36a07f14d61a76dfe
cabauman <cab452005@hotmail.com>
2019-01-06 18:39:17 +0900

    Fix failing DependencyResolver test

--- integrationtests/IntegrationTests.Shared.Tests/Features/Initialize/DependencyResolverMixinsTests.cs
+++ integrationtests/IntegrationTests.Shared.Tests/Features/Initialize/DependencyResolverMixinsTests.cs
@@ -20,7 +20,7 @@ namespace IntegrationTests.Shared.Tests.Features.Initialize
         {
             Dictionary<Type, List<Type>> serviceTypeToImplementationTypes = new Dictionary<Type, List<Type>>();
 
-            Registrations registrations = new Registrations();
+            var registrations = new Registrations();
             registrations.Register((factory, serviceType) =>
             {
                 if (serviceTypeToImplementationTypes.TryGetValue(serviceType, out List<Type> implementationTypes) == false)
@@ -32,33 +32,17 @@ namespace IntegrationTests.Shared.Tests.Features.Initialize
                 implementationTypes.Add(factory().GetType());
             });
 
-            Type platformRegistrationsType = Type.GetType("ReactiveUI.Wpf.Registrations, ReactiveUI.Wpf");
-            if (platformRegistrationsType == null)
+            var platformRegistrations = new PlatformRegistrations();
+            platformRegistrations.Register((factory, serviceType) =>
             {
-                platformRegistrationsType = Type.GetType("ReactiveUI.XamForms.Registrations, ReactiveUI.XamForms");
-            }
-
-            if (platformRegistrationsType == null)
-            {
-                platformRegistrationsType = Type.GetType("ReactiveUI.Winforms.Registrations, ReactiveUI.Winforms");
-            }
-
-            if (platformRegistrationsType != null)
-            {
-                var platformRegistrations = Activator.CreateInstance(platformRegistrationsType);
-                System.Reflection.MethodInfo register = platformRegistrationsType.GetMethod("Register");
-                Action<Func<object>, Type> registerParameter = new Action<Func<object>, Type>((factory, serviceType) =>
+                if (serviceTypeToImplementationTypes.TryGetValue(serviceType, out List<Type> implementationTypes) == false)
                 {
-                    if (serviceTypeToImplementationTypes.TryGetValue(serviceType, out List<Type> implementationTypes) == false)
-                    {
-                        implementationTypes = new List<Type>();
-                        serviceTypeToImplementationTypes.Add(serviceType, implementationTypes);
-                    }
+                    implementationTypes = new List<Type>();
+                    serviceTypeToImplementationTypes.Add(serviceType, implementationTypes);
+                }
 
-                    implementationTypes.Add(factory().GetType());
-                });
-                register.Invoke(platformRegistrations, new object[] { registerParameter });
-            }
+                implementationTypes.Add(factory().GetType());
+            });
 
             return serviceTypeToImplementationTypes;
         }
@@ -69,9 +53,13 @@ namespace IntegrationTests.Shared.Tests.Features.Initialize
         [Fact]
         public void Registrations_Should_Resolve_Correctly()
         {
+            var resolver = new ModernDependencyResolver();
+            resolver.InitializeSplat();
+            resolver.InitializeReactiveUI();
+
             foreach (KeyValuePair<Type, List<Type>> shouldRegistered in GetServicesThatShouldRegistered())
             {
-                IEnumerable<object> resolvedServices = Locator.Current.GetServices(shouldRegistered.Key);
+                IEnumerable<object> resolvedServices = resolver.GetServices(shouldRegistered.Key);
                 Assert.Equal(shouldRegistered.Value.Count, resolvedServices.Count());
                 foreach (Type implementationType in shouldRegistered.Value)
                 {
