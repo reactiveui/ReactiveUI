7e5fdce886f1e319b9802e1235a91951c570449e
Glenn Watson <glenn@glennwatson.net>
2019-01-12 15:42:59 -0800

    housekeeping: Update headers and stricter analyzers

--- src/EventBuilder/Platforms/XamForms.cs
+++ src/EventBuilder/Platforms/XamForms.cs
@@ -1,16 +1,14 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
+// See the LICENSE file in the project root for full license information.
 
-using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using EventBuilder.NuGet;
-using NuGet;
 using NuGet.Packaging.Core;
 using NuGet.Versioning;
-using Polly;
 using Serilog;
 
 namespace EventBuilder.Platforms
