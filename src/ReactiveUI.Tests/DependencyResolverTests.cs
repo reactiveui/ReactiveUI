be74c1d479a554c2916badc1e555fe02bf595b36
Colt <cab452005@gmail.com>
2019-01-13 15:32:57 +0900

    Housekeeping: add resolver tests (#1901)
    * Fix: WrappingFullLogger constructor update
    
    * Add dependency resolver test

--- src/ReactiveUI.Tests/DependencyResolverTests.cs
+++ src/ReactiveUI.Tests/DependencyResolverTests.cs
@@ -4,7 +4,8 @@
 // See the LICENSE file in the project root for full license information.
 
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 using ReactiveUI;
 using ReactiveUI.Tests;
 using Splat;
@@ -185,9 +186,87 @@ namespace ReactiveUI.Tests
             }
         }
 
+        [Fact]
+        public void AllDefaultServicesShouldBeRegistered()
+        {
+            using (_resolver.WithResolver())
+            {
+                foreach (var shouldRegistered in GetServicesThatShouldBeRegistered())
+                {
+                    IEnumerable<object> resolvedServices = _resolver.GetServices(shouldRegistered.Key);
+                    Assert.Equal(shouldRegistered.Value.Count, resolvedServices.Count());
+                    foreach (Type implementationType in shouldRegistered.Value)
+                    {
+                        var isRegistered = resolvedServices.Any(rs => rs.GetType() == implementationType);
+                        Assert.Equal(true, isRegistered);
+                    }
+                }
+            }
+        }
+
         public void Dispose()
         {
             _resolver?.Dispose();
+        }
+
+        private static Dictionary<Type, List<Type>> GetServicesThatShouldBeRegistered()
+        {
+            Dictionary<Type, List<Type>> serviceTypeToImplementationTypes = new Dictionary<Type, List<Type>>();
+
+            new Registrations().Register((factory, serviceType) =>
+            {
+                if (serviceTypeToImplementationTypes.TryGetValue(serviceType, out List<Type> implementationTypes) == false)
+                {
+                    implementationTypes = new List<Type>();
+                    serviceTypeToImplementationTypes.Add(serviceType, implementationTypes);
+                }
+
+                implementationTypes.Add(factory().GetType());
+            });
+
+            new PlatformRegistrations().Register((factory, serviceType) =>
+            {
+                if (serviceTypeToImplementationTypes.TryGetValue(serviceType, out List<Type> implementationTypes) == false)
+                {
+                    implementationTypes = new List<Type>();
+                    serviceTypeToImplementationTypes.Add(serviceType, implementationTypes);
+                }
+
+                implementationTypes.Add(factory().GetType());
+            });
+
+            var typeNames = new[]
+            {
+                "ReactiveUI.XamForms.Registrations, ReactiveUI.XamForms",
+                "ReactiveUI.Winforms.Registrations, ReactiveUI.Winforms",
+                "ReactiveUI.Wpf.Registrations, ReactiveUI.Wpf"
+            };
+
+            typeNames.ForEach(typeName => GetRegistrationsForPlatform(typeName, serviceTypeToImplementationTypes));
+
+            return serviceTypeToImplementationTypes;
+        }
+
+        private static void GetRegistrationsForPlatform(string typeName, Dictionary<Type, List<Type>> serviceTypeToImplementationTypes)
+        {
+            Type platformRegistrationsType = Type.GetType(typeName);
+            if (platformRegistrationsType != null)
+            {
+                var platformRegistrations = Activator.CreateInstance(platformRegistrationsType);
+                System.Reflection.MethodInfo register = platformRegistrationsType.GetMethod("Register");
+                var registerParameter = new Action<Func<object>, Type>((factory, serviceType) =>
+                {
+                    if (serviceTypeToImplementationTypes.TryGetValue(serviceType, out List<Type> implementationTypes) == false)
+                    {
+                        implementationTypes = new List<Type>();
+                        serviceTypeToImplementationTypes.Add(serviceType, implementationTypes);
+                    }
+
+                    implementationTypes.Add(factory().GetType());
+                });
+
+                register.Invoke(platformRegistrations, new object[] { registerParameter });
+            }
         }
     }
 }
