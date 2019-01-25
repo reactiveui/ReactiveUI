5abf0a0faf4eee5691bc2f78d3c78f082b649448
Glenn Watson <glenn@glennwatson.net>
2019-01-24 21:07:57 -0800

    fix: Fix UWP activating too soon

--- src/ReactiveUI/Platforms/uap10.0.16299/ActivationForViewFetcher.cs
+++ src/ReactiveUI/Platforms/uap10.0.16299/ActivationForViewFetcher.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
@@ -9,9 +9,7 @@ using System.Reactive.Linq;
 using System.Reflection;
 using System.Windows;
 
-#if NETFX_CORE
 using Windows.UI.Xaml;
-#endif
 
 namespace ReactiveUI
 {
@@ -37,33 +35,19 @@ namespace ReactiveUI
             {
                 return Observable<bool>.Empty;
             }
-#if WINDOWS_UWP
+
             var viewLoaded = WindowsObservable.FromEventPattern<FrameworkElement, object>(
                 x => fe.Loading += x,
-                x => fe.Loading -= x)
-                .Select(_ => true);
-
-            var hitTestVisible = fe.WhenAnyValue(x => x.IsHitTestVisible);
-#else
-            var viewLoaded = Observable.FromEventPattern<RoutedEventHandler, RoutedEventArgs>(
-                x => fe.Loaded += x,
-                x => fe.Loaded -= x)
-                .Select(_ => true);
-
-            var hitTestVisible = Observable.FromEventPattern<DependencyPropertyChangedEventHandler, DependencyPropertyChangedEventArgs>(
-                x => fe.IsHitTestVisibleChanged += x,
-                x => fe.IsHitTestVisibleChanged -= x)
-                .Select(x => (bool)x.EventArgs.NewValue);
-#endif
+                x => fe.Loading -= x).Select(_ => true);
 
             var viewUnloaded = Observable.FromEventPattern<RoutedEventHandler, RoutedEventArgs>(
                 x => fe.Unloaded += x,
-                x => fe.Unloaded -= x)
-                .Select(_ => false);
+                x => fe.Unloaded -= x).Select(_ => false);
 
             return viewLoaded
                 .Merge(viewUnloaded)
-                .Merge(hitTestVisible)
+                .Select(b => b ? fe.WhenAnyValue(x => x.IsHitTestVisible).SkipWhile(x => !x) : Observables.False)
+                .Switch()
                 .DistinctUntilChanged();
         }
     }
