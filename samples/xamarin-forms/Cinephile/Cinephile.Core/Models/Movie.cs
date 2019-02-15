718b9839287fab1d580d248d01505d4fab29b6b5
Giusepe Casagrande <giusepe@gmail.com>
2019-02-15 14:02:43 +0100

    Minor tweaks

--- samples/xamarin-forms/Cinephile/Cinephile.Core/Models/Movie.cs
+++ samples/xamarin-forms/Cinephile/Cinephile.Core/Models/Movie.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 namespace Cinephile.Core.Models
 {
-    public class Movie
+    public class Movie : IEquatable<Movie>
     {
         public string PosterSmall { get; set; }
         public string PosterBig { get; set; }
@@ -16,5 +16,46 @@ namespace Cinephile.Core.Models
         public IList<string> Genres { get; set; }
         public int Id { get; set; }
         public string Title { get; set; }
+
+        public bool Equals(Movie other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+
+            var equals = Id == other.Id
+                && PosterSmall == other.PosterSmall
+                && PosterBig == other.PosterBig
+                && Overview == other.Overview
+                && ReleaseDate == other.ReleaseDate
+                && Title == other.Title;
+
+                return equals;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != GetType())
+                return false;
+
+            return Equals((Movie)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 745094178;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(PosterSmall);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(PosterBig);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Overview);
+            hashCode = hashCode * -1521134295 + ReleaseDate.GetHashCode();
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Title);
+            return hashCode;
+        }
     }
 }
