2b62d1d832572d7e48e1c124bfe1200943fe1d17
Glenn Watson <glenn@glennwatson.net>
2019-02-21 05:38:12 +0000

    feature: Add winforms auto binding to Tables/Flow based controls (#1956)

---
+++ samples/winforms/getting-started/ReactiveDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ReactiveUI;
+using Splat;
+
+namespace ReactiveDemo
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // A helper method that will register all classes that derive off IViewFor 
+            // into our dependency injection container. ReactiveUI uses Splat for it's 
+            // dependency injection by default, but you can override this if you like.
+            Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+    }
+}
