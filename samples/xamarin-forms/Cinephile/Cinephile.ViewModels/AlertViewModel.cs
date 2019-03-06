6cd3c24b74250e94d72ad6634969de5e2241370f
Giusepe Casagrande <giusepe.casagrande@accruent.com>
2019-03-06 15:13:18 +0100

    Lots of small refactorings Added a display alert Imrpoved the view and viewmodel base

---
+++ samples/xamarin-forms/Cinephile/Cinephile.ViewModels/AlertViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Cinephile.ViewModels
+{
+    public class AlertViewModel
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string ButtonText { get; private set; }
+
+        public AlertViewModel(string title, string description, string buttonText)
+        {
+            Title = title;
+            Description = description;
+            ButtonText = buttonText;
+        }
+    }
+}
--- samples/xamarin-forms/Cinephile/Cinephile.ViewModels/ViewModelBase.cs
+++ samples/xamarin-forms/Cinephile/Cinephile.ViewModels/ViewModelBase.cs
@@ -2,37 +2,46 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Reactive.Disposables;
+using System.Reactive;
+using System.Reactive.Concurrency;
 using ReactiveUI;
 using Splat;
 
 namespace Cinephile.ViewModels
 {
-    public class ViewModelBase : ReactiveObject, IRoutableViewModel, ISupportsActivation
+    public abstract class ViewModelBase : ReactiveObject, IRoutableViewModel, ISupportsActivation
     {
         public string UrlPathSegment
         {
             get;
-            protected set;
+            private set;
         }
 
         public IScreen HostScreen
         {
             get;
-            protected set;
-        }
-
-        public ViewModelActivator Activator
-        {
-            get { return viewModelActivator; }
+            private set;
         }
 
         protected readonly ViewModelActivator viewModelActivator = new ViewModelActivator();
+        public ViewModelActivator Activator => viewModelActivator;
 
-        public ViewModelBase(IScreen hostScreen = null)
+        protected readonly Interaction<AlertViewModel, Unit> m_showAlert;
+        public Interaction<AlertViewModel, Unit> ShowAlert => m_showAlert;
+
+        protected readonly IScheduler _mainThreadScheduler;
+        protected readonly IScheduler _taskPoolScheduler;
+
+        protected ViewModelBase(string title, IScheduler mainThreadScheduler = null, IScheduler taskPoolScheduler = null, IScreen hostScreen = null)
         {
+            UrlPathSegment = title;
             HostScreen = hostScreen ?? Locator.Current.GetService<IScreen>();
+
+            // Set the schedulers like this so we can inject the test scheduler later on when doing VM unit tests
+            _mainThreadScheduler = mainThreadScheduler ?? RxApp.MainThreadScheduler;
+            _taskPoolScheduler = taskPoolScheduler ?? RxApp.TaskpoolScheduler;
+
+            m_showAlert = new Interaction<AlertViewModel, Unit>(_mainThreadScheduler);
         }
     }
 }
