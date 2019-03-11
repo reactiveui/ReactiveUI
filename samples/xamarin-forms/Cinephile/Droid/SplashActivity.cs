ff2d7d9f5d1efc17cfe7e41a1a000b792873d16a
Giusepe Casagrande <giusepe.casagrande@accruent.com>
2019-03-11 15:03:55 +0100

    WIP: I hate android... Spalshscreen is weird and I won't fix

---
+++ samples/xamarin-forms/Cinephile/Droid/SplashActivity.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Cinephile.Droid
+{
+    [Activity(
+        Label = "Cinephile",
+        Icon = "@drawable/icon",
+        Theme = "@style/Theme.Splash",
+        NoHistory = true,
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        LaunchMode = LaunchMode.SingleTask)]
+    public class SplashActivity : AppCompatActivity
+    {
+        private static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
