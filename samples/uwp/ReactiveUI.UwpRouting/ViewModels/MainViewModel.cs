84c05e5bb2a14a07ee17b9248b794254633a56f1
Artjom Graf <prizrak.nra@gmail.com>
2019-05-13 14:10:23 +0300

    Add Universal Windows routing example

---
+++ samples/uwp/ReactiveUI.UwpRouting/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Reactive;
+using ReactiveUI.UwpRouting.Views;
+using Splat;
+
+namespace ReactiveUI.UwpRouting.ViewModels
+{
+    public class MainViewModel : ReactiveObject, IScreen
+    {
+        // The Router associated with this Screen.
+        // Required by the IScreen interface.
+        public RoutingState Router { get; } = new RoutingState();
+
+        // The command that navigates a user to first view model.
+        public ReactiveCommand<Unit, IRoutableViewModel> GoNext { get; }
+
+        // The command that navigates a user back.
+        public ReactiveCommand<Unit, Unit> GoBack => Router.NavigateBack;
+
+        public MainViewModel()
+        {
+            // Router uses Splat.Locator to resolve views for
+            // view models, so we need to register our views
+            // using Locator.CurrentMutable.Register* methods.
+            //
+            Locator.CurrentMutable.Register(() => new FirstView(), typeof(IViewFor<FirstViewModel>));
+
+            // Manage the routing state. Use the Router.Navigate.Execute
+            // command to navigate to different view models. 
+            //
+            // Note, that the Navigate.Execute method accepts an instance 
+            // of a view model, this allows you to pass parameters to 
+            // your view models, or to reuse existing view models.
+            //
+            GoNext = ReactiveCommand.CreateFromObservable(
+                () => Router.Navigate.Execute(new FirstViewModel(this))
+            );
+        }
+    }
+}
