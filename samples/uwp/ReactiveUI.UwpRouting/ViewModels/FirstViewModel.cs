fdeecff17726287f8031a4fbf6131ef00af993ab
Artyom <worldbeater-dev@yandex.ru>
2019-05-13 14:30:14 +0300

    Add Universal Windows routing example (#2030)

---
+++ samples/uwp/ReactiveUI.UwpRouting/ViewModels/FirstViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReactiveUI.UwpRouting.ViewModels
+{
+    public class FirstViewModel : ReactiveObject, IRoutableViewModel
+    {
+        // Reference to IScreen that owns the routable view model.
+        public IScreen HostScreen { get; }
+
+        // Unique identifier for the routable view model.
+        public string UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
+
+        public FirstViewModel(IScreen screen) => HostScreen = screen;
+    }
+}
