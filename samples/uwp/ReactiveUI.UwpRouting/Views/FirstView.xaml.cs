97d1aa3780f79f933933befda79666b27a83a76b
Artyom <worldbeater-dev@yandex.ru>
2019-05-13 14:30:14 +0300

    Add Universal Windows routing example (#2030)

---
+++ samples/uwp/ReactiveUI.UwpRouting/Views/FirstView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using ReactiveUI.UwpRouting.ViewModels;
+
+namespace ReactiveUI.UwpRouting.Views
+{
+    public sealed partial class FirstView : UserControl, IViewFor<FirstViewModel>
+    {
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty
+            .Register(nameof(ViewModel), typeof(FirstViewModel), typeof(FirstView), null);
+
+        public FirstView()
+        {
+            InitializeComponent();
+            this.WhenActivated(disposables => { });
+        }
+
+        public FirstViewModel ViewModel
+        {
+            get => (FirstViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (FirstViewModel)value;
+        }
+    }
+}
---
+++ samples/uwp/ReactiveUI.UwpRouting/Views/MainView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using ReactiveUI.UwpRouting.ViewModels;
+
+namespace ReactiveUI.UwpRouting.Views
+{
+    public sealed partial class MainView : Page, IViewFor<MainViewModel>
+    {
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty
+            .Register(nameof(ViewModel), typeof(MainViewModel), typeof(MainView), null);
+
+        public MainView()
+        {
+            InitializeComponent();
+            ViewModel = new MainViewModel();
+            this.WhenActivated(disposables => { });
+        }
+
+        public MainViewModel ViewModel
+        {
+            get => (MainViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (MainViewModel)value;
+        }
+    }
+}
