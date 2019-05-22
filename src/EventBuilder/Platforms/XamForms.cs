ae43b6606b9cef08b9df72a5b88314b1d7e9636a
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2019-05-21 21:33:21 -0500

    housekeeping: Upgraded Xamarin.Forms to v3.6 (#2045)

--- src/EventBuilder/Platforms/XamForms.cs
+++ src/EventBuilder/Platforms/XamForms.cs
@@ -21,7 +21,7 @@ namespace EventBuilder.Platforms
     {
         private readonly PackageIdentity[] _packageNames = new[]
         {
-            new PackageIdentity("Xamarin.Forms", new NuGetVersion("3.5.0.129452")),
+            new PackageIdentity("Xamarin.Forms", new NuGetVersion("3.6.0.220655")),
         };
 
         /// <inheritdoc />
