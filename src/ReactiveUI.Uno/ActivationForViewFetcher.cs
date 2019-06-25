aaf53e7792ee889f9f2a0acd3bf65e3aa3683804
Glenn Watson <GitHub@glennwatson.net>
2019-06-25 15:36:52 +1000

    adding results to parent directories

---
+++ src/ReactiveUI.Uno/ActivationForViewFetcher.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+#if HAS_UNO
+using System;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reflection;
+
+using Windows.Foundation;
+using Windows.UI.Xaml;
+
+namespace ReactiveUI
+{
+    /// <summary>
+    /// ActiveationForViewFetcher is how ReactiveUI determine when a
+    /// View is activated or deactivated. This is usually only used when porting
+    /// ReactiveUI to a new UI framework.
+    /// </summary>
+    public class ActivationForViewFetcher : IActivationForViewFetcher
+    {
+        /// <inheritdoc/>
+        public int GetAffinityForView(Type view)
+        {
+            return typeof(FrameworkElement).GetTypeInfo().IsAssignableFrom(view.GetTypeInfo()) ? 10 : 0;
+        }
+
+        /// <inheritdoc/>
+        public IObservable<bool> GetActivationForView(IActivatable view)
+        {
+            var fe = view as FrameworkElement;
+
+            if (fe == null)
+            {
+                return Observable<bool>.Empty;
+            }
+
+            var viewLoaded = Observable.FromEvent<RoutedEventHandler, bool>(
+                eventHandler =>
+                {
+                    void Handler(object sender, RoutedEventArgs e) => eventHandler(true);
+                    return Handler;
+                },
+                x => fe.Loading += x,
+                x => fe.Loading -= x);
+
+            var viewUnloaded = Observable.FromEvent<RoutedEventHandler, bool>(
+                handler =>
+                {
+                    void EventHandler(object sender, RoutedEventArgs e) => handler(false);
+                    return EventHandler;
+                },
+                x => fe.Unloaded += x,
+                x => fe.Unloaded -= x);
+
+            return viewLoaded
+                .Merge(viewUnloaded)
+                .Select(b => b ? fe.WhenAnyValue(x => x.IsHitTestVisible).SkipWhile(x => !x) : Observables.False)
+                .Switch()
+                .DistinctUntilChanged();
+        }
+    }
+}
+#endif
---
+++ src/ReactiveUI.Uno/platforms/netstandard/PlatformRegistrations.cs
@@ -0,0 +1,30 @@
+// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Reactive.Concurrency;
+
+namespace ReactiveUI
+{
+    /// <summary>
+    /// UWP platform registrations.
+    /// </summary>
+    /// <seealso cref="ReactiveUI.IWantsToRegisterStuff" />
+    public class PlatformRegistrations : IWantsToRegisterStuff
+    {
+        /// <inheritdoc/>
+        public void Register(Action<Func<object>, Type> registerFunction)
+        {
+            registerFunction(() => new PlatformOperations(), typeof(IPlatformOperations));
+            registerFunction(() => new ActivationForViewFetcher(), typeof(IActivationForViewFetcher));
+            registerFunction(() => new DependencyObjectObservableForProperty(), typeof(ICreatesObservableForProperty));
+            registerFunction(() => new BooleanToVisibilityTypeConverter(), typeof(IBindingTypeConverter));
+            registerFunction(() => new AutoDataTemplateBindingHook(), typeof(IPropertyBindingHook));
+            RxApp.TaskpoolScheduler = WasmScheduler.Default;
+            RxApp.MainThreadScheduler = WasmScheduler.Default;
+            registerFunction(() => new WinRTAppDataDriver(), typeof(ISuspensionDriver));
+        }
+    }
+}
