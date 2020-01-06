796fd94da35ad6f9e01a4796197687e581115846
Glenn Watson <GitHub@glennwatson.net>
2020-01-06 14:41:27 +1100

    fix: Blazor StateHasChanged not firing on ViewModel change

--- src/ReactiveUI.Blazor/ReactiveLayoutComponentBase.cs
+++ src/ReactiveUI.Blazor/ReactiveLayoutComponentBase.cs
@@ -33,7 +33,8 @@ namespace ReactiveUI.Blazor
         /// </summary>
         public ReactiveLayoutComponentBase()
         {
-            var propertyChangedObservable = this.WhenAnyValue(x => x.ViewModel)
+            this.WhenAnyValue(x => x.ViewModel).Subscribe(_ => StateHasChanged());
+            var viewModelsPropertyChanged = this.WhenAnyValue(x => x.ViewModel)
                 .Select(x => Observable.FromEvent<PropertyChangedEventHandler, Unit>(
                     eventHandler =>
                     {
@@ -45,7 +46,7 @@ namespace ReactiveUI.Blazor
                     eh => x.PropertyChanged -= eh))
                 .Switch();
 
-            propertyChangedObservable.Do(_ => StateHasChanged()).Subscribe();
+            viewModelsPropertyChanged.Do(_ => StateHasChanged()).Subscribe();
         }
 
         /// <inheritdoc />
