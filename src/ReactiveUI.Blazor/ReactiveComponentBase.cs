f10cb43c9df23c1b8e38b736719308d059c1f1bd
Glenn <5834289+glennawatson@users.noreply.github.com>
2020-04-26 10:21:29 +1000

    Update ReactiveComponentBase.cs

--- src/ReactiveUI.Blazor/ReactiveComponentBase.cs
+++ src/ReactiveUI.Blazor/ReactiveComponentBase.cs
@@ -88,16 +88,14 @@ namespace ReactiveUI.Blazor
                 // The following subscriptions are here because if they are done in OnInitialized, they conflict with certain JavaScript frameworks.
                 var viewModelChanged =
                     this.WhenAnyValue(x => x.ViewModel)
+                        .Where(x => x != null)
                         .Publish()
                         .RefCount();
 
                 viewModelChanged
-                    .Skip(1)
-                    .Where(x => x != null)
                     .Subscribe(_ => InvokeAsync(StateHasChanged));
 
                 viewModelChanged
-                    .Where(x => x != null)
                     .Select(x =>
                         Observable
                             .FromEvent<PropertyChangedEventHandler, Unit>(
