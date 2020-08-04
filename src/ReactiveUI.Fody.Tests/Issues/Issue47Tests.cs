9a3c0b4f13f3857342b2b11f4793973b356b593e
Colt Bauman <cab452005@hotmail.com>
2020-08-04 14:18:38 +0900

    Fix: ReactiveUI.Fody "long" property breaks build

---
+++ src/ReactiveUI.Fody.Tests/Issues/Issue47Tests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
+using ReactiveUI.Fody.Helpers;
+
+namespace ReactiveUI.Fody.Tests.Issues
+{
+    public static class Issue47Tests
+    {
+        /// <summary>
+        /// The "test" here is simply for these to compile
+        /// Tests ObservableAsPropertyWeaver.EmitDefaultValue.
+        /// </summary>
+        [SuppressMessage("Microsoft.Performance", "CA1812: Avoid uninstantiated internal classes", Justification = "Purpose is just to be compiled.")]
+        private class TestModel : ReactiveObject
+        {
+            [ObservableAsProperty]
+            public int IntProperty { get; }
+
+            [ObservableAsProperty]
+            public double DoubleProperty { get; }
+
+            [ObservableAsProperty]
+            public float FloatProperty { get; }
+
+            [ObservableAsProperty]
+            public long LongProperty { get; }
+        }
+    }
+}
