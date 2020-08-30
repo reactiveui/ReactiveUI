4c8117f688ffb41388383cfed5722a8326bbf03f
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-08-29 19:13:10 -0500

    fixing integration tests

--- src/ReactiveUI/Mixins/DependencyResolverMixins.cs
+++ src/ReactiveUI/Mixins/DependencyResolverMixins.cs
@@ -6,8 +6,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
