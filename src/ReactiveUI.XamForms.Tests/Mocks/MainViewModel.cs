a527d7c432874e80ac12f7eb40d409c60cbba2e1
Rastislav Novotný <rasto@duracellko.net>
2020-10-19 14:38:16 +0800

    Unit Tests for Xamarin.Forms RoutedViewHost (#2332) (#2560)
    * Unit Tests for Xamarin.Forms RoutedViewHost (#2332)
    
    * Add XamForms.Tests to Cake
    
    * Remove GetAwaiter in RoutedViewHostTest

---
+++ src/ReactiveUI.XamForms.Tests/Mocks/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Splat;
+
+namespace ReactiveUI.XamForms.Tests.Mocks
+{
+    public class MainViewModel : ReactiveObject, IRoutableViewModel
+    {
+        public MainViewModel()
+        {
+            HostScreen = Locator.Current.GetService<IScreen>();
+        }
+
+        public string? UrlPathSegment => "Main view";
+
+        public IScreen HostScreen { get; }
+    }
+}
