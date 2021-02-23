09bf1f58b1b65863ea0824866535bc05df750d12
artyom <worldbeater-dev@yandex.ru>
2021-02-23 21:48:07 +0300

    housekeeping: Update Copyright Year

--- src/ReactiveUI.XamForms/ActivationForViewFetcher.cs
+++ src/ReactiveUI.XamForms/ActivationForViewFetcher.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2020 .NET Foundation and Contributors. All rights reserved.
+﻿// Copyright (c) 2021 .NET Foundation and Contributors. All rights reserved.
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
