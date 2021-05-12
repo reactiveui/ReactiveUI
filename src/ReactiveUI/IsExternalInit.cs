5e3623a529d495155a6f51a38638220342d35ddb
Glenn Watson <GitHub@glennwatson.net>
2021-05-12 12:14:06 +1000

    Fix tests

--- src/ReactiveUI/IsExternalInit.cs
+++ src/ReactiveUI/IsExternalInit.cs
@@ -1,5 +1,6 @@
-// Copyright (c) 2019-2021 ReactiveUI Association Incorporated. All rights reserved.
-// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// Copyright (c) 2021 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
 using System.ComponentModel;
