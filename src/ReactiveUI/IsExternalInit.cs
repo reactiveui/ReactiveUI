c0bf3f3399705ca56d1389918c7be72c144469b6
Glenn Watson <GitHub@glennwatson.net>
2021-05-12 11:48:15 +1000

    Add IsExternalInit

---
+++ src/ReactiveUI/IsExternalInit.cs
@@ -0,0 +1,17 @@
+// Copyright (c) 2019-2021 ReactiveUI Association Incorporated. All rights reserved.
+// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit
+    {
+    }
+}
