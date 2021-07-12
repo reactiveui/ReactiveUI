075f60bd8e5c43c490de88279bbb6980b3f26ebd
Chris Pulman <chris.pulman@yahoo.com>
2021-07-13 00:21:07 +0100

    fix Transitioning Content Control DPI issue
    RenderTargetBitmap take an int therefore the minimum input value must be 1.

--- src/ReactiveUI.Tests/Platforms/wpf/Mocks/TransitionMock/TCMockWindowViewModel.cs
+++ src/ReactiveUI.Tests/Platforms/wpf/Mocks/TransitionMock/TCMockWindowViewModel.cs
@@ -3,20 +3,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
-using System.Windows;
-using System.Windows.Controls;
-
 namespace ReactiveUI.Tests.Wpf
 {
-    public class WpfTestWindow : Window, IActivatableView
+    public class TCMockWindowViewModel : ReactiveObject
     {
-        public WpfTestWindow()
-        {
-            RootGrid = new Grid();
-
-            AddChild(RootGrid);
-        }
-
-        public Grid RootGrid { get; }
     }
 }
