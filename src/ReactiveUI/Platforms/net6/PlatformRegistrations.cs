a58a99a8a2a8980bc6c981eb3f6cc3d8392f3f36
Glenn Watson <GitHub@glennwatson.net>
2021-08-17 14:17:34 +1000

    Add missing files

---
+++ src/ReactiveUI/Platforms/net6/PlatformRegistrations.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2021 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Reactive.Concurrency;
+using Splat;
+
+namespace ReactiveUI
+{
+    /// <summary>
+    /// Platform registrations for a .NET Core application.
+    /// </summary>
+    public class PlatformRegistrations : IWantsToRegisterStuff
+    {
+        /// <inheritdoc/>
+        public void Register(Action<Func<object>, Type> registerFunction)
+        {
+            if (registerFunction is null)
+            {
+                throw new ArgumentNullException(nameof(registerFunction));
+            }
+
+            registerFunction(() => new ComponentModelTypeConverter(), typeof(IBindingTypeConverter));
+
+            if (!ModeDetector.InUnitTestRunner())
+            {
+                RxApp.TaskpoolScheduler = TaskPoolScheduler.Default;
+                RxApp.MainThreadScheduler = DefaultScheduler.Instance;
+            }
+        }
+    }
+}
