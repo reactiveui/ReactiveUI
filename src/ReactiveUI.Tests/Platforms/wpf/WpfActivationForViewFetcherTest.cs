c48b8a80b449b82b9529db397d671a8f4adb48ca
Glenn <5834289+glennawatson@users.noreply.github.com>
2021-10-30 18:19:12 +1100

    Update WpfActivationForViewFetcherTest.cs

--- src/ReactiveUI.Tests/Platforms/wpf/WpfActivationForViewFetcherTest.cs
+++ src/ReactiveUI.Tests/Platforms/wpf/WpfActivationForViewFetcherTest.cs
@@ -3,11 +3,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
-// Copyright (c) 2021 .NET Foundation and Contributors. All rights reserved.
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for full license information.
-
 using System;
 using System.Reactive.Concurrency;
 using System.Windows;
