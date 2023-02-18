a9743b097a988bf7f522074faaba79d87ba061cf
Chris Pulman <chris.pulman@yahoo.com>
2023-02-18 15:46:53 +0000

    Add Feature to MAUI
    Add ReactiveShell
    Add ReactiveShellContent
    Update Blazor packages
    Update AndriodX packages

---
+++ src/ReactiveUI.Maui/ReactiveShell.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2022 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Microsoft.Maui.Controls;
+
+namespace ReactiveUI.Maui;
+
+/// <summary>
+/// ReactiveShell.
+/// </summary>
+/// <typeparam name="TViewModel">The type of the view model.</typeparam>
+/// <seealso cref="Microsoft.Maui.Controls.Shell" />
+/// <seealso cref="ReactiveUI.IViewFor&lt;TViewModel&gt;" />
+public class ReactiveShell<TViewModel> : Shell, IViewFor<TViewModel>
+    where TViewModel : class
+{
+    /// <summary>
+    /// The view model bindable property.
+    /// </summary>
+    public static readonly BindableProperty ViewModelProperty = BindableProperty.Create(
+     nameof(ViewModel),
+     typeof(TViewModel),
+     typeof(ReactiveShell<TViewModel>),
+     default(TViewModel),
+     BindingMode.OneWay,
+     propertyChanged: OnViewModelChanged);
+
+    /// <summary>
+    /// Gets or sets the ViewModel to display.
+    /// </summary>
+    public TViewModel? ViewModel
+    {
+        get => (TViewModel)GetValue(ViewModelProperty);
+        set => SetValue(ViewModelProperty, value);
+    }
+
+    /// <inheritdoc/>
+    object? IViewFor.ViewModel
+    {
+        get => ViewModel;
+        set => ViewModel = (TViewModel?)value;
+    }
+
+    /// <inheritdoc/>
+    protected override void OnBindingContextChanged()
+    {
+        base.OnBindingContextChanged();
+        ViewModel = BindingContext as TViewModel;
+    }
+
+    private static void OnViewModelChanged(BindableObject bindableObject, object oldValue, object newValue) => bindableObject.BindingContext = newValue;
+}
