45462feefc9a2e296d924a519d027373d65095f5
Glenn Watson <glenn@glennwatson.net>
2023-07-13 11:28:37 +1000

    feature: Add IReactiveCommand generic interface to allow for generic variants

--- src/ReactiveUI/ReactiveCommand/IReactiveCommand.cs
+++ src/ReactiveUI/ReactiveCommand/IReactiveCommand.cs
@@ -34,3 +34,40 @@ public interface IReactiveCommand : IDisposable, IHandleObservableErrors
     /// </remarks>
     IObservable<bool> CanExecute { get; }
 }
+
+/// <summary>
+/// Encapsulates a user action behind a reactive interface.
+/// This is for interop inside for the command binding.
+/// Not meant for external use due to the fact it doesn't implement ICommand
+/// to force the user to favor the Reactive style command execution.
+/// </summary>
+/// <typeparam name="TParam">
+/// The type of parameter values passed in during command execution.
+/// </typeparam>
+/// <typeparam name="TResult">
+/// The type of the values that are the result of command execution.
+/// </typeparam>
+/// <remarks>
+/// <para>
+/// This interface extends <see cref="IReactiveCommand"/> and adds generic type parameters for the parameter values passed
+/// into command execution, and the return values of command execution.
+/// </para>
+/// </remarks>
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "SA1402: File may only contain a single type", Justification = "Same interface name")]
+public interface IReactiveCommand<in TParam, out TResult> : IObservable<TResult>, IReactiveCommand
+{
+    /// <summary>
+    /// Executes the command with the parameter.
+    /// </summary>
+    /// <param name="parameter">
+    /// The parameter to pass into command execution.
+    /// </param>
+    /// <returns>The result.</returns>
+    IObservable<TResult> Execute(TParam parameter);
+
+    /// <summary>
+    /// Executes the command.
+    /// </summary>
+    /// <returns>The result.</returns>
+    IObservable<TResult> Execute();
+}
--- src/ReactiveUI/ReactiveCommand/ReactiveCommandBase.cs
+++ src/ReactiveUI/ReactiveCommand/ReactiveCommandBase.cs
@@ -70,7 +70,7 @@ namespace ReactiveUI;
 /// <typeparam name="TResult">
 /// The type of the values that are the result of command execution.
 /// </typeparam>
-public abstract class ReactiveCommandBase<TParam, TResult> : IObservable<TResult>, ICommand, IReactiveCommand
+public abstract class ReactiveCommandBase<TParam, TResult> : IReactiveCommand<TParam, TResult>, ICommand
 {
     private EventHandler? _canExecuteChanged;
     private bool _canExecuteValue;
@@ -144,61 +144,10 @@ public abstract class ReactiveCommandBase<TParam, TResult> : IObservable<TResult
     /// </returns>
     public abstract IDisposable Subscribe(IObserver<TResult> observer);
 
-    /// <summary>
-    /// Gets an observable that, when subscribed, executes this command.
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// Invoking this method will return a cold (lazy) observable that, when subscribed, will execute the logic
-    /// encapsulated by the command. It is worth restating that the returned observable is lazy. Nothing will
-    /// happen if you call <c>Execute</c> and neglect to subscribe (directly or indirectly) to the returned observable.
-    /// </para>
-    /// <para>
-    /// If no parameter value is provided, a default value of type <typeparamref name="TParam"/> will be passed into
-    /// the execution logic.
-    /// </para>
-    /// <para>
-    /// Any number of subscribers can subscribe to a given execution observable and the execution logic will only
-    /// run once. That is, the result is broadcast to those subscribers.
-    /// </para>
-    /// <para>
-    /// In those cases where execution fails, there will be no result value. Instead, the failure will tick through the
-    /// <see cref="ThrownExceptions"/> observable.
-    /// </para>
-    /// </remarks>
-    /// <param name="parameter">
-    /// The parameter to pass into command execution.
-    /// </param>
-    /// <returns>
-    /// An observable that will tick the single result value if and when it becomes available.
-    /// </returns>
+    /// <inheritdoc/>
     public abstract IObservable<TResult> Execute(TParam parameter);
 
-    /// <summary>
-    /// Gets an observable that, when subscribed, executes this command.
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// Invoking this method will return a cold (lazy) observable that, when subscribed, will execute the logic
-    /// encapsulated by the command. It is worth restating that the returned observable is lazy. Nothing will
-    /// happen if you call <c>Execute</c> and neglect to subscribe (directly or indirectly) to the returned observable.
-    /// </para>
-    /// <para>
-    /// If no parameter value is provided, a default value of type <typeparamref name="TParam"/> will be passed into
-    /// the execution logic.
-    /// </para>
-    /// <para>
-    /// Any number of subscribers can subscribe to a given execution observable and the execution logic will only
-    /// run once. That is, the result is broadcast to those subscribers.
-    /// </para>
-    /// <para>
-    /// In those cases where execution fails, there will be no result value. Instead, the failure will tick through the
-    /// <see cref="ThrownExceptions"/> observable.
-    /// </para>
-    /// </remarks>
-    /// <returns>
-    /// An observable that will tick the single result value if and when it becomes available.
-    /// </returns>
+    /// <inheritdoc/>
     public abstract IObservable<TResult> Execute();
 
     /// <summary>
