246e8ef1ca47bff88228e33a1897aa966f6de109
Chris Pulman <chris.pulman@yahoo.com>
2023-11-23 00:30:14 +0000

    breaking change: Make Bind use non-generic IViewFor (#3676)
    <!-- Please be sure to read the
    [Contribute](https://github.com/reactiveui/reactiveui#contribute)
    section of the README -->
    
    **What kind of change does this PR introduce?**
    <!-- Bug fix, feature, docs update, ... -->
    
    Fix for #3644
    
    **What is the current behavior?**
    <!-- You can also link to an open issue here. -->
    
    BindCommand requires a view inherited from `IViewFor<TViewModel>`
    ReactiveUserControl inherits `IViewFor<TViewModel>` which requires
    Generics which WinForms does not work well with.
    
    **What is the new behavior?**
    <!-- If this is a feature change -->
    
    Add BindCommandNonGeneric this is to avoid the request of not changing
    the existing code, therefore adding to the available options.
    NOTE: Bind only requires `IViewFor` and `IViewFor<TViewModel>` inherits
    `IViewFor` therefore would still be an API change if BindCommand were
    relaxed to IViewFor in the same way as Bind, but perhaps the best route.
    
    add ReactiveUserControlNonGeneric for WinForms -  inherits `IViewFor`
    
    **What might this PR break?**
    
    Non due to new functions
    
    **Please check if the PR fulfills these requirements**
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    **Other information**:

--- src/ReactiveUI/Bindings/Command/CommandBinder.cs
+++ src/ReactiveUI/Bindings/Command/CommandBinder.cs
@@ -63,6 +63,39 @@ public static class CommandBinder
     /// <typeparam name="TViewModel">The view model type.</typeparam>
     /// <typeparam name="TProp">The property type.</typeparam>
     /// <typeparam name="TControl">The control type.</typeparam>
+    /// <typeparam name="TParam">The parameter type.</typeparam>
+    /// <returns>A class representing the binding. Dispose it to disconnect
+    /// the binding.</returns>
+    /// <param name="view">The View.</param>
+    /// <param name="viewModel">The View model.</param>
+    /// <param name="propertyName">The ViewModel command to bind.</param>
+    /// <param name="controlName">The name of the control on the view.</param>
+    /// <param name="withParameter">The ViewModel property to pass as the
+    /// param of the ICommand.</param>
+    /// <param name="toEvent">If specified, bind to the specific event
+    /// instead of the default.
+    /// NOTE: If this parameter is used inside WhenActivated, it's
+    /// important to dispose the binding when the view is deactivated.</param>
+    public static IReactiveBinding<TView, TProp> BindCommandNonGeneric<TView, TViewModel, TProp, TControl, TParam>(
+        this TView view,
+        TViewModel? viewModel,
+        Expression<Func<TViewModel, TProp?>> propertyName,
+        Expression<Func<TView, TControl>> controlName,
+        IObservable<TParam?> withParameter,
+        string? toEvent = null)
+        where TView : class, IViewFor
+        where TViewModel : class
+        where TProp : ICommand =>
+        _binderImplementation.BindCommandNonGeneric(viewModel, view, propertyName, controlName, withParameter, toEvent);
+
+    /// <summary>
+    /// Bind a command from the ViewModel to an explicitly specified control
+    /// on the View.
+    /// </summary>
+    /// <typeparam name="TView">The view type.</typeparam>
+    /// <typeparam name="TViewModel">The view model type.</typeparam>
+    /// <typeparam name="TProp">The property type.</typeparam>
+    /// <typeparam name="TControl">The control type.</typeparam>
     /// <returns>A class representing the binding. Dispose it to disconnect
     /// the binding.</returns>
     /// <param name="view">The View.</param>
@@ -83,6 +116,35 @@ public static class CommandBinder
         where TViewModel : class
         where TProp : ICommand =>
         _binderImplementation.BindCommand(viewModel, view, propertyName, controlName, toEvent);
+
+    /// <summary>
+    /// Bind a command from the ViewModel to an explicitly specified control
+    /// on the View.
+    /// </summary>
+    /// <typeparam name="TView">The view type.</typeparam>
+    /// <typeparam name="TViewModel">The view model type.</typeparam>
+    /// <typeparam name="TProp">The property type.</typeparam>
+    /// <typeparam name="TControl">The control type.</typeparam>
+    /// <returns>A class representing the binding. Dispose it to disconnect
+    /// the binding.</returns>
+    /// <param name="view">The View.</param>
+    /// <param name="viewModel">The View model.</param>
+    /// <param name="propertyName">The ViewModel command to bind.</param>
+    /// <param name="controlName">The name of the control on the view.</param>
+    /// <param name="toEvent">If specified, bind to the specific event
+    /// instead of the default.
+    /// NOTE: If this parameter is used inside WhenActivated, it's
+    /// important to dispose the binding when the view is deactivated.</param>
+    public static IReactiveBinding<TView, TProp> BindCommandNonGeneric<TView, TViewModel, TProp, TControl>(
+        this TView view,
+        TViewModel? viewModel,
+        Expression<Func<TViewModel, TProp?>> propertyName,
+        Expression<Func<TView, TControl>> controlName,
+        string? toEvent = null)
+        where TView : class, IViewFor
+        where TViewModel : class
+        where TProp : ICommand =>
+        _binderImplementation.BindCommandNonGeneric(viewModel, view, propertyName, controlName, toEvent);
 
     /// <summary>
     /// Bind a command from the ViewModel to an explicitly specified control
@@ -126,6 +188,56 @@ public static class CommandBinder
 #endif
 
         return _binderImplementation.BindCommand(
+                                                 viewModel,
+                                                 view,
+                                                 propertyName,
+                                                 controlName,
+                                                 withParameter,
+                                                 toEvent);
+    }
+
+    /// <summary>
+    /// Bind a command from the ViewModel to an explicitly specified control
+    /// on the View.
+    /// </summary>
+    /// <typeparam name="TView">The view type.</typeparam>
+    /// <typeparam name="TViewModel">The view model type.</typeparam>
+    /// <typeparam name="TProp">The property type.</typeparam>
+    /// <typeparam name="TControl">The control type.</typeparam>
+    /// <typeparam name="TParam">The parameter type.</typeparam>
+    /// <returns>A class representing the binding. Dispose it to disconnect
+    /// the binding.</returns>
+    /// <param name="view">The View.</param>
+    /// <param name="viewModel">The View model.</param>
+    /// <param name="propertyName">The ViewModel command to bind.</param>
+    /// <param name="controlName">The name of the control on the view.</param>
+    /// <param name="withParameter">The ViewModel property to pass as the
+    /// param of the ICommand.</param>
+    /// <param name="toEvent">If specified, bind to the specific event
+    /// instead of the default.
+    /// NOTE: If this parameter is used inside WhenActivated, it's
+    /// important to dispose the binding when the view is deactivated.</param>
+    public static IReactiveBinding<TView, TProp> BindCommandNonGeneric<TView, TViewModel, TProp, TControl, TParam>(
+        this TView view,
+        TViewModel? viewModel,
+        Expression<Func<TViewModel, TProp?>> propertyName,
+        Expression<Func<TView, TControl>> controlName,
+        Expression<Func<TViewModel, TParam?>> withParameter,
+        string? toEvent = null)
+        where TView : class, IViewFor
+        where TViewModel : class
+        where TProp : ICommand
+    {
+#if NET6_0_OR_GREATER
+        ArgumentNullException.ThrowIfNull(view);
+#else
+        if (view is null)
+        {
+            throw new ArgumentNullException(nameof(view));
+        }
+#endif
+
+        return _binderImplementation.BindCommandNonGeneric(
                                                  viewModel,
                                                  view,
                                                  propertyName,
--- src/ReactiveUI/Bindings/Command/ICommandBinderImplementation.cs
+++ src/ReactiveUI/Bindings/Command/ICommandBinderImplementation.cs
@@ -63,4 +63,56 @@ internal interface ICommandBinderImplementation : IEnableLogger
         where TView : class, IViewFor<TViewModel>
         where TViewModel : class
         where TProp : ICommand;
+
+    /// <summary>
+    /// Binds the command on a ViewModel to a control on the View.
+    /// </summary>
+    /// <param name="viewModel">The view model to bind to.</param>
+    /// <param name="view">The view to bind to.</param>
+    /// <param name="vmProperty">The name of the property on the View Model.</param>
+    /// <param name="controlProperty">The name of the control on the View.</param>
+    /// <param name="withParameter">A function if we want to pass a parameter to the ICommand.</param>
+    /// <param name="toEvent">A event on the view that will also trigger the command.</param>
+    /// <typeparam name="TView">The type of the view.</typeparam>
+    /// <typeparam name="TViewModel">The type of the view model.</typeparam>
+    /// <typeparam name="TProp">The type of the property on the view model.</typeparam>
+    /// <typeparam name="TControl">The type of control on the view.</typeparam>
+    /// <typeparam name="TParam">The type of the parameter to pass to the ICommand.</typeparam>
+    /// <returns>A reactive binding. Often only used for disposing the binding.</returns>
+    IReactiveBinding<TView, TProp> BindCommandNonGeneric<TView, TViewModel, TProp, TControl, TParam>(
+        TViewModel? viewModel,
+        TView view,
+        Expression<Func<TViewModel, TProp?>> vmProperty,
+        Expression<Func<TView, TControl>> controlProperty,
+        Expression<Func<TViewModel, TParam?>> withParameter,
+        string? toEvent = null)
+        where TView : class, IViewFor
+        where TViewModel : class
+        where TProp : ICommand;
+
+    /// <summary>
+    /// Binds the command on a ViewModel to a control on the View.
+    /// </summary>
+    /// <param name="viewModel">The view model to bind to.</param>
+    /// <param name="view">The view to bind to.</param>
+    /// <param name="vmProperty">The name of the property on the View Model.</param>
+    /// <param name="controlProperty">The name of the control on the View.</param>
+    /// <param name="withParameter">A observable if we want to pass a parameter to the ICommand.</param>
+    /// <param name="toEvent">A event on the view that will also trigger the command.</param>
+    /// <typeparam name="TView">The type of the view.</typeparam>
+    /// <typeparam name="TViewModel">The type of the view model.</typeparam>
+    /// <typeparam name="TProp">The type of the property on the view model.</typeparam>
+    /// <typeparam name="TControl">The type of control on the view.</typeparam>
+    /// <typeparam name="TParam">The type of the parameter to pass to the ICommand.</typeparam>
+    /// <returns>A reactive binding. Often only used for disposing the binding.</returns>
+    IReactiveBinding<TView, TProp> BindCommandNonGeneric<TView, TViewModel, TProp, TControl, TParam>(
+        TViewModel? viewModel,
+        TView view,
+        Expression<Func<TViewModel, TProp?>> vmProperty,
+        Expression<Func<TView, TControl>> controlProperty,
+        IObservable<TParam?> withParameter,
+        string? toEvent = null)
+        where TView : class, IViewFor
+        where TViewModel : class
+        where TProp : ICommand;
 }
