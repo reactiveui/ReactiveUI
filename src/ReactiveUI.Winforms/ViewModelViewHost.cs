518c807ffa1836382341c6bf81b815a5fc8c6c23
Chris Pulman <chris.pulman@yahoo.com>
2025-01-17 20:08:00 +0000

    Housekeeping Update to Net 9.0 (#3933)
    <!-- Please be sure to read the
    [Contribute](https://github.com/reactiveui/reactiveui#contribute)
    section of the README -->
    
    **What kind of change does this PR introduce?**
    <!-- Bug fix, feature, docs update, ... -->
    
    update
    
    **What is the current behavior?**
    <!-- You can also link to an open issue here. -->
    
    Supports Net 6
    
    **What is the new behavior?**
    <!-- If this is a feature change -->
    
    Net 6 retired and added Net 9.0
    
    **What might this PR break?**
    
    Users of Net 6.0 will need to migrate to net 8 or net 9
    
    **Please check if the PR fulfills these requirements**
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    **Other information**:

--- src/ReactiveUI.Winforms/ViewModelViewHost.cs
+++ src/ReactiveUI.Winforms/ViewModelViewHost.cs
@@ -14,10 +14,12 @@ namespace ReactiveUI.Winforms;
 public partial class ViewModelControlHost : UserControl, IReactiveObject, IViewFor
 {
     private readonly CompositeDisposable _disposables = [];
+#pragma warning disable IDE0032 // Use auto property
     private Control? _defaultContent;
     private IObservable<string>? _viewContractObservable;
     private object? _viewModel;
     private object? _content;
+#pragma warning restore IDE0032 // Use auto property
     private bool _cacheViews;
 
     /// <summary>
@@ -39,6 +41,7 @@ public partial class ViewModelControlHost : UserControl, IReactiveObject, IViewF
     /// <summary>
     /// Gets or sets a value indicating whether [default cache views enabled].
     /// </summary>
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
     public static bool DefaultCacheViewsEnabled { get; set; }
 
     /// <summary>
@@ -51,6 +54,7 @@ public partial class ViewModelControlHost : UserControl, IReactiveObject, IViewF
     /// </summary>
     [Category("ReactiveUI")]
     [Description("The default control when no viewmodel is specified")]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
     public Control? DefaultContent
     {
         get => _defaultContent;
@@ -64,6 +68,7 @@ public partial class ViewModelControlHost : UserControl, IReactiveObject, IViewF
     /// The view contract observable.
     /// </value>
     [Browsable(false)]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
     public IObservable<string>? ViewContractObservable
     {
         get => _viewContractObservable;
@@ -74,12 +79,14 @@ public partial class ViewModelControlHost : UserControl, IReactiveObject, IViewF
     /// Gets or sets the view locator.
     /// </summary>
     [Browsable(false)]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
     public IViewLocator? ViewLocator { get; set; }
 
     /// <inheritdoc/>
     [Category("ReactiveUI")]
     [Description("The viewmodel to host.")]
     [Bindable(true)]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
     public object? ViewModel
     {
         get => _viewModel;
@@ -92,6 +99,7 @@ public partial class ViewModelControlHost : UserControl, IReactiveObject, IViewF
     [Category("ReactiveUI")]
     [Description("The Current View")]
     [Bindable(true)]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
     public object? Content
     {
         get => _content;
