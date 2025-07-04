ab1cd516ac461045ea14f9c3a778369dbfb2c5ee
Chris Pulman <chris.pulman@yahoo.com>
2025-07-04 15:11:16 +0100

    Refactor ViewModel change subscriptions in Blazor components (#4069)
    <!-- Please be sure to read the
    [Contribute](https://github.com/reactiveui/reactiveui#contribute)
    section of the README -->
    
    **What kind of change does this PR introduce?**
    <!-- Bug fix, feature, docs update, ... -->
    
    Bug Fix for #4068
    
    **What is the current behavior?**
    <!-- You can also link to an open issue here. -->
    
    #4068
    
    **What is the new behavior?**
    <!-- If this is a feature change -->
    
    Consolidated and streamlined the logic for subscribing to ViewModel
    changes and property changes in ReactiveComponentBase,
    ReactiveInjectableComponentBase, and ReactiveLayoutComponentBase. This
    improves code readability and consistency across the components by using
    a shared pattern for handling ViewModel and property change
    notifications.
    
    Closes #4068 
    
    **What might this PR break?**
    
    None expected
    
    **Please check if the PR fulfills these requirements**
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    **Other information**:

--- src/ReactiveUI.Blazor/ReactiveComponentBase.cs
+++ src/ReactiveUI.Blazor/ReactiveComponentBase.cs
@@ -97,15 +97,15 @@ public class ReactiveComponentBase<T> : ComponentBase, IViewFor<T>, INotifyPrope
 
             viewModelChanged
                 .Select(x =>
-                            Observable
-                                .FromEvent<PropertyChangedEventHandler?, Unit>(
-                                                                               eventHandler =>
-                                                                               {
-                                                                                   void Handler(object? sender, PropertyChangedEventArgs e) => eventHandler(Unit.Default);
-                                                                                   return Handler;
-                                                                               },
-                                                                               eh => x.PropertyChanged += eh,
-                                                                               eh => x.PropertyChanged -= eh))
+                    Observable
+                        .FromEvent<PropertyChangedEventHandler?, Unit>(
+                            eventHandler =>
+                            {
+                                void Handler(object? sender, PropertyChangedEventArgs e) => eventHandler(Unit.Default);
+                                return Handler;
+                            },
+                            eh => x.PropertyChanged += eh,
+                            eh => x.PropertyChanged -= eh))
                 .Switch()
                 .Subscribe(_ => InvokeAsync(StateHasChanged))
                 .DisposeWith(_compositeDisposable);
