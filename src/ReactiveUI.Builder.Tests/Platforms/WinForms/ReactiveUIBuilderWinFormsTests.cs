72709c9807870c818d7d030af4e73b1517b342f8
Glenn <5834289+glennawatson@users.noreply.github.com>
2025-09-09 00:16:15 +1000

    Update ReactiveUIBuilderWinFormsTests.cs

--- src/ReactiveUI.Builder.Tests/Platforms/WinForms/ReactiveUIBuilderWinFormsTests.cs
+++ src/ReactiveUI.Builder.Tests/Platforms/WinForms/ReactiveUIBuilderWinFormsTests.cs
@@ -3,7 +3,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
-using ReactiveUI.Builder;
+using ReactiveUI.Winforms;
 using Splat.Builder;
 
 namespace ReactiveUI.Builder.Tests.Platforms.WinForms;
