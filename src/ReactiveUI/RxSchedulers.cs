7aff4f48e41db48cc4a6a9cf877cb94aec06be3b
Glenn <5834289+glennawatson@users.noreply.github.com>
2025-09-23 16:11:40 +1000

    Simplify return statements in RxSchedulers

--- src/ReactiveUI/RxSchedulers.cs
+++ src/ReactiveUI/RxSchedulers.cs
@@ -85,13 +85,9 @@ public static class RxSchedulers
                 }
 
 #if !PORTABLE
-                {
-                    return _taskpoolScheduler ??= TaskPoolScheduler.Default;
-                }
+                return _taskpoolScheduler ??= TaskPoolScheduler.Default;
 #else
-                {
-                    return _taskpoolScheduler ??= DefaultScheduler.Instance;
-                }
+                return _taskpoolScheduler ??= DefaultScheduler.Instance;
 #endif
             }
         }
