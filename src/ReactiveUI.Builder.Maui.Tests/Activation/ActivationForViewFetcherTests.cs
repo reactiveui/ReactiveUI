5d80b8256a93d18183397c4fbf99b183937e3ec8
Chris Pulman <chris.pulman@yahoo.com>
2025-12-14 23:53:39 +0000

    Update ActivationForViewFetcherTests.cs

--- src/ReactiveUI.Builder.Maui.Tests/Activation/ActivationForViewFetcherTests.cs
+++ src/ReactiveUI.Builder.Maui.Tests/Activation/ActivationForViewFetcherTests.cs
@@ -3,15 +3,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
-using System;
 using System.Reactive.Disposables;
 using System.Reflection;
-using System.Threading;
 using Microsoft.Maui.Controls;
-using NUnit.Framework;
-using ReactiveUI;
 using ReactiveUI.Maui;
-using Splat;
 using Splat.Builder;
 
 namespace ReactiveUI.Builder.Maui.Tests.Activation;
