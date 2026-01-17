a6622e192c6f4b0513273441d818bcbc0b92cadc
Glenn Watson <5834289+glennawatson@users.noreply.github.com>
2026-01-18 02:49:26 +1100

    fix build

--- src/tests/ReactiveUI.Wpf.Tests/Wpf/ReactivePropertyMixinsTests.cs
+++ src/tests/ReactiveUI.Wpf.Tests/Wpf/ReactivePropertyMixinsTests.cs
@@ -5,6 +5,8 @@
 
 using System.ComponentModel.DataAnnotations;
 
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
 namespace ReactiveUI.Tests.Wpf;
 
 [NotInParallel]
