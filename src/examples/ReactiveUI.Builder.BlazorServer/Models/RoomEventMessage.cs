61b64d64d7b5bffe17c245554e6bd21647c61c64
Bekzod Allaev <Allaev_@outlook.com>
2026-01-27 18:17:16 +0100

    Add core chat models for Blazor Server app
    Introduced six model classes in the ReactiveUI.Builder.BlazorServer.Models namespace to support chat functionality: ChatMessage, ChatNetworkMessage, ChatRoom, ChatState, ChatStateChanged, and RoomEventMessage. These models enable chat room management, message broadcasting, and state synchronization. All files include appropriate .NET Foundation copyright and MIT license headers.

---
+++ src/examples/ReactiveUI.Builder.BlazorServer/Models/RoomEventMessage.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) 2025 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace ReactiveUI.Builder.BlazorServer.Models;
+
+/// <summary>
+/// Network event describing a change in the rooms list.
+/// </summary>
+public sealed class RoomEventMessage
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RoomEventMessage"/> class.
+    /// </summary>
+    public RoomEventMessage()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RoomEventMessage"/> class.
+    /// </summary>
+    /// <param name="kind">The event kind.</param>
+    /// <param name="roomName">The room name.</param>
+    public RoomEventMessage(RoomEventKind kind, string roomName)
+    {
+        Kind = kind;
+        RoomName = roomName;
+    }
+
+    /// <summary>
+    /// Gets or sets the event kind.
+    /// </summary>
+    public RoomEventKind Kind { get; set; }
+
+    /// <summary>
+    /// Gets or sets the room name for this event.
+    /// </summary>
+    public string RoomName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the originating instance id.
+    /// </summary>
+    public Guid InstanceId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the current snapshot of room names. Used in response to SyncRequest.
+    /// </summary>
+    public List<string>? Snapshot { get; set; }
+}
