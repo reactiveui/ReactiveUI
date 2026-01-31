f4d5e405be64aa155618e001f4da237b9012826e
Bekzod Allaev <allaev_@outlook.com>
2026-01-31 16:26:03 +0100

    feature: Add blazor example app (#4279)
    **What kind of change does this PR introduce?**
    <!-- Bug fix, feature, docs update, ... -->
    Adding Blazor test app as request in
    https://github.com/reactiveui/ReactiveUI/issues/2450
    
    
    **What is the current behavior?**
    
    
    
    
    **What is the new behavior?**
    
    
    
    
    **What might this PR break?**
    
    
    
    **Please check if the PR fulfills these requirements**
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    **Other information**:
    I took WpfTest app as a sample and mostly used the code from it. Changes
    that were made was to adopt code from WpfTest to Blazor specifics. Here
    is demonstration of the test app
    
    
    https://github.com/user-attachments/assets/9ca32fdb-4017-4a96-ab71-c5a2690fba50

---
+++ src/examples/ReactiveUI.Builder.BlazorServer/Views/ChatRoomView.razor.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2025 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Reactive.Linq;
+using Microsoft.AspNetCore.Components.Web;
+using ReactiveUI.Blazor;
+using ReactiveUI.Builder.BlazorServer.ViewModels;
+
+namespace ReactiveUI.Builder.BlazorServer.Views
+{
+    /// <summary>
+    /// Represents a view component for displaying and interacting with a chat room in the user interface.
+    /// </summary>
+    public partial class ChatRoomView : ReactiveComponentBase<ChatRoomViewModel>
+    {
+        private async Task OnSendClicked(MouseEventArgs args)
+        {
+            if (ViewModel is null)
+            {
+                throw new ArgumentNullException(nameof(ViewModel));
+            }
+
+            await ViewModel.SendMessage.Execute();
+        }
+
+        private async Task OnNavigateBack(MouseEventArgs args)
+        {
+            if (ViewModel is null)
+            {
+                throw new ArgumentNullException(nameof(ViewModel));
+            }
+
+            await ViewModel.NavigateBack.Execute();
+        }
+    }
+}
---
+++ src/examples/ReactiveUI.Builder.BlazorServer/Views/LobbyView.razor.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) 2025 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Reactive.Linq;
+using Microsoft.AspNetCore.Components.Web;
+using ReactiveUI;
+using ReactiveUI.Blazor;
+using ReactiveUI.Builder.BlazorServer.Models;
+using ReactiveUI.Builder.BlazorServer.ViewModels;
+
+namespace ReactiveUI.Builder.BlazorServer.Views
+{
+    /// <summary>
+    /// Lobby (rooms listing) view.
+    /// </summary>
+    public partial class LobbyView : ReactiveComponentBase<LobbyViewModel>
+    {
+        private async Task OnCreateRoomClicked(MouseEventArgs args)
+        {
+            if (ViewModel is null)
+            {
+                throw new ArgumentNullException(nameof(ViewModel));
+            }
+
+            await ViewModel.CreateRoom.Execute();
+        }
+
+        private async Task OnDeleteSelectedClicked(MouseEventArgs args)
+        {
+            if (ViewModel is null)
+            {
+                throw new ArgumentNullException(nameof(ViewModel));
+            }
+
+            if (ViewModel.SelectedChatRoom is null)
+            {
+                throw new InvalidOperationException("No chat room selected to delete.");
+            }
+
+            await ViewModel.DeleteRoom.Execute(ViewModel.SelectedChatRoom);
+        }
+
+        private void OnRoomClicked(ChatRoom room)
+        {
+            if (ViewModel is null)
+            {
+                throw new ArgumentNullException(nameof(ViewModel));
+            }
+
+            ViewModel.SelectedChatRoom = room;
+        }
+
+        private async Task OnRoomDoubleClicked(ChatRoom room)
+        {
+            if (ViewModel is null)
+            {
+                throw new ArgumentNullException(nameof(ViewModel));
+            }
+
+            await ViewModel.JoinRoom.Execute(room);
+        }
+    }
+}
