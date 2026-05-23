164f9e799b93eb73d7d8e9baf5728e22b866b7a9
Glenn Watson <5834289+glennawatson@users.noreply.github.com>
2026-05-23 14:26:40 +1000

    Fix: Add ConfigureAwait(false) to Interaction.cs to prevent deadlocks

--- src/ReactiveUI/Interactions/Interaction.cs
+++ src/ReactiveUI/Interactions/Interaction.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2009-2026 .NET Foundation and Contributors. All rights reserved.
+﻿// Copyright (c) 2009-2026 .NET Foundation and Contributors. All rights reserved.
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
@@ -124,7 +124,7 @@ public class Interaction<TInput, TOutput>(IScheduler? handlerScheduler = null) :
 
         async Task InvokeAsync(IInteractionContext<TInput, TOutput> interaction)
         {
-            await YieldToCurrentContext();
+            await YieldToCurrentContext().ConfigureAwait(false);
             await handler(interaction).ConfigureAwait(false);
         }
     }
